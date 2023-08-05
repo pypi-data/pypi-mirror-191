15952703877d40abba23d9ec83107947d8d74a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:36 +0000

    Add package vsaa/1.6.0/vsaa-1.6.0-py3-none-any.whl

---
+++ packages/vsaa/vsaa-1.6.0-py3-none-any.whl/vsaa/antialiasers/znedi3.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any
+
+from vstools import core, vs
+
+from ..abstract import Antialiaser, DoubleRater, SingleRater, SuperSampler, _Antialiaser
+from . import nnedi3
+
+__all__ = [
+    'Znedi3', 'Znedi3DR'
+]
+
+
+@dataclass
+class ZNEDI3(_Antialiaser):
+    nsize: int = 4
+    nns: int = 4
+    qual: int = 2
+    etype: int = 0
+    pscrn: int = 2
+
+    def get_aa_args(self, clip: vs.VideoNode, **kwargs: Any) -> dict[str, Any]:
+        return nnedi3.NNEDI3.get_aa_args(self, clip, **kwargs)  # type: ignore
+
+    def interpolate(self, clip: vs.VideoNode, double_y: bool, **kwargs: Any) -> vs.VideoNode:
+        interpolated = core.znedi3.nnedi3(
+            clip, self.field, double_y or not self.drop_fields, **kwargs
+        )
+
+        return self.shift_interpolate(clip, interpolated, double_y)
+
+    _shift = 0.5
+
+
+class Znedi3SS(ZNEDI3, SuperSampler):
+    ...
+
+
+class Znedi3SR(ZNEDI3, SingleRater):
+    ...
+
+
+class Znedi3DR(ZNEDI3, DoubleRater):
+    ...
+
+
+class Znedi3(ZNEDI3, Antialiaser):
+    ...
