9375e67626b3a87d04dfba3755d2959dba1e6b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:38 +0000

    Add package cli-template-proj/0.2.1/cli_template_proj-0.2.1.tar.gz

---
+++ packages/cli-template-proj/cli_template_proj-0.2.1.tar.gz/cli_template_proj-0.2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cli_template_proj',
+    version='0.2.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'click',
+    ],
+    entry_points='''
+        [console_scripts]
+        cli_proj=cli_template_proj.cli:main
+    ''',
+)
