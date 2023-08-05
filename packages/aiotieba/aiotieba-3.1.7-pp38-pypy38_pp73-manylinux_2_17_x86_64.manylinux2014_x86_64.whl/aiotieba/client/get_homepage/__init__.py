015750b86f4b1339e2aca9197cfd16941f2d21ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:27 +0000

    Add package aiotieba/3.1.7/aiotieba-3.1.7-pp38-pypy38_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/aiotieba/aiotieba-3.1.7-pp38-pypy38_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aiotieba/client/get_homepage/__init__.py
@@ -0,0 +1,2 @@
+from ._api import CMD, pack_proto, parse_body, request_http
+from ._classdef import Thread_home, UserInfo_home
---
+++ packages/aiotieba/aiotieba-3.1.7-pp38-pypy38_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aiotieba/client/get_recom_status/_api.py
@@ -0,0 +1,44 @@
+import sys
+
+import yarl
+
+from .._core import APP_BASE_HOST, HttpCore
+from .._helper import APP_SECURE_SCHEME, log_exception, pack_form_request, parse_json, send_request
+from ..exception import TiebaServerError
+from ._classdef import RecomStatus
+
+
+def parse_body(body: bytes) -> RecomStatus:
+    res_json = parse_json(body)
+    if code := int(res_json['error_code']):
+        raise TiebaServerError(code, res_json['error_msg'])
+
+    status = RecomStatus()._init(res_json)
+
+    return status
+
+
+async def request(http_core: HttpCore, fid: int) -> RecomStatus:
+    data = [
+        ('BDUSS', http_core.core._BDUSS),
+        ('_client_version', http_core.core.main_version),
+        ('forum_id', fid),
+        ('pn', '1'),
+        ('rn', '0'),
+    ]
+
+    request = pack_form_request(
+        http_core,
+        yarl.URL.build(scheme=APP_SECURE_SCHEME, host=APP_BASE_HOST, path="/c/f/bawu/getRecomThreadList"),
+        data,
+    )
+
+    try:
+        body = await send_request(request, http_core.connector, read_bufsize=2 * 1024)
+        status = parse_body(body)
+
+    except Exception as err:
+        log_exception(sys._getframe(1), err, f"fid={fid}")
+        status = RecomStatus()._init_null()
+
+    return status
