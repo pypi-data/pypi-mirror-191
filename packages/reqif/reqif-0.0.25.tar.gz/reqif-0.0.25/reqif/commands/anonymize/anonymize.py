618317c153d04732408f61110edb7dc50c053a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:35 +0000

    Add package reqif/0.0.25/reqif-0.0.25.tar.gz

---
+++ packages/reqif/reqif-0.0.25.tar.gz/reqif-0.0.25/reqif/commands/anonymize/anonymize.py
@@ -0,0 +1,81 @@
+import os
+import sys
+from typing import List
+
+from reqif.cli.cli_arg_parser import AnonimizeCommandConfig
+from reqif.models.reqif_spec_object import ReqIFSpecObject, SpecObjectAttribute
+from reqif.models.reqif_specification import ReqIFSpecification
+from reqif.models.reqif_types import SpecObjectAttributeType
+from reqif.parser import ReqIFParser
+from reqif.unparser import ReqIFUnparser
+
+ANONYMIZED = "...Anonymized..."
+
+
+class AnonymizeCommand:
+    @classmethod
+    def execute(cls, config: AnonimizeCommandConfig) -> None:
+        input_file = config.input_file
+        if not os.path.isfile(input_file):
+            sys.stdout.flush()
+            message = "error: passthrough command's input file does not exist"
+            print(f"{message}: {input_file}")  # noqa: T201
+            sys.exit(1)
+
+        output = AnonymizeCommand._anonymize(config)
+        output_file = config.output_file
+        output_dir = os.path.dirname(output_file)
+        if not os.path.isdir(output_dir):
+            print(  # noqa: T201
+                f"error: output directory does not exist: {output_file}"
+            )
+            sys.exit(1)
+        with open(output_file, "w", encoding="UTF-8") as file:
+            file.write(output)
+
+    @staticmethod
+    def _anonymize(passthrough_config: AnonimizeCommandConfig):
+        reqif_bundle = ReqIFParser.parse(passthrough_config.input_file)
+
+        req_if_header = reqif_bundle.req_if_header
+        if req_if_header:
+            if req_if_header.title:
+                req_if_header.title = ANONYMIZED
+            if req_if_header.comment:
+                req_if_header.comment = ANONYMIZED
+
+        core_content = reqif_bundle.core_content
+        if core_content:
+            reqif_content = core_content.req_if_content
+            if reqif_content:
+                spec_objects = reqif_content.spec_objects
+                if spec_objects:
+                    AnonymizeCommand._anonymize_spec_objects(spec_objects)
+                specifications = reqif_content.specifications
+                if specifications:
+                    AnonymizeCommand._anonymize_specifications(specifications)
+        reqif_xml_output = ReqIFUnparser.unparse(reqif_bundle)
+        return reqif_xml_output
+
+    @staticmethod
+    def _anonymize_spec_objects(spec_objects: List[ReqIFSpecObject]):
+        for spec_object in spec_objects:
+            for attribute in spec_object.attributes:
+                AnonymizeCommand._anonymize_attribute(attribute)
+
+    @staticmethod
+    def _anonymize_specifications(specifications: List[ReqIFSpecification]):
+        for specification in specifications:
+            if specification.long_name:
+                specification.long_name = ANONYMIZED
+            if specification.values:
+                for value in specification.values:
+                    AnonymizeCommand._anonymize_attribute(value)
+
+    @staticmethod
+    def _anonymize_attribute(attribute):
+        assert isinstance(attribute, SpecObjectAttribute), f"{attribute}"
+        if attribute.attribute_type == SpecObjectAttributeType.STRING:
+            attribute.value = ANONYMIZED
+        elif attribute.attribute_type == SpecObjectAttributeType.XHTML:
+            attribute.value = f"<xhtml:div>{ANONYMIZED}</xhtml:div>"
---
+++ packages/reqif/reqif-0.0.25.tar.gz/reqif-0.0.25/reqif/models/reqif_spec_object.py
@@ -0,0 +1,59 @@
+from typing import Any, Dict, List, Optional
+
+from reqif.helpers.debug import auto_described
+from reqif.models.reqif_types import SpecObjectAttributeType
+
+
+@auto_described
+class SpecObjectAttribute:
+    def __init__(
+        self,
+        xml_node: Optional[Any],
+        attribute_type: SpecObjectAttributeType,
+        definition_ref: str,
+        value: str,
+    ):
+        self.xml_node: Optional[Any] = xml_node
+        self.attribute_type: SpecObjectAttributeType = attribute_type
+        self.definition_ref: str = definition_ref
+        self.value: str = value
+
+
+@auto_described
+class ReqIFSpecObject:  # pylint: disable=too-many-instance-attributes
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        xml_node: Optional[Any],
+        description: Optional[str],
+        identifier: str,
+        last_change: Optional[str],
+        long_name: Optional[str],
+        spec_object_type,
+        attributes: List[SpecObjectAttribute],
+    ):
+        self.xml_node: Optional[Any] = xml_node
+        self.description: Optional[str] = description
+        self.identifier: str = identifier
+        self.last_change: Optional[str] = last_change
+        self.long_name: Optional[str] = long_name
+        self.spec_object_type = spec_object_type
+        self.attributes: List[SpecObjectAttribute] = attributes
+        self.attribute_map: Dict[str, SpecObjectAttribute] = {}
+        for attribute in attributes:
+            self.attribute_map[attribute.definition_ref] = attribute
+
+    @staticmethod
+    def create(
+        identifier: str,
+        spec_object_type: str,
+        attributes: List[SpecObjectAttribute],
+    ):
+        return ReqIFSpecObject(
+            xml_node=None,
+            description=None,
+            identifier=identifier,
+            last_change=None,
+            long_name=None,
+            spec_object_type=spec_object_type,
+            attributes=attributes,
+        )
