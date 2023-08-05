a1eac30c0034577f5be0f51c10f7b206e4d5a0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:23 +0000

    Add package setuptools-protobuf/0.1.6/setuptools-protobuf-0.1.6.tar.gz

---
+++ packages/setuptools-protobuf/setuptools-protobuf-0.1.6.tar.gz/setuptools-protobuf-0.1.6/setuptools_protobuf/__init__.py
@@ -0,0 +1,89 @@
+import os
+import subprocess
+import sys
+from distutils.spawn import find_executable
+
+import setuptools.command.build
+from setuptools import Command
+from setuptools.errors import ExecError, PlatformError  # type: ignore
+
+__version__ = (0, 1, 6)
+
+
+def has_protobuf(command):
+    return bool(getattr(command.distribution, 'protobufs', []))
+
+
+class build_protobuf(Command):
+    user_options = [('protoc', None, 'path of compiler protoc')]
+    description = 'build .proto files'
+
+    def initialize_options(self):
+        self.protoc = os.environ.get('PROTOC') or find_executable('protoc')
+
+    def finalize_options(self):
+        if self.protoc is None or not os.path.exists(self.protoc):
+            raise PlatformError(
+                "Unable to find protobuf compiler %s"
+                % (self.protoc or 'protoc'))
+
+    def run(self):
+        for protobuf in getattr(self.distribution, 'protobufs', []):
+            source_mtime = os.path.getmtime(protobuf.path)
+            for output in protobuf.outputs():
+                try:
+                    output_mtime = os.path.getmtime(output)
+                except FileNotFoundError:
+                    break
+                else:
+                    if output_mtime < source_mtime:
+                        break
+            else:
+                continue
+            command = [self.protoc, '--python_out=.']
+            if protobuf.mypy:
+                command.append('--mypy_out=.')
+            command.append(protobuf.path)
+            sys.stderr.write(
+                'creating %r from %s\n' % (protobuf.outputs(), protobuf.path))
+            # TODO(jelmer): Support e.g. building mypy ?
+            try:
+                subprocess.check_call(command, )
+            except subprocess.CalledProcessError as e:
+                raise ExecError(f'error running protoc: {e.returncode}')
+
+
+setuptools.command.build.build.sub_commands.insert(
+    0, ('build_protobuf', has_protobuf))
+
+
+class clean_protobuf(Command):
+    description = 'clean .proto files'
+
+    def run(self):
+        for protobuf in getattr(self, 'protobufs', []):
+            for output in protobuf.outputs():
+                try:
+                    os.unlink(output)
+                except FileNotFoundError:
+                    pass
+
+
+class Protobuf:
+
+    def __init__(self, path, mypy=None):
+        self.path = path
+        if mypy is None:
+            mypy = find_executable('protoc-gen-mypy') is not None
+        self.mypy = mypy
+
+    def outputs(self):
+        return [self.path[:-len('.proto')] + '_pb2.py']
+
+
+def protobufs(dist, keyword, value):
+    for protobuf in value:
+        if not isinstance(protobuf, Protobuf):
+            raise TypeError(protobuf)
+
+    dist.protobufs = value
