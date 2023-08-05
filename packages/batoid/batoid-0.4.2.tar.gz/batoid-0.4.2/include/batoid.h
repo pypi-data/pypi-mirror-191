93ee1057132ef8aa047e0ca38bd2add9973781e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:43 +0000

    Add package batoid/0.4.2/batoid-0.4.2.tar.gz

---
+++ packages/batoid/batoid-0.4.2.tar.gz/batoid-0.4.2/include/batoid.h
@@ -0,0 +1,55 @@
+#ifndef batoid_batoid_h
+#define batoid_batoid_h
+
+#include <array>
+#include "rayVector.h"
+#include "surface.h"
+#include "medium.h"
+#include "obscuration.h"
+#include "coating.h"
+
+namespace batoid {
+    using vec3 = std::array<double, 3>;
+    using mat3 = std::array<double, 9>;  // Column major rotation matrix.
+
+    void applyForwardTransform(const vec3 dr, const mat3 drot, RayVector& rv);
+    void applyReverseTransform(const vec3 dr, const mat3 drot, RayVector& rv);
+    void obscure(const Obscuration& obsc, RayVector& rv);
+    void intersect(
+        const Surface& surface, const vec3 dr, const mat3 drot, RayVector& rv,
+        const Coating* coating
+    );
+    void reflect(
+        const Surface& surface, const vec3 dr, const mat3 drot, RayVector& rv,
+        const Coating* coating
+    );
+    void refract(
+        const Surface& surface, const vec3 dr, const mat3 drot,
+        const Medium& m1, const Medium& m2, RayVector& rv, const Coating* coating
+    );
+    void rSplit(
+        const Surface& surface, const vec3 dr, const mat3 drot,
+        const Medium& m1, const Medium& m2,
+        const Coating& coating,
+        RayVector& rv, RayVector& rvSplit
+    );
+    void refractScreen(
+        const Surface& surface, const vec3 dr, const mat3 drot,
+        const Surface& screen, RayVector& rv
+    );
+
+    void applyForwardTransformArrays(
+        const vec3 dr, const mat3 drot,
+        double* x, double* y, double* z,
+        size_t n
+    );
+    void applyReverseTransformArrays(
+        const vec3 dr, const mat3 drot,
+        double* x, double* y, double* z,
+        size_t n
+    );
+
+    void finishParallel(const vec3 dr, const mat3 drot, const vec3 vv, double* x, double* y, double* z, size_t n);
+}
+
+#endif
---
+++ packages/batoid/batoid-0.4.2.tar.gz/batoid-0.4.2/src/surface.cpp
@@ -0,0 +1,80 @@
+#include "surface.h"
+
+namespace batoid {
+
+    #if defined(BATOID_GPU)
+        #pragma omp declare target
+    #endif
+
+    Surface::Surface() :
+        _devPtr(nullptr)
+    {}
+
+    Surface::~Surface() {
+        #if defined(BATOID_GPU)
+            if (_devPtr) {
+                freeDevPtr();
+            }
+        #endif
+    }
+
+    bool Surface::timeToIntersect(
+        const double x, const double y, const double z,
+        const double vx, const double vy, const double vz,
+        double& dt  // Used as initial guess on input!
+    ) const {
+        // The better the initial estimate of dt, the better this will perform
+        double rPx = x+vx*dt;
+        double rPy = y+vy*dt;
+        double rPz = z+vz*dt;
+
+        double sz = sag(rPx, rPy);
+        // Always do exactly 5 iterations.  GPUifies better this way.
+        // Unit tests pass (as of 20/10/13) with just 3 iterations.
+        for (int iter=0; iter<5; iter++) {
+            // repeatedly intersect plane tangent to surface at (rPx, rPy, sz) with ray
+            double nx, ny, nz;
+            normal(rPx, rPy, nx, ny, nz);
+            dt = (rPx-x)*nx + (rPy-y)*ny + (sz-z)*nz;
+            dt /= (nx*vx + ny*vy + nz*vz);
+            rPx = x+vx*dt;
+            rPy = y+vy*dt;
+            rPz = z+vz*dt;
+            sz = sag(rPx, rPy);
+        }
+        return (std::abs(sz-rPz) < 1e-12);
+    }
+
+    void Surface::grad(
+        double x, double y,
+        double& dzdx, double& dzdy
+    ) const {
+        double nx, ny, nz;
+        normal(x, y, nx, ny, nz);
+        if (std::isnan(nx)) {
+            dzdx = NAN;
+            dzdy = NAN;
+            return;
+        }
+        dzdx = -nx/nz;
+        dzdy = -ny/nz;
+    }
+
+    #if defined(BATOID_GPU)
+        #pragma omp end declare target
+    #endif
+
+    #if defined(BATOID_GPU)
+    void Surface::freeDevPtr() const {
+        if(_devPtr) {
+            Surface* ptr = _devPtr;
+            _devPtr = nullptr;
+            #pragma omp target is_device_ptr(ptr)
+            {
+                delete ptr;
+            }
+        }
+    }
+    #endif
+
+}
