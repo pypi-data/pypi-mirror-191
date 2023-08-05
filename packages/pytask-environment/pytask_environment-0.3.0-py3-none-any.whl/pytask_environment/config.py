eced163bda6550b034435a0a9c1613c022cd9265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:21 +0000

    Add package pytask-environment/0.3.0/pytask_environment-0.3.0-py3-none-any.whl

---
+++ packages/pytask-environment/pytask_environment-0.3.0-py3-none-any.whl/pytask_environment/config.py
@@ -0,0 +1,28 @@
+"""This module contains everything related to the configuration."""
+from __future__ import annotations
+
+from typing import Any
+
+import click
+from pytask import hookimpl
+
+
+@hookimpl
+def pytask_extend_command_line_interface(cli: click.Group) -> None:
+    """Extend the cli."""
+    cli.commands["build"].params.append(
+        click.Option(
+            ["--update-environment"],
+            is_flag=True,
+            default=False,
+            help="Update the information on the environment stored in the database.",
+        )
+    )
+
+
+@hookimpl
+def pytask_parse_config(config: dict[str, Any]) -> None:
+    """Parse the configuration."""
+    config["check_python_version"] = config.get("check_python_version", True)
+    config["check_environment"] = config.get("check_environment", True)
+    config["update_environment"] = config.get("update_environment", False)
---
+++ packages/pytask-environment/pytask_environment-0.3.0-py3-none-any.whl/pytask_environment/plugin.py
@@ -0,0 +1,16 @@
+"""Entry-point for the plugin."""
+from __future__ import annotations
+
+from pluggy import PluginManager
+from pytask import hookimpl
+from pytask_environment import config
+from pytask_environment import database
+from pytask_environment import logging
+
+
+@hookimpl
+def pytask_add_hooks(pm: PluginManager) -> None:
+    """Register some plugins."""
+    pm.register(logging)
+    pm.register(config)
+    pm.register(database)
