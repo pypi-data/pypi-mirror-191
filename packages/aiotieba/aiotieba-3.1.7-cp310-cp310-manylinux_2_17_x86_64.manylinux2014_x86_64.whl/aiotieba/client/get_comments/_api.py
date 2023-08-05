c2fe8d1fae0149ff0e322b85b62323377c577809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:26 +0000

    Add package aiotieba/3.1.7/aiotieba-3.1.7-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/aiotieba/aiotieba-3.1.7-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aiotieba/client/get_comments/_api.py
@@ -0,0 +1,56 @@
+import sys
+
+import yarl
+
+from .._core import APP_BASE_HOST, HttpCore, TbCore
+from .._helper import APP_INSECURE_SCHEME, log_exception, pack_proto_request, send_request
+from ..exception import TiebaServerError
+from ._classdef import Comments
+from .protobuf import PbFloorReqIdl_pb2, PbFloorResIdl_pb2
+
+CMD = 302002
+
+
+def pack_proto(core: TbCore, tid: int, pid: int, pn: int, is_floor: bool) -> bytes:
+    req_proto = PbFloorReqIdl_pb2.PbFloorReqIdl()
+    req_proto.data.common._client_type = 2
+    req_proto.data.common._client_version = core.main_version
+    req_proto.data.tid = tid
+    if is_floor:
+        req_proto.data.spid = pid
+    else:
+        req_proto.data.pid = pid
+    req_proto.data.pn = pn
+
+    return req_proto.SerializeToString()
+
+
+def parse_body(body: bytes) -> Comments:
+    res_proto = PbFloorResIdl_pb2.PbFloorResIdl()
+    res_proto.ParseFromString(body)
+
+    if code := res_proto.error.errorno:
+        raise TiebaServerError(code, res_proto.error.errmsg)
+
+    data_proto = res_proto.data
+    comments = Comments()._init(data_proto)
+
+    return comments
+
+
+async def request_http(http_core: HttpCore, tid: int, pid: int, pn: int, is_floor: bool) -> Comments:
+    request = pack_proto_request(
+        http_core,
+        yarl.URL.build(scheme=APP_INSECURE_SCHEME, host=APP_BASE_HOST, path="/c/f/pb/floor", query_string=f"cmd={CMD}"),
+        pack_proto(http_core.core, tid, pid, pn, is_floor),
+    )
+
+    try:
+        body = await send_request(request, http_core.connector, read_bufsize=8 * 1024)
+        comments = parse_body(body)
+
+    except Exception as err:
+        log_exception(sys._getframe(1), err, f"tid={tid} pid={pid}")
+        comments = Comments()._init_null()
+
+    return comments
---
+++ packages/aiotieba/aiotieba-3.1.7-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aiotieba/client/get_forum_detail/_api.py
@@ -0,0 +1,42 @@
+import sys
+
+import yarl
+
+from .._core import APP_BASE_HOST, HttpCore
+from .._helper import APP_INSECURE_SCHEME, log_exception, pack_form_request, parse_json, send_request
+from ..exception import TiebaServerError
+from ._classdef import Forum_detail
+
+
+def parse_body(body: bytes) -> Forum_detail:
+    res_json = parse_json(body)
+    if code := int(res_json['error_code']):
+        raise TiebaServerError(code, res_json['error_msg'])
+
+    forum_dict = res_json['forum_info']
+    forum = Forum_detail()._init(forum_dict)
+
+    return forum
+
+
+async def request(http_core: HttpCore, fid: int) -> Forum_detail:
+    data = [
+        ('_client_version', http_core.core.main_version),
+        ('forum_id', fid),
+    ]
+
+    request = pack_form_request(
+        http_core,
+        yarl.URL.build(scheme=APP_INSECURE_SCHEME, host=APP_BASE_HOST, path="/c/f/forum/getforumdetail"),
+        data,
+    )
+
+    try:
+        body = await send_request(request, http_core.connector, read_bufsize=8 * 1024)
+        forum = parse_body(body)
+
+    except Exception as err:
+        log_exception(sys._getframe(1), err, f"fid={fid}")
+        forum = Forum_detail()._init_null()
+
+    return forum
