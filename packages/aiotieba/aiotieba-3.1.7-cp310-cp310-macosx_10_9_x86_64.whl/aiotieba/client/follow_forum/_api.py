f29dcb755bc5645ae3061bdaf1691dcdf5bc50d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:26 +0000

    Add package aiotieba/3.1.7/aiotieba-3.1.7-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/aiotieba/aiotieba-3.1.7-cp310-cp310-macosx_10_9_x86_64.whl/aiotieba/client/follow_forum/_api.py
@@ -0,0 +1,43 @@
+import sys
+
+import yarl
+
+from .._core import APP_BASE_HOST, HttpCore
+from .._helper import APP_SECURE_SCHEME, log_exception, log_success, pack_form_request, parse_json, send_request
+from ..exception import TiebaServerError
+
+
+def parse_body(body: bytes) -> None:
+    res_json = parse_json(body)
+    if code := int(res_json['error_code']):
+        raise TiebaServerError(code, res_json['error_msg'])
+    if code := int(res_json['error']['errno']):
+        raise TiebaServerError(code, res_json['error']['errmsg'])
+
+
+async def request(http_core: HttpCore, fid: int) -> bool:
+    data = [
+        ('BDUSS', http_core.core._BDUSS),
+        ('fid', fid),
+        ('tbs', http_core.core._tbs),
+    ]
+
+    request = pack_form_request(
+        http_core,
+        yarl.URL.build(scheme=APP_SECURE_SCHEME, host=APP_BASE_HOST, path="/c/c/forum/like"),
+        data,
+    )
+
+    log_str = f"fid={fid}"
+    frame = sys._getframe(1)
+
+    try:
+        body = await send_request(request, http_core.connector, read_bufsize=2 * 1024)
+        parse_body(body)
+
+    except Exception as err:
+        log_exception(frame, err, log_str)
+        return False
+
+    log_success(frame, log_str)
+    return True
