51dad4b2fd280ab5fa21525cb6b1963e39f6d506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:21 +0000

    Add package vik/1.0.2b0/vik-1.0.2b0.tar.gz

---
+++ packages/vik/vik-1.0.2b0.tar.gz/vik-1.0.2b0/vik/vik.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import sys
+from importlib import resources
+from tempfile import TemporaryDirectory
+
+
+def _create_vimrc_file(directory: str) -> str:
+    vimrc = resources.files("vik").joinpath(".vimrc").read_text(encoding="utf-8")
+    vimrc_path = os.path.join(directory, ".vimrc")
+    with open(vimrc_path, "w") as f:
+        f.write(vimrc)
+    return vimrc_path
+
+
+def main():
+    with TemporaryDirectory() as tmp_dir:
+        if len(sys.argv) < 2:
+            sys.stderr.write("File not specified\n")
+            sys.exit(2)
+        vimrc_path = _create_vimrc_file(tmp_dir)
+        subprocess.run(["vim", "-u", vimrc_path, sys.argv[1]])
+
+
+if __name__  == "__main__":
+    main()
