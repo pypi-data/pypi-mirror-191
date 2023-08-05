3a5cfcf3defdb0a8a477607477b184c7203aa902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:59 +0000

    Add package strawberry-graphql/0.156.3/strawberry_graphql-0.156.3.tar.gz

---
+++ packages/strawberry-graphql/strawberry_graphql-0.156.3.tar.gz/strawberry_graphql-0.156.3/strawberry/channels/context.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Any, Dict, Optional
+
+if TYPE_CHECKING:
+    from strawberry.channels.handlers.base import ChannelsConsumer
+
+
+@dataclass
+class StrawberryChannelsContext:
+    """
+    A Channels context for GraphQL
+    """
+
+    request: "ChannelsConsumer"
+    connection_params: Optional[Dict[str, Any]] = None
+
+    @property
+    def ws(self):
+        return self.request
