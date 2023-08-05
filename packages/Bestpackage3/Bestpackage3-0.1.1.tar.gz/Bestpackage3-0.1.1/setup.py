62a6fe2a3aef932ac904dcc4c2fb44a766dea71d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:40 +0000

    Add package Bestpackage3/0.1.1/Bestpackage3-0.1.1.tar.gz

---
+++ packages/Bestpackage3/Bestpackage3-0.1.1.tar.gz/Bestpackage3-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Bestpackage3',
+    version='0.1.1',
+    description='To be the                 best',
+    author='Goog',
+    author_email='your_email@example.com',
+    packages=find_packages(),
+)
