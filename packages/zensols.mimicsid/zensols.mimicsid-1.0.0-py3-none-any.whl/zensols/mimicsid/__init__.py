87fd2f8a9099cdd5fc1b3d6b316d2cc24659940f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:30 +0000

    Add package zensols.mimicsid/1.0.0/zensols.mimicsid-1.0.0-py3-none-any.whl

---
+++ packages/zensols.mimicsid/zensols.mimicsid-1.0.0-py3-none-any.whl/zensols/mimicsid/__init__.py
@@ -0,0 +1,9 @@
+"""MIMIC-III corpus parsing and section prediction with MedSecId.
+
+"""
+__author__ = 'Paul Landes'
+
+from .domain import *
+from .anon import *
+from .app import *
+from .cli import *
