d1195304b3043abcb7fe3706f27b4369f8bcb12d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:21 +0000

    Add package sexpdata/0.0.4/sexpdata-0.0.4.tar.gz

---
+++ packages/sexpdata/sexpdata-0.0.4.tar.gz/sexpdata-0.0.4/setup.py
@@ -0,0 +1,40 @@
+import sys
+
+from distutils.core import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+install_requires = []
+if sys.version.split(" ")[0] < "3.4":
+    install_requires.append('singledispatch')
+
+setup(
+    name='sexpdata',
+    version='0.0.4',
+    py_modules=['sexpdata'],
+    author='Joshua D. Boyd, Takafumi Arakaki',
+    author_email='jdboyd@jdboyd.net',
+    url='https://github.com/jd-boyd/sexpdata',
+    license='BSD License',
+    description='S-expression parser for Python',
+    long_description=long_description,
+    keywords='s-expression, lisp, parser',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Lisp",
+        "Programming Language :: Emacs-Lisp",
+        # see: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    install_requires=install_requires,
+)
