1218f3133844929d87e8b4acaf6b35aa339536cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:25 +0000

    Add package novu/1.0.0a2/novu-1.0.0a2.tar.gz

---
+++ packages/novu/novu-1.0.0a2.tar.gz/novu-1.0.0a2/novu/constants.py
@@ -0,0 +1,11 @@
+"""This module is used to gather all constants and magic number used in the Novu client."""
+
+LAYOUTS_ENDPOINT = "/v1/layouts"
+INTEGRATIONS_ENDPOINT = "/v1/integrations"
+EVENTS_ENDPOINT = "/v1/events/trigger"
+NOTIFICATION_GROUPS_ENDPOINT = "/v1/notification-groups"
+SUBSCRIBERS_ENDPOINT = "/v1/subscribers"
+TOPICS_ENDPOINT = "/v1/topics"
+CHANGES_ENDPOINT = "/v1/changes"
+
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f%z"
---
+++ packages/novu/novu-1.0.0a2.tar.gz/novu-1.0.0a2/novu/enums/__init__.py
@@ -0,0 +1,29 @@
+"""This module is used to gather all enumerations defined by Novu in Python format to be reused by developers."""
+
+from novu.enums.change import ChangeKind
+from novu.enums.channel import Channel
+from novu.enums.event import EventStatus
+from novu.enums.provider import (
+    ChatProviderIdEnum,
+    CredentialsKeyEnum,
+    EmailProviderIdEnum,
+    InAppProviderIdEnum,
+    ProviderIdEnum,
+    PushProviderIdEnum,
+    SmsProviderIdEnum,
+)
+from novu.enums.template import TemplateVariableTypeEnum
+
+__all__ = [
+    "ChangeKind",
+    "Channel",
+    "EventStatus",
+    "ChatProviderIdEnum",
+    "CredentialsKeyEnum",
+    "EmailProviderIdEnum",
+    "TemplateVariableTypeEnum",
+    "InAppProviderIdEnum",
+    "PushProviderIdEnum",
+    "SmsProviderIdEnum",
+    "ProviderIdEnum",
+]
