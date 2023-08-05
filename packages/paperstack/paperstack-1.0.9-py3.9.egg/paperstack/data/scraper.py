03c5987b14001e794a081f91a41bacdae896774c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:57 +0000

    Add package paperstack/1.0.9/paperstack-1.0.9-py3.9.egg

---
+++ packages/paperstack/paperstack-1.0.9-py3.9.egg/paperstack/data/scraper.py
@@ -0,0 +1,415 @@
+"Module in charge of web scraping."
+
+from urllib.parse import quote_plus
+from xml.etree import ElementTree
+import re
+
+from datetime import datetime
+
+import requests
+
+import bibtexparser
+
+from paperstack.data.record import record_constructors
+
+
+class Scraper:
+    """Superclass for web scrapers. Children will specialize in their
+    respective databases.
+
+    Parameters
+    ----------
+    record : dict
+        Keep track of scraped information.
+    config : paperstack.filesystem.config.Config
+    messenger : paperstack.interface.message.Messenger
+
+    Attributes
+    ----------
+    SUGGESTED_FIELDS : list
+        Fields potentially needed to scrape information.
+    record : dict
+        Keep track of scraped information.
+    pdf_url : str
+        Keep track of PDF download url after scraping.
+    config : paperstack.filesystem.config.Config
+    messenger : paperstack.interface.message.Messenger
+    """
+
+    SUGGESTED_FIELDS = []
+
+    def __init__(self, record, config, messenger):
+        self.record = record
+        self.pdf_url = None
+
+        self.config = config
+        self.messenger = messenger
+
+
+    @property
+    def suggested_fields(self):
+        "Get suggested fields."
+
+        return self.__class__.SUGGESTED_FIELDS
+
+
+    def scrape(self):
+        """Use select `self.record` entries (e.g., DOI; will throw error if
+        they are not present) to get more data. Populate `self.record` with
+        scraped data.
+        """
+
+        raise NotImplementedError
+
+
+    def download_pdf(self, save_path):
+        """Download PDF from database using scraped `pdf_url` and save to
+        specified path.
+
+        Parameters
+        ----------
+        save_path : str
+        """
+
+        raise NotImplementedError
+
+
+    def create_record(self):
+        """Create `Record` instance based on scraped data.
+
+        Returns
+        -------
+        paperstack.data.record.Record
+        """
+
+        self.scrape()
+
+        if 'record_type' not in self.record:
+            self.messenger.send_error('Record type must be specified to build record.')
+
+        record_type = self.record['record_type']
+
+        if record_type not in record_constructors:
+            self.messenger.send_error('Invalid record type.')
+
+        return record_constructors[record_type](
+            self.record,
+            self.config,
+            self.messenger
+        )
+
+
+    def populate_record(self, record):
+        """Populate `Record` instance using scraped data.
+        """
+
+        self.scrape()
+
+        record.record = {**self.record, **record.record}
+
+
+class ADSScraper(Scraper):
+    """Scraper for the NASA/Harvard Astrophysics Data System. We need an
+    API key for this one, specified in the config under [ads]/key.
+    """
+
+    SUGGESTED_FIELDS = [
+        ('bibcode', 'Bibcode'),
+        ('doi', 'DOI'),
+        ('title', 'Title')
+    ]
+
+    def get_headers(self):
+        "Generate request headers."
+
+        key = self.config.get('ads', 'key')
+
+        if not key:
+            self.messenger.send_error('Specify a valid API key to scrape ADS.')
+
+        return {
+            'Authorization': f'Bearer {key}',
+            'Content-Type': 'application/json'
+        }
+
+
+    def get_metadata(self):
+        """Get bibcode and abstract from DOI or title by communicating with
+        ADS.
+        """
+
+        if 'bibcode' in self.record:
+            bibcode = self.record['bibcode']
+            identifier = f'bibcode:{bibcode}'
+        elif 'doi' in self.record:
+            doi = self.record['doi']
+            identifier = f'doi:{doi}'
+        elif 'title' in self.record:
+            title = self.record['title']
+            identifier = f'title:{title}'
+        else:
+            self.messenger.send_error('Bibcode, DOI, or title is needed to scrape with ADS.')
+
+        identifier = quote_plus(identifier)
+        url = f'https://api.adsabs.harvard.edu/v1/search/query?q={identifier}&fl=bibcode,abstract'
+
+        try:
+            response = requests.get(
+                url,
+                headers = self.get_headers(),
+                timeout = float(self.config.get('ads', 'timeout'))
+            )
+
+            if not response.ok:
+                raise requests.ConnectionError
+        except requests.ConnectionError:
+            self.messenger.send_error('Having trouble connecting to ADS.')
+        except requests.Timeout:
+            self.messenger.send_error('Request timed out while connecting to ADS.')
+
+        data = response.json()['response']['docs']
+
+        if len(data) == 0 or 'bibcode' not in data[0]:
+            self.messenger.send_error('Could not find record on ADS.')
+
+        metadata = {
+            'bibcode': data[0]['bibcode']
+        }
+
+        if 'abstract' in data[0]:
+            metadata['abstract'] = data[0]['abstract']
+
+        return metadata
+
+
+    def scrape(self):
+        metadata = self.get_metadata()
+        identifier = {'bibcode': [metadata['bibcode']]}
+
+        self.pdf_url = 'https://ui.adsabs.harvard.edu/link_gateway/{}/'.format(
+            metadata['bibcode']
+        )
+
+        try:
+            response = requests.post(
+                'https://api.adsabs.harvard.edu/v1/export/bibtex',
+                headers = self.get_headers(),
+                timeout = float(self.config.get('ads', 'timeout')),
+                json = identifier
+            )
+        except requests.ConnectionError:
+            self.messenger.send_error('Having trouble connecting to ADS.')
+        except requests.Timeout:
+            self.messenger.send_error('Request timed out while connecting to ADS.')
+
+        data = bibtexparser.loads(
+            response.json()['export']
+        ).entries[0]
+
+        entries = [
+            ('record_type', 'ENTRYTYPE'),
+            ('author', 'author'),
+            ('title', 'title'),
+            ('journal', 'journal'),
+            ('year', 'year'),
+            ('volume', 'volume'),
+            ('number', 'number'),
+            ('pages', 'pages'),
+            ('month', 'month'),
+            ('doi', 'doi'),
+            ('issn', 'issn'),
+            ('bibnote', 'adsnote'),
+        ]
+
+        record = {
+            'bibcode': metadata['bibcode']
+        }
+
+        for record_key, data_key in entries:
+            if data_key in data:
+                record[record_key] = data[data_key]
+
+        if 'abstract' in metadata:
+            record['abstract'] = metadata['abstract']
+
+        self.record = record
+
+
+    def attempt_get(self, url):
+        """Attempt to get URL.
+
+        Parameters
+        ----------
+        url : str
+        """
+
+        try:
+            response = requests.get(
+                url,
+                timeout = float(self.config.get('ads', 'timeout')),
+            )
+        except requests.ConnectionError:
+            self.messenger.send_warning('Having trouble connecting to ADS.')
+        except requests.Timeout:
+            self.messenger.send_warning('Request timed out while connecting to ADS.')
+
+        return response
+
+
+    def download_pdf(self, save_path):
+        if not self.pdf_url:
+            self.messenger.send_warning('Not enough information to download PDF.')
+            return
+
+        response = self.attempt_get(self.pdf_url + 'PUB_PDF')
+
+        if not response or not response.ok:
+            response = self.attempt_get(self.pdf_url + 'EPRINT_PDF')
+
+        if not response or not response.ok:
+            return
+
+        try:
+            with open(save_path, 'wb') as f:
+                f.write(response.content)
+        except:
+            self.messenger.send_warning('Could not write PDF to data directory.')
+            return
+
+        self.record['path'] = str(save_path)
+
+
+class ArXivScraper(Scraper):
+    """Scraper for the arXiv database.
+    """
+
+    SUGGESTED_FIELDS = [
+        ('arxiv', 'arXiv ID'),
+        ('title', 'Title')
+    ]
+
+    def scrape(self):
+        if 'arxiv' in self.record:
+            arxiv = self.record['arxiv'].strip()
+
+            id_match = re.search(r'[0-9vV]+\.[0-9vV]+$', arxiv)
+
+            if not id_match:
+                self.messenger.send_error('Invalid arXiv ID.')
+
+            arxiv = id_match.group(0)
+
+            identifier = f'id:{arxiv}'
+        elif 'title' in self.record:
+            title = self.record['title']
+            identifier = f'ti:{title}'
+        else:
+            self.messenger.send_error('arXiv ID or title is needed to scrape with arXiv.')
+
+        identifier = quote_plus(identifier)
+        url = f'http://export.arxiv.org/api/query?search_query={identifier}&start=0&max_results=1'
+
+        try:
+            response = requests.get(
+                url,
+                timeout = float(self.config.get('arxiv', 'timeout')),
+                headers = {'Content-Type': 'application/json'}
+            )
+        except requests.ConnectionError:
+            self.messenger.send_error('Having trouble connecting to arXiv.')
+        except requests.Timeout:
+            self.messenger.send_error('Request timed out while connecting to arXiv.')
+
+        tree = ElementTree.fromstring(response.content)
+        entry = tree[-1]
+
+        results_count = int(tree.find(
+            '{http://a9.com/-/spec/opensearch/1.1/}totalResults'
+        ).text)
+
+        if results_count == 0:
+            self.messenger.send_error('No results found matching query.')
+
+        abstract = entry.find('{http://www.w3.org/2005/Atom}summary').text
+        abstract = abstract.strip().replace('\n', ' ').replace('  ', '')
+
+        title = entry.find('{http://www.w3.org/2005/Atom}title').text
+        title = title.strip().replace('\n', ' ').replace('  ', '')
+
+        doi_element = entry.find('{http://arxiv.org/schemas/atom}doi')
+        doi = None
+
+        if doi_element:
+            doi = doi_element.text.strip()
+
+        date = entry.find('{http://www.w3.org/2005/Atom}published').text.strip()
+        date = datetime.strptime(date, '%Y-%m-%dT%H:%M:%SZ')
+
+        comment_tag = entry.find('{http://arxiv.org/schemas/atom}comment')
+        comment = None
+
+        if comment_tag is not None:
+            comment = comment_tag.text.strip().replace('\n', ' ').replace('  ', '')
+
+        authors = []
+
+        for author in entry.findall('{http://www.w3.org/2005/Atom}author'):
+            authors.append(
+                author.find('{http://www.w3.org/2005/Atom}name').text
+            )
+
+        authors = ' and '.join(authors)
+
+        for child in entry:
+            if 'title' in child.attrib and child.attrib['title'] == 'pdf':
+                if 'href' in child.attrib:
+                    self.pdf_url = child.attrib['href']
+                    break
+
+        record = {
+            'record_type': 'article',
+            'author': authors,
+            'title': title,
+            'journal': 'arXiv',
+            'year': str(date.year),
+            'month': str(date.month),
+            'doi': doi
+        }
+
+        if comment is not None:
+            record['bibnote'] = comment
+
+        self.record = record
+
+
+    def download_pdf(self, save_path):
+        if not self.pdf_url:
+            self.messenger.send_warning('Could not download PDF.')
+            return
+
+        try:
+            response = requests.get(
+                self.pdf_url,
+                timeout = float(self.config.get('arxiv', 'timeout')),
+            )
+        except requests.ConnectionError:
+            self.messenger.send_warning('Having trouble connecting to arXiv.')
+            return
+        except requests.Timeout:
+            self.messenger.send_warning('Request timed out while connecting to arXiv.')
+            return
+
+        try:
+            with open(save_path, 'wb') as f:
+                f.write(response.content)
+        except:
+            self.messenger.send_warning('Could not write PDF to data directory.')
+            return
+
+        self.record['path'] = str(save_path)
+
+
+scraper_constructors = {
+    'ads': ADSScraper,
+    'arxiv': ArXivScraper
+}
---
+++ packages/paperstack/paperstack-1.0.9-py3.9.egg/paperstack/interface/details.py
@@ -0,0 +1,269 @@
+"Module reponsible for the details panel."
+
+import urwid as u
+
+from paperstack.interface.keymap import Keymap
+from paperstack.interface.util import clean_text
+
+
+class EntryElement(u.WidgetWrap):
+    """The widget corresponding to each entry in details.
+
+    Parameters
+    ----------
+    field_name : str
+    name : str
+    value : any
+    keymap : paperstack.interface.keymap.Keymap
+    """
+
+    def __init__ (self, field_name, name, value, keymap):
+        self.content = (field_name, name)
+
+        self.keymap = keymap
+
+        text = clean_text(value)
+
+        text = [
+            ('entry_name', f'{name}: '), clean_text(value)
+        ]
+
+        self.text = u.Text(text)
+
+        self.text_wrapper = u.AttrWrap(
+            self.text,
+            'entry',
+            'entry_selected'
+        )
+
+        super().__init__(u.Padding(self.text_wrapper, 'center', ('relative', 90)))
+
+
+    def keypress(self, size, key):
+        "Handle keypresses."
+
+        self.keymap.trigger(key)
+
+
+    def selectable(self):
+        return True
+
+
+class EntrySeparator(u.WidgetWrap):
+    "The widget corresponding to entry separators in details."
+
+    def __init__ (self):
+        self.content = ''
+
+        super().__init__(u.Text(''))
+
+
+    def keypress(self, size, key):
+        "Handle keypresses."
+
+        return key
+
+
+    def selectable(self):
+        return False
+
+
+class DetailView(u.WidgetWrap):
+    """Right panel, responsible for displaying details the selected record.
+
+    Parameters
+    ----------
+    messenger : paperstack.interface.message.AppMessenger
+    library : paperstack.data.library.Library
+    global_keymap : paperstack.interface.keymap.Keymap
+    vim_keys : bool
+
+    Attributes
+    ----------
+    messenger : paperstack.interface.message.AppMessenger
+    library : paperstack.data.library.Library
+    keymap : paperstack.interface.keymap.Keymap
+    has_focus : bool
+    record : paperstack.data.record.Record
+    """
+
+    def __init__(self, messenger, library, global_keymap, vim_keys):
+        self.messenger = messenger
+        self.library = library
+        self.keymap = Keymap(messenger, global_keymap)
+
+        self.has_focus = False
+
+        self.record = None
+
+        if vim_keys:
+            self.keymap.bind('h', 'Focus list', self.focus_list)
+            self.keymap.bind('j', 'Next', self.focus_next)
+            self.keymap.bind('k', 'Previous', self.focus_previous)
+        else:
+            self.keymap.bind('left', 'Focus list', self.focus_list)
+            self.keymap.bind('down', 'Next', self.focus_next)
+            self.keymap.bind('up', 'Previous', self.focus_previous)
+
+        self.keymap.bind('e', 'Edit entry', self.edit_entry)
+
+        self.keymap.bind('g', 'First', self.focus_first)
+        self.keymap.bind('G', 'Last', self.focus_last)
+
+        u.register_signal(self.__class__, ['focus_list'])
+
+        self.walker = u.SimpleFocusListWalker([])
+
+        list_box = u.ListBox(self.walker)
+        super().__init__(list_box)
+
+
+    def modified(self):
+        "When focus is modified, send signal."
+
+        if self.has_focus:
+            widget, _ = self.walker.get_focus()
+
+            self.keymap.show_hints()
+
+
+    def focus_list(self):
+        "Move focus to list panel."
+
+        u.emit_signal(self, 'focus_list')
+
+
+    def focus_previous(self):
+        "Move focus to previous entry."
+
+        try:
+            while True:
+                previous_index = self.walker.prev_position(
+                    self.walker.get_focus()[1]
+                )
+
+                self.walker.set_focus(previous_index)
+
+                if self.walker.get_focus()[0].selectable():
+                    break
+        except IndexError:
+            pass
+
+
+    def focus_next(self):
+        "Move focus to next entry."
+
+        try:
+            while True:
+                next_index = self.walker.next_position(
+                    self.walker.get_focus()[1]
+                )
+
+                self.walker.set_focus(next_index)
+
+                if self.walker.get_focus()[0].selectable():
+                    break
+        except IndexError:
+            pass
+
+
+    def focus_first(self):
+        "Move focus to first entry."
+
+        try:
+            self.walker.set_focus(0)
+        except IndexError:
+            pass
+
+
+    def focus_last(self):
+        "Move focus to last entry."
+
+        try:
+            self.walker.set_focus(len(self.walker) - 1)
+        except IndexError:
+            pass
+
+
+    def set_record(self, record):
+        """Set the current record being viewed.
+
+        Parameters
+        ----------
+        record : paperstack.data.record.Record
+        """
+
+        self.record = record
+
+        if record is None:
+            while len(self.walker) > 0:
+                self.walker.pop()
+            return
+
+        widgets = []
+
+        first_entry = True
+
+        for key, name, *_ in record.requirements:
+            if key in record.record and record.record[key]:
+                value = record.record[key]
+            else:
+                value = ''
+
+            if not first_entry:
+                widgets.append(EntrySeparator())
+
+            first_entry = False
+
+            widgets.append(EntryElement(
+                key,
+                name,
+                value,
+                self.keymap
+            ))
+
+        u.disconnect_signal(self.walker, 'modified', self.modified)
+
+        while len(self.walker) > 0:
+            self.walker.pop()
+
+        self.walker.extend(widgets)
+
+        u.connect_signal(self.walker, 'modified', self.modified)
+
+        self.walker.set_focus(0)
+
+
+    def edit_entry(self):
+        "Edit currently selected entry."
+
+        widget, index = self.walker.get_focus()
+
+        field_name, name = widget.content
+
+        def commit_edit(text):
+            self.record.record[field_name] = text
+
+            record_id = self.record.record['record_id']
+
+            self.library.update(record_id, {
+                field_name: text
+            })
+            self.library.commit()
+
+            widget.text.set_text([
+                ('entry_name', f'{name}: '), clean_text(text)
+            ])
+
+            self.messenger.send_success('Edited entry.')
+
+        if field_name in self.record.record and self.record.record[field_name]:
+            value = self.record.record[field_name]
+        else:
+            value = ''
+
+        self.messenger.ask_input(
+            f'{name}: ',
+            value,
+            commit_edit
+        )
