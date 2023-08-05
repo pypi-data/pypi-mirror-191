9610e849f0ec9471e494266daf4333c4514f19e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:18 +0000

    Add package vkid/0.1/vkid-0.1.tar.gz

---
+++ packages/vkid/vkid-0.1.tar.gz/vkid-0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="vkid",
+    version="0.1",
+    author="",
+    author_email="",
+    description="VK ID API",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
