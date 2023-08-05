883485529e85e7d0619cefab6001ecc35dc4d628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:38 +0000

    Add package csv-detective/0.6.1.dev331/csv_detective-0.6.1.dev331-py3-none-any.whl

---
+++ packages/csv-detective/csv_detective-0.6.1.dev331-py3-none-any.whl/csv_detective/detect_fields/FR/geo/code_postal/__init__.py
@@ -0,0 +1,17 @@
+from os.path import dirname, join
+import re
+
+PROPORTION = 0.9
+f = open(join(dirname(__file__), 'code_postal.txt'), 'r')
+codes_postal = f.read().split('\n')
+f.close()
+
+
+def _is(val):
+    '''Renvoie True si val peut être un code postal, False sinon'''
+
+    regex = r'^[0-9]{5}$'
+    if not bool(re.match(regex, val)):
+        return False
+
+    return val in codes_postal
