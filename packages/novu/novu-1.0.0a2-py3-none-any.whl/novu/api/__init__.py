8e8983e8f00893c97f80fc4ab98b5650fca8ca12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:25 +0000

    Add package novu/1.0.0a2/novu-1.0.0a2-py3-none-any.whl

---
+++ packages/novu/novu-1.0.0a2-py3-none-any.whl/novu/api/__init__.py
@@ -0,0 +1,21 @@
+"""This module is used to gather all python wrappers used to describe resources in Novu API.
+
+In this SDK, we choose to split the Novu API by business resource to simplify its complexity.
+"""
+from novu.api.change import ChangeApi
+from novu.api.event import EventApi
+from novu.api.integration import IntegrationApi
+from novu.api.layout import LayoutApi
+from novu.api.notification_group import NotificationGroupApi
+from novu.api.subscriber import SubscriberApi
+from novu.api.topic import TopicApi
+
+__all__ = [
+    "ChangeApi",
+    "EventApi",
+    "IntegrationApi",
+    "LayoutApi",
+    "NotificationGroupApi",
+    "SubscriberApi",
+    "TopicApi",
+]
