bb94d45204ed0ded8000c69fc6d215a7df30941a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:28 +0000

    Add package Blooker/0.0.2/Blooker-0.0.2.tar.gz

---
+++ packages/Blooker/Blooker-0.0.2.tar.gz/Blooker-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+setup(
+    name="Blooker",
+    version="0.0.2",
+    author="Biggie",
+    author_email="thugshaker@yahoo.com",
+    description="Flood Blooket Games",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bigboy-baby/",
+    packages=find_packages(),
+    install_requires=['threading','requests','websocket'],
+    python_requires='>=3',
+)
