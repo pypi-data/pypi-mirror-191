e84c1f484947c61df6baf993cb8bab1e0634b9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:21 +0000

    Add package nleval/0.1.0.dev5/nleval-0.1.0.dev5-py3-none-any.whl

---
+++ packages/nleval/nleval-0.1.0.dev5-py3-none-any.whl/nleval/data/annotated_ontology/__init__.py
@@ -0,0 +1,10 @@
+"""Annotated ontology data."""
+from nleval.data.annotated_ontology.disgenet import DisGeNET
+from nleval.data.annotated_ontology.go import GOBP, GOCC, GOMF
+
+__all__ = [
+    "DisGeNET",
+    "GOBP",
+    "GOCC",
+    "GOMF",
+]
