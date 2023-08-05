f82a2935ba038966867629f84591d69e6ed95a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:28 +0000

    Add package Wrapper-Weather-Module-V3/0.0.1/Wrapper_Weather_Module_V3-0.0.1.tar.gz

---
+++ packages/Wrapper-Weather-Module-V3/Wrapper_Weather_Module_V3-0.0.1.tar.gz/Wrapper_Weather_Module_V3-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+    
+setup(
+    name='Wrapper_Weather_Module_V3',
+    version='0.0.1',
+    packages=find_packages(exclude=['Tests*']),
+    license='MIT',
+    description='Provides meaningful insights about weather forecast',
+    url='https://github.com/vkyal/DATA534_ProjectAPI',
+    author='Ujjwal, Varshita, Nowshabha',
+    author_email='upadhyay.ujjwal007@gmail.com',
+    python_requires='>=3.9',
+)
