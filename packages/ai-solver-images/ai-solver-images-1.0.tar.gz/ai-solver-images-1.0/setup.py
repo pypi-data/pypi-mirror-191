fbf19dd24cb8621f95334458735d17574a45bb46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:19 +0000

    Add package ai-solver-images/1.0/ai-solver-images-1.0.tar.gz

---
+++ packages/ai-solver-images/ai-solver-images-1.0.tar.gz/ai-solver-images-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+
+setup(
+
+    name='ai-solver-images',
+    packages=['ai-solver-images'],
+    version='1.0',
+    license='MIT',
+    description='Ai Solver',
+    author='helper',
+    keywords=['style'],
+    install_requires=[''],
+    classifiers=['Development Status :: 5 - Production/Stable']
+
+)
+
+
