77bb35a157da64542f6e34b6f96a602597d0bbb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:17 +0000

    Add package pyareas/0.0.3/pyareas-0.0.3.tar.gz

---
+++ packages/pyareas/pyareas-0.0.3.tar.gz/pyareas-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3' 
+DESCRIPTION = 'Python package for Areas'
+LONG_DESCRIPTION = 'Python package to help get the areas of 2D shapes'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="pyareas", 
+        version=VERSION,
+        author="TomTheCodingGuy",
+        author_email="tomb80940@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'pyareas'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
