fe1e2d7e208f37e6c56e76e369abbe023b124fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:17 +0000

    Add package csv-detective/0.6.0/csv_detective-0.6.0-py3-none-any.whl

---
+++ packages/csv-detective/csv_detective-0.6.0-py3-none-any.whl/csv_detective/detect_fields/FR/other/insee_ape700/__init__.py
@@ -0,0 +1,13 @@
+from os.path import dirname, join
+from csv_detective.process_text import _process_text
+
+PROPORTION = 1
+f = open(join(dirname(__file__), 'insee_ape700.txt'), 'r')
+condes_insee_ape = f.read().split('\n')
+f.close()
+
+
+def _is(val):
+    '''Repère les codes APE700 de l'INSEE'''
+    val = _process_text(val).upper()
+    return val in condes_insee_ape
