a0bd69825c45caaf8a5c71d873df485aa8544068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:11 +0000

    Add package mmselfsup/1.0.0rc6/mmselfsup-1.0.0rc6.tar.gz

---
+++ packages/mmselfsup/mmselfsup-1.0.0rc6.tar.gz/mmselfsup-1.0.0rc6/mmselfsup/.mim/configs/benchmarks/classification/knn_imagenet.py
@@ -0,0 +1,39 @@
+dataset_type = 'mmcls.ImageNet'
+data_root = 'data/imagenet'
+file_client_args = dict(backend='disk')
+
+extract_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='mmcls.ResizeEdge', scale=256, edge='short'),
+    dict(type='CenterCrop', crop_size=224),
+    dict(type='PackSelfSupInputs'),
+]
+
+train_dataloader = dict(
+    batch_size=256,
+    num_workers=8,
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='meta/train.txt',
+        data_prefix='train',
+        pipeline=extract_pipeline),
+    collate_fn=dict(type='default_collate'),
+    sampler=dict(type='DefaultSampler', shuffle=False),
+)
+
+val_dataloader = dict(
+    batch_size=256,
+    num_workers=8,
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file='meta/val.txt',
+        data_prefix='val',
+        pipeline=extract_pipeline),
+    collate_fn=dict(type='default_collate'),
+    sampler=dict(type='DefaultSampler', shuffle=False),
+)
+
+# pooling cfg
+pool_cfg = dict(type='AvgPool2d')
