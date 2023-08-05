634fb307e662297e4b12583583ae9e341fb761ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:05:54 +0000

    Add package rustylib/1.5.7/rustylib-1.5.7.tar.gz

---
+++ packages/rustylib/rustylib-1.5.7.tar.gz/rustylib-1.5.7/src/rustylib/__init__.py
@@ -0,0 +1,20 @@
+from Rrandom import Random
+from Rmath import Math
+from Rcolor import Fore
+from Rusty import Rusty, Webhook, Rplus
+from Rrequests import Request
+from Rsugaku import Sugaku
+
+import Rrequests
+import Rrandom
+import Rmath
+import Rcolor
+import Rusty
+import Rsugaku
+
+from Rrandom import *
+from Rmath import *
+from Rcolor import *
+from Rusty import *
+from Rrequests import *
+from Rsugaku import *
