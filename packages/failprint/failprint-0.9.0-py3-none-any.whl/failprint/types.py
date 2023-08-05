2a171729a22293e9d2817c9018640f56b1b8e310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:37 +0000

    Add package failprint/0.9.0/failprint-0.9.0-py3-none-any.whl

---
+++ packages/failprint/failprint-0.9.0-py3-none-any.whl/failprint/types.py
@@ -0,0 +1,14 @@
+"""
+Special types.
+
+Attributes:
+    CmdType: Type for a command.
+    CmdFuncType: Type for a command or function.
+"""
+
+from __future__ import annotations
+
+from typing import Callable, List, Union
+
+CmdType = Union[str, List[str]]  # noqa: E1136 (bug on Python 3.9)
+CmdFuncType = Union[CmdType, Callable]  # noqa: E1136 (bug on Python 3.9)
