f20f759b689527fd83ebd2412b3d68547a493301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:11 +0000

    Add package bluemist-ai/0.1.1.8/bluemist-ai-0.1.1.8.tar.gz

---
+++ packages/bluemist-ai/bluemist-ai-0.1.1.8.tar.gz/bluemist-ai-0.1.1.8/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as file:
+    required_packages = file.read().splitlines()
+
+setup(
+    name='bluemist-ai',
+    long_description='Low code python library',
+    version='0.1.1.8',
+    license='MIT',
+    author="Shashank Agrawal",
+    author_email='shashanka89@gmail.com',
+    packages=find_packages('bluemist'),
+    package_dir={'': 'bluemist'},
+    package_data={'': ['../requirements.txt']},
+    url='https://github.com/shashanka89/bluemist-ai',
+    install_requires=required_packages
+)
