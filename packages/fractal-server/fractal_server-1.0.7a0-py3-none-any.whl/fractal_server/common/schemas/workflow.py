93bf0d85cb3561fa603f4641ca0a28dc9d288554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:04 +0000

    Add package fractal-server/1.0.7a0/fractal_server-1.0.7a0-py3-none-any.whl

---
+++ packages/fractal-server/fractal_server-1.0.7a0-py3-none-any.whl/fractal_server/common/schemas/workflow.py
@@ -0,0 +1,93 @@
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+
+from pydantic import validator
+from sqlmodel import SQLModel
+
+from .task import TaskExport
+from .task import TaskImport
+from .task import TaskRead
+
+
+__all__ = (
+    "WorkflowCreate",
+    "WorkflowRead",
+    "WorkflowUpdate",
+    "WorkflowImport",
+    "WorkflowExport",
+    "WorkflowTaskCreate",
+    "WorkflowTaskImport",
+    "WorkflowTaskExport",
+    "WorkflowTaskRead",
+    "WorkflowTaskUpdate",
+)
+
+
+class _WorkflowTaskBase(SQLModel):
+    order: Optional[int]
+    meta: Optional[Dict[str, Any]] = None
+    args: Optional[Dict[str, Any]] = None
+
+
+class WorkflowTaskCreate(_WorkflowTaskBase):
+    task_id: int
+    workflow_id: Optional[int]  # FIXME check optional
+
+
+class WorkflowTaskRead(_WorkflowTaskBase):
+    id: int
+    workflow_id: int
+    task: TaskRead
+    task_id: Optional[int]  # FIXME make required
+
+
+class WorkflowTaskImport(_WorkflowTaskBase):
+    task: TaskImport
+
+
+class WorkflowTaskExport(_WorkflowTaskBase):
+    task: TaskExport
+
+
+class WorkflowTaskUpdate(_WorkflowTaskBase):
+    args: Optional[Dict[str, Any]]  # type: ignore
+    meta: Optional[Dict[str, Any]]  # type: ignore
+    workflow_id: Optional[int]  # FIXME remove later
+    task_id: Optional[int]  # FIXME remove later
+
+    @validator("meta")
+    def check_no_parallelisation_level(cls, m):
+        if "parallelization_level" in m:
+            raise ValueError(
+                "Overriding task parallelization level currently not allowed"
+            )
+        return m
+
+
+class _WorkflowBase(SQLModel):
+    name: str
+
+
+class WorkflowRead(_WorkflowBase):
+    id: int
+    project_id: int
+    task_list: List[WorkflowTaskRead]
+
+
+class WorkflowCreate(_WorkflowBase):
+    project_id: int
+
+
+class WorkflowUpdate(_WorkflowBase):
+    name: Optional[str]  # type: ignore
+    project_id: Optional[int]  # type: ignore
+
+
+class WorkflowImport(_WorkflowBase):
+    task_list: List[WorkflowTaskImport]
+
+
+class WorkflowExport(_WorkflowBase):
+    task_list: List[WorkflowTaskExport]
