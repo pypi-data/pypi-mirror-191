8737ae8ddea49d99d0d3c3ed9424aad9fce9b735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:24 +0000

    Add package silver-platter/0.5.7/silver_platter-0.5.7-py3-none-any.whl

---
+++ packages/silver-platter/silver_platter-0.5.7-py3-none-any.whl/silver_platter/apply.py
@@ -0,0 +1,258 @@
+#!/usr/bin/python
+# Copyright (C) 2021 Jelmer Vernooij <jelmer@jelmer.uk>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import json
+import logging
+import os
+import subprocess
+import sys
+import tempfile
+from contextlib import suppress
+from dataclasses import dataclass, field
+from typing import BinaryIO, Dict, List, Optional, Tuple, Union
+
+from breezy.commit import PointlessCommit
+from breezy.revision import RevisionID
+from breezy.workingtree import WorkingTree
+from breezy.workspace import check_clean_tree, reset_tree
+
+
+class ScriptMadeNoChanges(Exception):
+    "Script made no changes."
+
+
+class ScriptFailed(Exception):
+    """Script failed to run."""
+
+
+class ScriptNotFound(Exception):
+    """Script to run was not found."""
+
+
+class DetailedFailure(Exception):
+    """Detailed failure"""
+
+    def __init__(self, result_code, description, stage=None, details=None):
+        self.result_code = result_code
+        self.description = description
+        self.stage = stage
+        self.details = details
+
+    @classmethod
+    def from_json(cls, json):
+        return cls(
+            result_code=json.get('result_code'),
+            description=json.get('description'),
+            stage=tuple(json['stage']) if json.get('stage') else None,
+            details=json.get('details'))
+
+
+class ResultFileFormatError(Exception):
+    """The result file was invalid."""
+
+    def __init__(self, inner_error):
+        self.inner_error = inner_error
+
+
+@dataclass
+class CommandResult:
+
+    description: Optional[str] = None
+    value: Optional[int] = None
+    serialized_context: Optional[str] = None
+    context: Dict[str, str] = field(default_factory=dict)
+    tags: List[Tuple[str, RevisionID]] = field(default_factory=list)
+    old_revision: Optional[RevisionID] = None
+    new_revision: Optional[RevisionID] = None
+    target_branch_url: Optional[str] = None
+
+    @classmethod
+    def from_json(cls, data):
+        if 'tags' in data:
+            tags = []
+            for name, revid in data['tags']:
+                tags.append((name, revid.encode('utf-8')))
+        else:
+            tags = None
+        return cls(
+            value=data.get('value', None),
+            context=data.get('context', {}),
+            description=data.get('description'),
+            serialized_context=data.get('serialized_context', None),
+            target_branch_url=data.get('target-branch-url', None),
+            tags=tags)
+
+
+def script_runner(  # noqa: C901
+    local_tree: WorkingTree, script: Union[str, List[str]],
+    commit_pending: Optional[bool] = None,
+    resume_metadata=None, subpath: str = '', committer: Optional[str] = None,
+    extra_env: Optional[Dict[str, str]] = None,
+    stderr: Optional[BinaryIO] = None
+) -> CommandResult:  # noqa: C901
+    """Run a script in a tree and commit the result.
+
+    This ignores newly added files.
+
+    Args:
+      local_tree: Local tree to run script in
+      script: Script to run
+      commit_pending: Whether to commit pending changes
+        (True, False or None: only commit if there were no commits by the
+         script)
+    """
+    env = dict(os.environ)
+    if extra_env:
+        env.update(extra_env)
+    env['SVP_API'] = '1'
+    last_revision = local_tree.last_revision()
+    orig_tags = local_tree.branch.tags.get_tag_dict()
+    with tempfile.TemporaryDirectory() as td:
+        env['SVP_RESULT'] = os.path.join(td, 'result.json')
+        if resume_metadata:
+            env['SVP_RESUME'] = os.path.join(td, 'resume-metadata.json')
+            with open(env['SVP_RESUME'], 'w') as f:
+                json.dump(resume_metadata, f)
+        try:
+            p = subprocess.Popen(
+                script, cwd=local_tree.abspath(subpath),
+                stdout=subprocess.PIPE,
+                shell=isinstance(script, str),
+                stderr=stderr, env=env)
+        except FileNotFoundError as e:
+            raise ScriptNotFound(script) from e
+        (description_encoded, err) = p.communicate(b"")
+        try:
+            with open(env['SVP_RESULT']) as f:
+                try:
+                    result_json = json.load(f)
+                except json.decoder.JSONDecodeError as e:
+                    raise ResultFileFormatError(e)
+        except FileNotFoundError:
+            result_json = None
+        if p.returncode != 0:
+            if result_json is not None:
+                raise DetailedFailure.from_json(result_json)
+            raise ScriptFailed(script, p.returncode)
+        if result_json is not None:
+            result = CommandResult.from_json(result_json)
+        else:
+            result = CommandResult()
+    if not result.description:
+        result.description = description_encoded.decode()
+    new_revision = local_tree.last_revision()
+    if result.tags is None:
+        result.tags = []
+        for name, revid in local_tree.branch.tags.get_tag_dict().items():
+            if orig_tags.get(name) != revid:
+                result.tags.append((name, revid))
+    if last_revision == new_revision and commit_pending is None:
+        # Automatically commit pending changes if the script did not
+        # touch the branch.
+        commit_pending = True
+    if commit_pending:
+        local_tree.smart_add([local_tree.abspath(subpath)])
+        with suppress(PointlessCommit):
+            new_revision = local_tree.commit(
+                result.description, allow_pointless=False,
+                committer=committer)
+    if new_revision == last_revision:
+        raise ScriptMadeNoChanges()
+    result.old_revision = last_revision
+    result.new_revision = local_tree.last_revision()
+    return result
+
+
+def main(argv: List[str]) -> Optional[int]:  # noqa: C901
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "command", help="Path to script to run.", type=str,
+        nargs='?')
+    parser.add_argument(
+        "--diff", action="store_true", help="Show diff of generated changes."
+    )
+    parser.add_argument(
+        "--commit-pending",
+        help="Commit pending changes after script.",
+        choices=["yes", "no", "auto"],
+        default=None,
+        type=str,
+    )
+    parser.add_argument(
+        "--verify-command", type=str, help="Command to run to verify changes."
+    )
+    parser.add_argument(
+        "--recipe", type=str, help="Recipe to use.")
+    args = parser.parse_args(argv)
+
+    if args.recipe:
+        from .recipe import Recipe
+        recipe = Recipe.from_path(args.recipe)
+    else:
+        recipe = None
+
+    if args.commit_pending:
+        commit_pending = {
+            "auto": None, "yes": True, "no": False}[args.commit_pending]
+    elif recipe:
+        commit_pending = recipe.commit_pending
+    else:
+        commit_pending = None
+
+    if args.command:
+        command = args.command
+    elif recipe.command:
+        command = recipe.command
+    else:
+        parser.error('No command specified.')
+
+    local_tree, subpath = WorkingTree.open_containing('.')
+
+    check_clean_tree(local_tree)
+
+    try:
+        result = script_runner(
+            local_tree, script=command, commit_pending=commit_pending,
+            subpath=subpath)
+
+        if result.description:
+            logging.info('Succeeded: %s', result.description)
+
+        if args.verify_command:
+            try:
+                subprocess.check_call(
+                    args.verify_command, shell=True,
+                    cwd=local_tree.abspath(subpath)
+                )
+            except subprocess.CalledProcessError:
+                parser.error("Verify command failed.")
+    except Exception:
+        reset_tree(local_tree, subpath=subpath)
+        raise
+
+    if args.diff:
+        from breezy.diff import show_diff_trees
+        old_tree = local_tree.revision_tree(result.old_revision)
+        new_tree = local_tree.revision_tree(result.new_revision)
+        show_diff_trees(
+            old_tree,
+            new_tree,
+            sys.stdout.buffer,
+            old_label='old/',
+            new_label='new/')
+    return 0
---
+++ packages/silver-platter/silver_platter-0.5.7-py3-none-any.whl/silver_platter/recipe.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+# Copyright (C) 2021 Jelmer Vernooij <jelmer@jelmer.uk>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import os
+from dataclasses import dataclass
+from typing import Dict, List, Optional, Union
+
+import yaml
+from jinja2 import Template
+
+
+@dataclass
+class Recipe:
+    """Recipe to use."""
+
+    name: str
+    command: Union[str, List[str]]
+    merge_request_description_template: Dict[Optional[str], Template]
+    merge_request_commit_message_template: Template
+    merge_request_title_template: Template
+    resume: bool = False
+    commit_pending: Optional[bool] = True
+    propose_threshold: Optional[int] = None
+    mode: Optional[str] = None
+    labels: Optional[List[str]] = None
+
+    def to_yaml(self):
+        ret = {}
+        if self.mode:
+            ret['mode'] = self.mode
+        if self.labels:
+            ret['labels'] = self.labels
+        if (self.merge_request_description_template
+                or self.merge_request_commit_message_template
+                or self.merge_request_title_template
+                or self.propose_threshold is not None):
+            ret['merge-request'] = {}
+            if self.merge_request_description_template:
+                ret['merge-request']['description'] = (
+                    self.merge_request_description_template)
+            if self.merge_request_commit_message_template:
+                ret['merge-request']['commit-message'] = (
+                    self.merge_request_commit_message_template)
+            if self.merge_request_title_template:
+                ret['merge-request']['title'] = (
+                    self.merge_request_title_template)
+            if self.propose_threshold is not None:
+                ret['merge-request']['propose-threshold'] = (
+                    self.propose_threshold)
+        if self.command:
+            ret['command'] = self.command
+        if self.name:
+            ret['name'] = self.name
+        if self.commit_pending is not None:
+            ret['commit-pending'] = self.commit_pending
+        if self.resume is not None:
+            ret['resume'] = self.resume
+        return ret
+
+    @classmethod
+    def from_yaml(cls, d):
+        merge_request = d.get('merge-request', {})
+        if merge_request:
+            description = merge_request.get('description', {})
+            if isinstance(description, dict):
+                merge_request_description_template = description
+            else:
+                merge_request_description_template = {None: description}
+            merge_request_commit_message_template = merge_request.get(
+                'commit-message')
+            merge_request_title_template = merge_request.get(
+                'title')
+            propose_threshold = merge_request.get('propose-threshold')
+        else:
+            merge_request_description_template = {}
+            merge_request_commit_message_template = None
+            merge_request_title_template = None
+            propose_threshold = None
+        return cls(
+            name=d.get('name'),
+            labels=d.get('labels', []),
+            command=d.get('command'),
+            mode=d.get('mode'),
+            resume=d.get('resume', False),
+            commit_pending=d.get('commit-pending'),
+            merge_request_description_template=(
+                merge_request_description_template),
+            merge_request_commit_message_template=(
+                merge_request_commit_message_template),
+            merge_request_title_template=(
+                merge_request_title_template),
+            propose_threshold=propose_threshold)
+
+    def render_merge_request_commit_message(self, context):
+        template = self.merge_request_commit_message_template
+        if template:
+            return Template(template).render(context)
+        return None
+
+    def render_merge_request_title(self, context):
+        template = self.merge_request_title_template
+        if template:
+            return Template(template).render(context)
+        return None
+
+    def render_merge_request_description(self, description_format, context):
+        template = self.merge_request_description_template.get(
+                description_format)
+        if template is None:
+            try:
+                template = self.merge_request_description_template[None]
+            except KeyError:
+                return None
+        return Template(template).render(context)
+
+    @classmethod
+    def from_path(cls, path):
+        with open(path) as f:
+            ret = cls.from_yaml(yaml.full_load(f))
+            if not ret.name:
+                ret.name = os.path.splitext(os.path.basename(path))[0]
+            return ret
