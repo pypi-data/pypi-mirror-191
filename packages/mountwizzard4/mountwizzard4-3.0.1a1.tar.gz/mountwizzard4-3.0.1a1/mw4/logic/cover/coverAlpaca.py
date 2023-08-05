ca547f495065ddb7300b4ecdf0e7defdc69617f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:54 +0000

    Add package mountwizzard4/3.0.1a1/mountwizzard4-3.0.1a1.tar.gz

---
+++ packages/mountwizzard4/mountwizzard4-3.0.1a1.tar.gz/mountwizzard4-3.0.1a1/mw4/logic/cover/coverAlpaca.py
@@ -0,0 +1,110 @@
+############################################################
+# -*- coding: utf-8 -*-
+#
+#       #   #  #   #   #    #
+#      ##  ##  #  ##  #    #
+#     # # # #  # # # #    #  #
+#    #  ##  #  ##  ##    ######
+#   #   #   #  #   #       #
+#
+# Python-based Tool for interaction with the 10micron mounts
+# GUI with PyQT5 for python
+#
+# written in python3, (c) 2019-2023 by mworion
+# Licence APL2.0
+#
+###########################################################
+# standard libraries
+
+# external packages
+
+# local imports
+from base.alpacaClass import AlpacaClass
+
+
+class CoverAlpaca(AlpacaClass):
+    """
+    """
+
+    __all__ = ['CoverAlpaca']
+
+    def __init__(self, app=None, signals=None, data=None):
+        super().__init__(app=app, data=data)
+
+        self.alpacaSignals = signals
+        self.data = data
+
+    def workerPollData(self):
+        """
+        :return: true for test purpose
+        """
+        states = ['NotPresent', 'Closed', 'Moving', 'Open', 'Unknown', 'Error']
+        if not self.deviceConnected:
+            return False
+
+        state = self.getAlpacaProperty('coverstate')
+        stateText = states[state]
+        self.storePropertyToData(stateText, 'Status.Cover')
+        return True
+
+    def closeCover(self):
+        """
+        :return: true for test purpose
+        """
+        if not self.deviceConnected:
+            return False
+
+        self.getAlpacaProperty('closecover')
+        return True
+
+    def openCover(self):
+        """
+        :return: true for test purpose
+        """
+        if not self.deviceConnected:
+            return False
+
+        self.getAlpacaProperty('opencover')
+        return True
+
+    def haltCover(self):
+        """
+        :return: true for test purpose
+        """
+        if not self.deviceConnected:
+            return False
+
+        self.getAlpacaProperty('haltcover')
+        return True
+
+    def lightOn(self):
+        """
+        :return:
+        """
+        if not self.deviceConnected:
+            return False
+
+        brightness = self.getAlpacaProperty('brightness')
+        self.setAlpacaProperty('calibratoron', Brightness=brightness)
+        return True
+
+    def lightOff(self):
+        """
+        :return:
+        """
+        if not self.deviceConnected:
+            return False
+
+        self.getAlpacaProperty('calibratoroff')
+        return True
+
+    def lightIntensity(self, value):
+        """
+        :param value:
+        :return:
+        """
+        if not self.deviceConnected:
+            return False
+
+        self.setAlpacaProperty('calibratoron', Brightness=value)
+        return True
---
+++ packages/mountwizzard4/mountwizzard4-3.0.1a1.tar.gz/mountwizzard4-3.0.1a1/mw4/logic/cover/coverAscom.py
@@ -0,0 +1,106 @@
+############################################################
+# -*- coding: utf-8 -*-
+#
+#       #   #  #   #   #    #
+#      ##  ##  #  ##  #    #
+#     # # # #  # # # #    #  #
+#    #  ##  #  ##  ##    ######
+#   #   #   #  #   #       #
+#
+# Python-based Tool for interaction with the 10micron mounts
+# GUI with PyQT5 for python
+#
+# written in python3, (c) 2019-2023 by mworion
+# Licence APL2.0
+#
+###########################################################
+# standard libraries
+
+# external packages
+
+# local imports
+from base.ascomClass import AscomClass
+
+
+class CoverAscom(AscomClass):
+    """
+    """
+
+    __all__ = ['CoverAscom']
+    coverStates = ['NotPresent', 'Closed', 'Moving', 'Open', 'Unknown', 'Error']
+
+    def __init__(self, app=None, signals=None, data=None):
+        super().__init__(app=app, data=data)
+
+        self.signals = signals
+        self.data = data
+
+    def workerPollData(self):
+        """
+        :return: true for test purpose
+        """
+        state = self.getAscomProperty('CoverState')
+        stateText = self.coverStates[state]
+        self.storePropertyToData(stateText, 'Status.Cover')
+        return True
+
+    def closeCover(self):
+        """
+        :return: true for test purpose
+        """
+        if not self.deviceConnected:
+            return False
+
+        self.callMethodThreaded(self.client.CloseCover)
+        return True
+
+    def openCover(self):
+        """
+        :return: true for test purpose
+        """
+        if not self.deviceConnected:
+            return False
+
+        self.callMethodThreaded(self.client.OpenCover)
+        return True
+
+    def haltCover(self):
+        """
+        :return: true for test purpose
+        """
+        if not self.deviceConnected:
+            return False
+
+        self.callMethodThreaded(self.client.HaltCover)
+        return True
+
+    def lightOn(self):
+        """
+        :return:
+        """
+        if not self.deviceConnected:
+            return False
+
+        self.callMethodThreaded(self.client.CalibratorOn, 128)
+        return True
+
+    def lightOff(self):
+        """
+        :return:
+        """
+        if not self.deviceConnected:
+            return False
+
+        self.callMethodThreaded(self.client.CalibratorOff)
+        return True
+
+    def lightIntensity(self, value):
+        """
+        :param value:
+        :return:
+        """
+        if not self.deviceConnected:
+            return False
+
+        self.callMethodThreaded(self.client.CalibratorOn, value)
+        return True
