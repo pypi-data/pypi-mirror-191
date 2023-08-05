c556f5c5358e44e34685d46ef2ee881dfe938dd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:24 +0000

    Add package python-deco/0.1.1/python_deco-0.1.1-py3-none-any.whl

---
+++ packages/python-deco/python_deco-0.1.1-py3-none-any.whl/py_deco/time/time_utils.py
@@ -0,0 +1,13 @@
+import time
+
+
+def timeit(func):
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f"\nFunction {func.__name__}{args} Took {total_time:.4f} seconds")
+        return result
+
+    return timeit_wrapper
