6b00140aab215174ff02774354fc7df739888403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:49 +0000

    Add package clippings2anki/0.1.0/clippings2anki-0.1.0-py3-none-any.whl

---
+++ packages/clippings2anki/clippings2anki-0.1.0-py3-none-any.whl/clippings2anki/__main__.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append("")
+
+import clippings2anki.clippings as clippings
+
+if __name__ == "__main__":
+    # read cli arguments
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", type=str, help="Path to the 'My clippings.txt' file")
+    parser.add_argument("language", type=str, help="Language of the clippings (e.g. 'english' or 'german' for English or German)")
+    parser.add_argument('-o', "--output", type=str, default="Kindle flashcards.txt", help="Path to the output file (default: 'Kindle flashcards.txt')")
+    parser.add_argument('--json', action=argparse.BooleanOptionalAction, help="Save the parsed clippings as a json file (default: False)")
+    args = parser.parse_args()
+
+    clippings.main(args.input_file, args.language, args.output, save_json=(args.json==True))
