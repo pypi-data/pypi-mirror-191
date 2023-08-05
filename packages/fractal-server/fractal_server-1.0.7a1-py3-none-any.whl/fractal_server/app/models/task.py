764fd255fc8861dcadd5f94270d8b9c337a18aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:26 +0000

    Add package fractal-server/1.0.7a1/fractal_server-1.0.7a1-py3-none-any.whl

---
+++ packages/fractal-server/fractal_server-1.0.7a1-py3-none-any.whl/fractal_server/app/models/task.py
@@ -0,0 +1,44 @@
+from typing import Any
+from typing import Dict
+from typing import Optional
+
+from sqlalchemy import Column
+from sqlalchemy.types import JSON
+from sqlmodel import Field
+
+from ...common.schemas.task import _TaskBase
+
+
+class Task(_TaskBase, table=True):  # type: ignore
+    """
+    Task model
+
+    Attributes:
+        id: Primary key
+        default_args: TBD
+        meta: TBD
+    """
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    default_args: Optional[Dict[str, Any]] = Field(
+        sa_column=Column(JSON), default={}
+    )
+    meta: Optional[Dict[str, Any]] = Field(sa_column=Column(JSON), default={})
+
+    @property
+    def parallelization_level(self) -> Optional[str]:
+        try:
+            return self.meta["parallelization_level"]
+        except KeyError:
+            return None
+
+    @property
+    def is_parallel(self) -> bool:
+        return bool(self.parallelization_level)
+
+    @property
+    def executor(self) -> Optional[str]:
+        try:
+            return self.meta["executor"]
+        except KeyError:
+            return None
