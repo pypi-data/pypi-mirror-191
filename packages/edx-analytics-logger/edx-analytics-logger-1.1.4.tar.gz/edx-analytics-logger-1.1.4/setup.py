0dd15d0f92b3d3da5e6010e97e4fa72a1f6fe5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:15 +0000

    Add package edx-analytics-logger/1.1.4/edx-analytics-logger-1.1.4.tar.gz

---
+++ packages/edx-analytics-logger/edx-analytics-logger-1.1.4.tar.gz/edx-analytics-logger-1.1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='edx-analytics-logger',
+    version='1.1.4',
+    packages=['edx_analytics_logger'],
+    url='https://github.com/javisenberg/edx-analytics-logger',
+    license='MIT',
+    author='javisenberg',
+    description='edx powered ApiBackend',
+    long_description=open('README.rst').read(),
+)
