ae5c52c70c27dc3055067a83f7f40fed1f3f59da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:53 +0000

    Add package nonebot-plugin-alconna/0.2.2/nonebot-plugin-alconna-0.2.2.tar.gz

---
+++ packages/nonebot-plugin-alconna/nonebot-plugin-alconna-0.2.2.tar.gz/nonebot-plugin-alconna-0.2.2/src/nonebot_plugin_alconna/adapters/console.py
@@ -0,0 +1,9 @@
+from nonebot_plugin_alconna.typings import gen_unit
+from nonebot_plugin_alconna.analyser import MessageContainer
+
+MessageContainer.config(
+    preprocessors={"MessageSegment": lambda x: str(x) if x.type == "text" else None}
+)
+
+Markdown = gen_unit("markdown")
+Text = str
