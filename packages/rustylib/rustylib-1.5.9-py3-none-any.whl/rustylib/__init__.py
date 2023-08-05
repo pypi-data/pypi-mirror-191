764e060fdfe2900bd3ee397e7ed764a4c1c1e18b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:31 +0000

    Add package rustylib/1.5.9/rustylib-1.5.9-py3-none-any.whl

---
+++ packages/rustylib/rustylib-1.5.9-py3-none-any.whl/rustylib/__init__.py
@@ -0,0 +1,8 @@
+from .Rrandom import Random, init
+from .Rmath import Math
+from .Rrequests import Request
+from .Rsugaku import Sugaku
+from .Rusty import Rusty
+from .Rusty import Webhook
+from .Rusty import Rplus
+from .Rcolor import Back
