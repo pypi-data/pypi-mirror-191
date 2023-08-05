3391d4074e9a6bf49ed21f1c6ce72ce6946f41cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:06 +0000

    Add package py-graphql-mapper/1.1.0/py_graphql_mapper-1.1.0-py3-none-any.whl

---
+++ packages/py-graphql-mapper/py_graphql_mapper-1.1.0-py3-none-any.whl/pygqlmap/gql_types.py
@@ -0,0 +1,8 @@
+class ID(str): pass
+class NonNull_int(int): pass
+class NonNull_float(float): pass
+class NonNull_bool(int): pass
+class NonNull_str(str): pass
+class NonNull_ID(ID): pass
+class NonNull_Bool(int): pass
+class NonNull_list(list): pass
