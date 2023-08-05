44df4a5c30be3bba7b03e759ef942aec046cc8ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:09 +0000

    Add package pythonMY/0.1/pythonMY-0.1.tar.gz

---
+++ packages/pythonMY/pythonMY-0.1.tar.gz/pythonMY-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pythonMY',
+    version='0.1',
+    description='Packet de biblio scapy pour analyser le reseau',
+    author='MOHAMED HAOUARI',
+    author_email='mohamed.haouari.2001@gmail.com',
+    packages=find_packages(),
+)
+
+
+
+
