19ae657eb3175484a5a5aa27608b06dd920eb487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:27 +0000

    Add package aiotieba/3.1.7/aiotieba-3.1.7-cp38-cp38-musllinux_1_1_x86_64.whl

---
+++ packages/aiotieba/aiotieba-3.1.7-cp38-cp38-musllinux_1_1_x86_64.whl/aiotieba/client/get_uinfo_getuserinfo_app/_api.py
@@ -0,0 +1,51 @@
+import sys
+
+import yarl
+
+from .._core import APP_BASE_HOST, HttpCore
+from .._helper import APP_INSECURE_SCHEME, log_exception, pack_proto_request, send_request
+from ..exception import TiebaServerError
+from ._classdef import UserInfo_guinfo_app
+from .protobuf import GetUserInfoReqIdl_pb2, GetUserInfoResIdl_pb2
+
+CMD = 303024
+
+
+def pack_proto(user_id: int) -> bytes:
+    req_proto = GetUserInfoReqIdl_pb2.GetUserInfoReqIdl()
+    req_proto.data.user_id = user_id
+
+    return req_proto.SerializeToString()
+
+
+def parse_body(body: bytes) -> UserInfo_guinfo_app:
+    res_proto = GetUserInfoResIdl_pb2.GetUserInfoResIdl()
+    res_proto.ParseFromString(body)
+
+    if error_code := res_proto.error.errorno:
+        raise TiebaServerError(error_code, res_proto.error.errmsg)
+
+    user_proto = res_proto.data.user
+    user = UserInfo_guinfo_app()._init(user_proto)
+
+    return user
+
+
+async def request_http(http_core: HttpCore, user_id: int) -> UserInfo_guinfo_app:
+    request = pack_proto_request(
+        http_core,
+        yarl.URL.build(
+            scheme=APP_INSECURE_SCHEME, host=APP_BASE_HOST, path="/c/u/user/getuserinfo", query_string=f"cmd={CMD}"
+        ),
+        pack_proto(user_id),
+    )
+
+    try:
+        body = await send_request(request, http_core.connector, read_bufsize=1024)
+        user = parse_body(body)
+
+    except Exception as err:
+        log_exception(sys._getframe(1), err, f"user_id={user_id}")
+        user = UserInfo_guinfo_app()._init_null()
+
+    return user
