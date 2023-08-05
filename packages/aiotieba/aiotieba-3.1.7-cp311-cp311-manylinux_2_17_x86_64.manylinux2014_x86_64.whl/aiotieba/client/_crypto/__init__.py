d640f6a8de12b001dcab84e19d7d4e16556ace41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:26 +0000

    Add package aiotieba/3.1.7/aiotieba-3.1.7-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/aiotieba/aiotieba-3.1.7-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aiotieba/client/_crypto/__init__.py
@@ -0,0 +1,1 @@
+from .crypto import c3_aid, cuid_galaxy2, rc4_42, sign
---
+++ packages/aiotieba/aiotieba-3.1.7-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aiotieba/client/get_self_follow_forums/_api.py
@@ -0,0 +1,42 @@
+import sys
+
+import yarl
+
+from .._core import WEB_BASE_HOST, HttpCore
+from .._helper import log_exception, pack_web_get_request, parse_json, send_request
+from ..exception import TiebaServerError
+from ._classdef import SelfFollowForums
+
+
+def parse_body(body: bytes) -> SelfFollowForums:
+    res_json = parse_json(body)
+    if code := res_json['errno']:
+        raise TiebaServerError(code, res_json['errmsg'])
+
+    data_dict = res_json['data']['like_forum']
+    self_follow_forums = SelfFollowForums()._init(data_dict)
+
+    return self_follow_forums
+
+
+async def request(http_core: HttpCore, pn: int) -> SelfFollowForums:
+    params = [
+        ('pn', pn),
+        ('rn', '200'),
+    ]
+
+    request = pack_web_get_request(
+        http_core,
+        yarl.URL.build(scheme="https", host=WEB_BASE_HOST, path="/mg/o/getForumHome"),
+        params,
+    )
+
+    try:
+        body = await send_request(request, http_core.connector, read_bufsize=128 * 1024)
+        self_follow_forums = parse_body(body)
+
+    except Exception as err:
+        log_exception(sys._getframe(1), err)
+        self_follow_forums = SelfFollowForums()._init_null()
+
+    return self_follow_forums
