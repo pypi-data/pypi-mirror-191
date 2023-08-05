7ff9f8e339e2915bc1604a6d690c93609fabc253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:06 +0000

    Add package frictionless/5.5.6/frictionless-5.5.6.tar.gz

---
+++ packages/frictionless/frictionless-5.5.6.tar.gz/frictionless-5.5.6/frictionless/server/__init__.py
@@ -0,0 +1,12 @@
+from .config import Config
+from .server import Server
+
+# Register modules
+from . import bytes
+from . import data
+from . import file
+from . import folder
+from . import package
+from . import project
+from . import table
+from . import text
