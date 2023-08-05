7cd8c56557eb31d9ba12b7203dd6e868a78a1c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:27 +0000

    Add package aiotieba/3.1.7/aiotieba-3.1.7-pp38-pypy38_pp73-macosx_10_9_x86_64.whl

---
+++ packages/aiotieba/aiotieba-3.1.7-pp38-pypy38_pp73-macosx_10_9_x86_64.whl/aiotieba/client/get_rank_users/_api.py
@@ -0,0 +1,39 @@
+import sys
+
+import bs4
+import yarl
+
+from .._core import WEB_BASE_HOST, HttpCore
+from .._helper import log_exception, pack_web_get_request, send_request
+from ._classdef import RankUsers
+
+
+def parse_body(body: bytes) -> RankUsers:
+    soup = bs4.BeautifulSoup(body, 'lxml')
+    rank_users = RankUsers()._init(soup)
+
+    return rank_users
+
+
+async def request(http_core: HttpCore, fname: str, pn: int) -> RankUsers:
+    params = [
+        ('kw', fname),
+        ('pn', pn),
+        ('ie', 'utf-8'),
+    ]
+
+    request = pack_web_get_request(
+        http_core,
+        yarl.URL.build(scheme="https", host=WEB_BASE_HOST, path="/f/like/furank"),
+        params,
+    )
+
+    try:
+        body = await send_request(request, http_core.connector, read_bufsize=64 * 1024)
+        rank_users = parse_body(body)
+
+    except Exception as err:
+        log_exception(sys._getframe(1), err, f"fname={fname}")
+        rank_users = RankUsers()._init_null()
+
+    return rank_users
---
+++ packages/aiotieba/aiotieba-3.1.7-pp38-pypy38_pp73-macosx_10_9_x86_64.whl/aiotieba/client/set_nickname_old/_api.py
@@ -0,0 +1,40 @@
+import sys
+
+import yarl
+
+from .._core import WEB_BASE_HOST, HttpCore
+from .._helper import log_exception, log_success, pack_web_form_request, parse_json, send_request
+from ..exception import TiebaServerError
+
+
+def parse_body(body: bytes) -> None:
+    res_json = parse_json(body)
+    if code := res_json['no']:
+        raise TiebaServerError(code, res_json['error'])
+
+
+async def request(http_core: HttpCore, nick_name: str) -> bool:
+    params = [
+        ('nickname', nick_name),
+        ('tbs', '1'),
+    ]
+
+    request = pack_web_form_request(
+        http_core,
+        yarl.URL.build(scheme="https", host=WEB_BASE_HOST, path="/mo/q/submit/modifyNickname", query=params),
+        [],
+    )
+
+    log_str = f"nick_name={nick_name}"
+    frame = sys._getframe(1)
+
+    try:
+        body = await send_request(request, http_core.connector, read_bufsize=1024)
+        parse_body(body)
+
+    except Exception as err:
+        log_exception(frame, err, log_str)
+        return False
+
+    log_success(frame, log_str)
+    return True
