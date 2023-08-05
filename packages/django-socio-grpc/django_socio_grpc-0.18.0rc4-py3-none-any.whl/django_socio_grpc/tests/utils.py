91fbded65b0df7aecbd9c3ec8457414e4d2c2aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:26 +0000

    Add package django-socio-grpc/0.18.0rc4/django_socio_grpc-0.18.0rc4-py3-none-any.whl

---
+++ packages/django-socio-grpc/django_socio_grpc-0.18.0rc4-py3-none-any.whl/django_socio_grpc/tests/utils.py
@@ -0,0 +1,10 @@
+from contextlib import contextmanager
+from unittest.mock import mock_open, patch
+
+
+@contextmanager
+def patch_open(read_data=""):
+    m = mock_open(read_data=read_data)
+
+    with patch("io.open", m), patch("builtins.open", m), patch("pathlib.Path.mkdir"):
+        yield m
