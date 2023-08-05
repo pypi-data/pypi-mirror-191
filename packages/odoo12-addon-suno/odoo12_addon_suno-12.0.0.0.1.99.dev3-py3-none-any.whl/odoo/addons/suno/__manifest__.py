9c35cb155e1530c10ca747c3f02abe46a1ae361c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:13 +0000

    Add package odoo12-addon-suno/12.0.0.0.1.99.dev3/odoo12_addon_suno-12.0.0.0.1.99.dev3-py3-none-any.whl

---
+++ packages/odoo12-addon-suno/odoo12_addon_suno-12.0.0.0.1.99.dev3-py3-none-any.whl/odoo/addons/suno/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Odoo Suno customizations",
+    'version': '12.0.0.0.1',
+    'depends': [
+        'sale_timesheet'
+    ],
+    'author': "Coopdevs Treball SCCL",
+    'website': 'https://coopdevs.org',
+    'category': "Cooperative management",
+    'description': "Odoo Suno customizations",
+    "license": "AGPL-3",
+    'data': [
+        "views/project_profitability_report_analysis_views.xml",
+    ],
+}
