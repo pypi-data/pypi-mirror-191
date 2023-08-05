aa9b828406c038a4fde59cd0c2aeee140f7f15e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:11 +0000

    Add package pydotbot/0.13.0/pydotbot-0.13.0-cp311-cp311-win_amd64.whl

---
+++ packages/pydotbot/pydotbot-0.13.0-cp311-cp311-win_amd64.whl/dotbot/frontend/src/constants.js
@@ -0,0 +1,16 @@
+export const ApplicationType = {
+  DotBot: 0,
+  SailBot: 1,
+};
+
+export const ControlModeType = {
+  Manual: 0,
+  Auto: 1,
+};
+
+export const maxWaypoints = 8;
+
+export const lh2_distance_threshold = 0.01;
+export const lh2_waypoint_area_radius = 0.07;
+export const gps_distance_threshold = 10;  // 10 meters
+export const gps_waypoint_area_radius = 15; // 15 meters
