bdb954ad95609de6515fcd688ef0e6121f17d0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:13 +0000

    Add package kcli/99.0.202302101521/kcli-99.0.202302101521-py3-none-any.whl

---
+++ packages/kcli/kcli-99.0.202302101521-py3-none-any.whl/kvirt/expose/static/js/exposeactions.js
@@ -0,0 +1,15 @@
+function exposedelete(plan){
+  $("#wheel").show();
+  var r = confirm("Are you sure you want to delete this Plan?");
+  if (r != true) {
+    return ;
+  }
+  $.ajax({
+       type: "DELETE",
+        url: `/expose/${plan}`,
+        success: function(data) {
+            $("#wheel").hide();
+                location.reload(true);
+        }
+    });
+}
