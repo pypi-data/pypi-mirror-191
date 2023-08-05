9b25793a6f6e68e34dbdd52ba4bccb29df4944ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:35 +0000

    Add package ansible-lint/6.12.2/ansible_lint-6.12.2-py3-none-any.whl

---
+++ packages/ansible-lint/ansible_lint-6.12.2-py3-none-any.whl/ansiblelint/schemas/__main__.py
@@ -0,0 +1,14 @@
+"""Module containing cached JSON schemas."""
+# pragma: no cover
+import sys
+
+from ansiblelint.schemas.main import refresh_schemas
+
+if __name__ == "__main__":
+    if refresh_schemas():
+        # flake8: noqa: T201
+        print("Schemas were updated.")
+        sys.exit(1)
+    else:
+        # flake8: noqa: T201
+        print("Schemas not updated", 0)
