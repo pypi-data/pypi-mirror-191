fe8710dbaa94e438fd82abdb7edd335378a3ed8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:27 +0000

    Add package aiotieba/3.1.7/aiotieba-3.1.7-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/aiotieba/aiotieba-3.1.7-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aiotieba/client/get_member_users/_api.py
@@ -0,0 +1,39 @@
+import sys
+
+import bs4
+import yarl
+
+from .._core import WEB_BASE_HOST, HttpCore
+from .._helper import log_exception, pack_web_get_request, send_request
+from ._classdef import MemberUsers
+
+
+def parse_body(body: bytes) -> MemberUsers:
+    soup = bs4.BeautifulSoup(body, 'lxml')
+    member_users = MemberUsers()._init(soup)
+
+    return member_users
+
+
+async def request(http_core: HttpCore, fname: str, pn: int) -> MemberUsers:
+    params = [
+        ('word', fname),
+        ('pn', pn),
+        ('ie', 'utf-8'),
+    ]
+
+    request = pack_web_get_request(
+        http_core,
+        yarl.URL.build(scheme="https", host=WEB_BASE_HOST, path="/bawu2/platform/listMemberInfo"),
+        params,
+    )
+
+    try:
+        body = await send_request(request, http_core.connector, read_bufsize=64 * 1024)
+        member_users = parse_body(body)
+
+    except Exception as err:
+        log_exception(sys._getframe(1), err, f"fname={fname}")
+        member_users = MemberUsers()._init_null()
+
+    return member_users
---
+++ packages/aiotieba/aiotieba-3.1.7-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/aiotieba/client/get_uinfo_panel/_api.py
@@ -0,0 +1,42 @@
+import sys
+
+import yarl
+
+from .._core import WEB_BASE_HOST, HttpCore
+from .._helper import is_portrait, log_exception, pack_web_get_request, parse_json, send_request
+from ..exception import TiebaServerError
+from ._classdef import UserInfo_panel
+
+
+def parse_body(body: bytes) -> UserInfo_panel:
+    res_json = parse_json(body)
+    if code := res_json['no']:
+        raise TiebaServerError(code, res_json['error'])
+
+    user_dict = res_json['data']
+    user = UserInfo_panel()._init(user_dict)
+
+    return user
+
+
+async def request(http_core: HttpCore, name_or_portrait: str) -> UserInfo_panel:
+    if is_portrait(name_or_portrait):
+        params = [('id', name_or_portrait)]
+    else:
+        params = [('un', name_or_portrait)]
+
+    request = pack_web_get_request(
+        http_core,
+        yarl.URL.build(scheme="https", host=WEB_BASE_HOST, path="/home/get/panel"),
+        params,
+    )
+
+    try:
+        body = await send_request(request, http_core.connector, read_bufsize=64 * 1024)
+        user = parse_body(body)
+
+    except Exception as err:
+        log_exception(sys._getframe(1), err, f"user={name_or_portrait}")
+        user = UserInfo_panel()._init_null()
+
+    return user
