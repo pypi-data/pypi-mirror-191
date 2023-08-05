ba0491a68c34e18b255ba97998db1be347386d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:23 +0000

    Add package noteql/0.7.2/noteql-0.7.2.tar.gz

---
+++ packages/noteql/noteql-0.7.2.tar.gz/noteql-0.7.2/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+    name="noteql",
+    version="0.7.2",
+    description="Write sql in a notbook",
+    author="David Raznick",
+    author_email="david.raznick@opendataservices.coop",
+    url="https://github.com/OpenDataServices/noteql",
+    packages=["noteql"],
+    install_requires=[
+        "ijson",
+        "xmltodict",
+        "sqlalchemy",
+        "psycopg2-binary",
+        "sqlalchemy<2",
+        "ijson",
+        "click",
+        "jupyter",
+        "jinja2",
+        "lxml",
+        "requests",
+        "pandas",
+        "openpyxl",
+        "pyparsing",
+        "noteql-jinjasql",
+    ],
+)
