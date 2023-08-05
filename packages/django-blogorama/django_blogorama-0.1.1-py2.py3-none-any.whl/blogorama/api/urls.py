4fcab979c2c02dbba53f107c67aa6d7d13698c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:35 +0000

    Add package django-blogorama/0.1.1/django_blogorama-0.1.1-py2.py3-none-any.whl

---
+++ packages/django-blogorama/django_blogorama-0.1.1-py2.py3-none-any.whl/blogorama/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from blogorama.api import views
+
+urlpatterns = [
+    # path("posts/", views.PostlListAPIView.as_view(), name="blogorama-post-list"),
+    # path(
+    #     "post/<slug:the_slug>/",
+    # ),
+]
