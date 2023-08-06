03c70f69b1bc54a6900c463987aee0cda607f587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:02 +0000

    Add package schimpy/1.4.3/schimpy-1.4.3.tar.gz

---
+++ packages/schimpy/schimpy-1.4.3.tar.gz/schimpy-1.4.3/setup.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+import versioneer
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+install_requires = ['geopandas', 'vtools3',
+                    'xarray', 'netcdf4', 'scipy',
+                    'matplotlib', 'statsmodels',
+                    'palettable', 'pyyaml',
+                    'scikit-learn','statsmodels',
+                    'beautifulsoup4','pyproj', 'nodepy',
+                    'shapely<2.0','rasterstats']
+
+requirements = install_requires
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="California Department of Water Resources",
+    author_email='Eli.Ateljevich@water.ca.gov, Kijin.Nam@water.ca.gov',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    description="Python package for SCHISM",
+    entry_points={
+        'console_scripts': [
+            'batch_metrics=schimpy.batch_metrics:main',
+            'clip_dems=schimpy.clip_dems:main',
+            'contour_smooth=schimpy.contour_smooth:main',
+            'convert_mesh=schimpy.convert_mesh:main',
+            'convert_polygons=schimpy.convert_polygons:main',
+            'convert_linestrings=schimpy.convert_linestrings:main',
+            'combine_consume=schimpy.combine_consume:main',
+            'prepare_schism=schimpy.prepare_schism:main',
+            'create_vgrid_lsc2=schimpy.create_vgrid_lsc2:main',
+            'schism_hotstart=schimpy.schism_hotstart:main',
+            'split_quad=schimpy.split_quad:main',
+            'model_time=schimpy.model_time:main',
+            'gen_elev2d=schimpy.gen_elev2D:main',
+            'small_areas=schimpy.small_areas:main',
+            'station=schimpy.station:main',
+            'create_hotstart=schimpy.schism_hotstart:main',
+            'create_nudging=schimpy.schism_nudging:main',
+            'interpolate_structure=schimpy.interpolate_structure:main'
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='schimpy',
+    name='schimpy',
+    packages=find_packages(include=['schimpy', 'schimpy.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/CADWRDeltaModeling/schimpy',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    zip_safe=False,
+)
---
+++ packages/schimpy/schimpy-1.4.3.tar.gz/schimpy-1.4.3/tests/test_namelist.py
@@ -0,0 +1,96 @@
+def test_namelist_parser():
+
+    file_content = """&NAMELIST1
+    ! a full line comment
+    VAR1 = 1 ! a line comment
+    VAR2 = 2 ! another comment
+    / ! a random comment
+    &NAMELIST2
+    VAR3 = 3
+    VAR4 = 4
+    /"""
+
+    from schimpy import namelist
+    namelists = namelist.parse(file_content)
+    print(namelists)
+
+    expected_namelists = {'NAMELIST1': {'comment': "",
+            'VAR1': {'value': '1', 'full_line_comment': ' a full line comment', 'inline_comment': ' a line comment'},
+            'VAR2': {'value': '2', 'full_line_comment': "", 'inline_comment': ' another comment'}},
+        'NAMELIST2': {'comment': "",
+            'VAR3': {'value': '3', 'full_line_comment': "", 'inline_comment': ""},
+        'VAR4': {'value': '4', 'full_line_comment': "", 'inline_comment': ""}}}
+
+    assert namelists == expected_namelists
+
+def test_write_namelist():
+    from schimpy import namelist
+    namelist_data = {
+        "first_namelist": {
+            "comment": "This is the first namelist",
+            "first_value": {
+                "value": 1,
+                "inline_comment": "This is the first value"
+            },
+            "second_value": {
+                "value": 2
+            }
+        },
+        "second_namelist": {
+            "comment": "This is the second namelist",
+            "third_value": {
+                "value": 3,
+                "inline_comment": "This is the third value"
+            }
+        }
+    }
+    expected_output = "!This is the first namelist\n&first_namelist\n  first_value = 1 !This is the first value\n  second_value = 2\n/\n!This is the second namelist\n&second_namelist\n  third_value = 3 !This is the third value\n/"
+    output = namelist.write(namelist_data)
+    assert output == expected_output, f"Expected: {expected_output}\nGot: {output}"
+
+    namelist_data = {
+        "first_namelist": {
+            "first_value": {
+                "value": 1
+            },
+            "second_value": {
+                "value": 2
+            }
+        },
+        "second_namelist": {
+            "third_value": {
+                "value": 3
+            }
+        }
+    }
+    expected_output = "&first_namelist\n  first_value = 1\n  second_value = 2\n/\n&second_namelist\n  third_value = 3\n/"
+    output = namelist.write(namelist_data)
+    assert output == expected_output, f"Expected: {expected_output}\nGot: {output}"
+
+def test_sample_param_nml():
+    url = 'https://raw.githubusercontent.com/schism-dev/schism/master/sample_inputs/param.nml'
+    import urllib.request
+    with urllib.request.urlopen(url) as response:
+        content = response.read().decode("utf-8")
+    with open('param-original.nml','w') as fh: fh.write(content)
+    from schimpy import namelist
+    params = namelist.parse(content)
+    with open('param.nml','w') as fh: fh.write(namelist.write(params))
+    with open('param.nml','r') as fh:
+        params2 = namelist.parse(fh.read())
+    with open('param2.nml','w') as fh: fh.write(namelist.write(params2))
+    import filecmp
+    assert filecmp.cmp('param.nml','param2.nml'), 'Reading and writing from parser to writer is resulting in differences!'
+
+
+def test_mapping():
+    from schimpy import namelist
+    namelist_dict = {'group1': {'var1': 1, 'var2': 2}, 'group2': {'var3': 3, 'var4': 4}}
+    obj = namelist.map_to_object(namelist_dict)
+    assert obj.group1.var1 == 1
+    assert obj.group1.var2 == 2
+    assert obj.group2.var3 == 3
+    assert obj.group2.var4 == 4
+    round_trip_dict = namelist.map_to_dict(obj)
+    assert namelist_dict == round_trip_dict
+
