8fbf4011dab0d7a01b212c809142c8e4e24881f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:02 +0000

    Add package resourcesing/0.1.2/resourcesing-0.1.2.tar.gz

---
+++ packages/resourcesing/resourcesing-0.1.2.tar.gz/resourcesing-0.1.2/resourcesing/__init__.py
@@ -0,0 +1,23 @@
+import sys as sys
+import pathlib as pathlib
+import inspect as inspect
+
+def get_path(object, path, resource_path="resources") -> pathlib.Path:
+    """
+    ```python
+    import resources
+    import you_module
+
+    path = resources.get_resource_path(you_module, 'icon.png')
+    print(path) # ...\you_module_path\\resources\icon.png
+    
+    o = lambda: None
+    path = resources.get_resource_path(o, 'icon.png')
+    print(path) # ...\current_path\\resources\icon.png
+
+    # Path -> str
+    path_s = path.__str__()
+    path_s = str(path)
+    ```
+    """
+    return pathlib.Path(inspect.getfile(object)).parent.joinpath(resource_path).joinpath(path).absolute()
