a75562b13463daf41dca3ef4f6b4928f9c474790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:41 +0000

    Add package fastode/0.1.2/fastode-0.1.2.tar.gz

---
+++ packages/fastode/fastode-0.1.2.tar.gz/fastode-0.1.2/fastode/fast_zip.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2022/10/21 12:55
+# @Author  : zhangbc0315@outlook.com
+# @File    : fast_zip.py
+# @Software: PyCharm
+
+
+class FastZIP:
+
+    @classmethod
+    def
+
+
+if __name__ == "__main__":
+    pass
