ee86ca3bc7a7bc6591d7beead6bea43637919355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:41 +0000

    Add package local-tuya/1.0.1/local_tuya-1.0.1-py3-none-any.whl

---
+++ packages/local-tuya/local_tuya-1.0.1-py3-none-any.whl/local_tuya/domoticz/__init__.py
@@ -0,0 +1,11 @@
+from local_tuya.domoticz.plugin import Option, Parameter, PluginMetadata, install_plugin
+from local_tuya.domoticz.units import (
+    SelectorSwitchStyle,
+    Unit,
+    UnitId,
+    UnitManager,
+    selector_switch_unit,
+    set_point_unit,
+    switch_unit,
+    temperature_unit,
+)
