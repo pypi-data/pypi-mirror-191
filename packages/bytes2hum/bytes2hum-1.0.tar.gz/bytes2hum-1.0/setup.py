5d96da4ab157eb354350b4d6f693ac8c68a444dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:36 +0000

    Add package bytes2hum/1.0/bytes2hum-1.0.tar.gz

---
+++ packages/bytes2hum/bytes2hum-1.0.tar.gz/bytes2hum-1.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup 
+# name mod_name
+# version 
+# description 
+# author 
+# py_modules 
+setup(name="bytes2hum", version="1.0", description="transfer bytes to humean", 
+author="feng_land@163.com", py_modules=['bytes2hum', ])
