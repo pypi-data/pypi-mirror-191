222e22fd02039280e1166af11eccabb155636fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:58 +0000

    Add package neatmesh/0.0.6/neatmesh-0.0.6.tar.gz

---
+++ packages/neatmesh/neatmesh-0.0.6.tar.gz/neatmesh-0.0.6/src/neatmesh/__init__.py
@@ -0,0 +1,29 @@
+from typing import Union
+
+from ._exceptions import (
+    InputMeshDimensionError,
+    InvalidMeshError,
+    NonSupportedElementError,
+)
+from ._mesh import Mesh2D, Mesh3D
+
+
+def read(mesh_file_path: str) -> Union[Mesh2D, Mesh3D]:
+    from ._reader import MeshReader2D, assign_reader
+
+    reader = assign_reader(mesh_file_path)
+
+    if isinstance(reader, MeshReader2D):
+        return Mesh2D(reader)
+    else:
+        return Mesh3D(reader)
+
+
+__all__ = [
+    "Mesh2D",
+    "Mesh3D",
+    "InputMeshDimensionError",
+    "InvalidMeshError",
+    "NonSupportedElementError",
+    "read",
+]
