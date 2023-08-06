1f47da83094205b003294d900b514bac5f8c480b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:20 +0000

    Add package django-helper-builder/0.1/django_helper_builder-0.1.tar.gz

---
+++ packages/django-helper-builder/django_helper_builder-0.1.tar.gz/django_helper_builder-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='django_helper_builder',
+      version='0.1',
+      description='Quick deployment of a Django project',
+      packages=['django_helper_builder'],
+      author_email='sergey_wilson@mail.ru',
+      zip_safe=False)
