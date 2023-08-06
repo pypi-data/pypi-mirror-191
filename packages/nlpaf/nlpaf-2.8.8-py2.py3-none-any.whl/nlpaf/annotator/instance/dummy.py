f13b40a9431b17f567714990462069e7ff899042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:19 +0000

    Add package nlpaf/2.8.8/nlpaf-2.8.8-py2.py3-none-any.whl

---
+++ packages/nlpaf/nlpaf-2.8.8-py2.py3-none-any.whl/nlpaf/annotator/instance/dummy.py
@@ -0,0 +1,13 @@
+from nlpaf import logger
+from nlpaf.annotator.orchestrator import PipeOrchestrator
+
+
+class DummyDefaultPipeOrchestrator(PipeOrchestrator):
+    pass
+
+
+
+
+def customized_pipe_func(input):
+    logger.debug("checking Dummy customized pipe")
+    return input
