583693582d92d8e537d83012fb2cf0a9ae50abd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:53 +0000

    Add package youtubedlapi-server-infusiblecoder/3.0/youtubedlapi_server_infusiblecoder-3.0.tar.gz

---
+++ packages/youtubedlapi-server-infusiblecoder/youtubedlapi_server_infusiblecoder-3.0.tar.gz/youtubedlapi_server_infusiblecoder-3.0/README.rst
@@ -0,0 +1,25 @@
+youtubedlapi-server-infusiblecoder
+=====================
+
+A REST API server for getting the info for videos from different sites, powered by `youtube-dl <http://rg3.github.io/youtube-dl/>`_.
+The installation instructions and the documentation are available at `Read the Docs <https://youtubedlapi-server-infusiblecoder.readthedocs.io/>`_.
+
+About
+-----
+
+``youtubedlapi-server-infusiblecoder`` is released to the public domain, read the `license <LICENSE>`_ for more info.
+
+
+Example
+-----
+
+youtubedlapi-server-infusiblecoder -p 8000 --host 127.0.0.1 --number-processes 1
+
+or
+
+youtubedlapi-server-infusiblecoder -p 9191 --host 0.0.0.0 --number-processes 1
+
+or for bg run 
+
+nohup youtubedlapi-server-infusiblecoder -p 9191 --host 0.0.0.0 --number-processes 1 &
+
---
+++ packages/youtubedlapi-server-infusiblecoder/youtubedlapi_server_infusiblecoder-3.0.tar.gz/youtubedlapi_server_infusiblecoder-3.0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='youtubedlapi_server_infusiblecoder',
+    version='3.0',
+    description='An API server based on yt-dlp',
+    long_description='Get the videos from different sites using a server running yt-dlp',
+    author='Syed Usama Ahmad',
+    author_email='syedusama5556@gmail.com',
+    url='https://github.com/syedusama5556/youtubedlapi-server-infusiblecoder',
+    packages=['youtubedlapi_server_infusiblecoder'],
+    entry_points={
+        'console_scripts': [
+            'youtubedlapi-server-infusiblecoder = youtubedlapi_server_infusiblecoder.server:main',
+        ],
+    },
+
+    install_requires=[
+        'Flask',
+        'yt_dlp >= 2022.11.11',
+    ],
+
+    classifiers=[
+        'Topic :: Multimedia :: Video',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: Public Domain',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
