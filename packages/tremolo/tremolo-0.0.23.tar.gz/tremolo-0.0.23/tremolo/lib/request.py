89097a9d63c9d9549733364d964d5324bbcd7b4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:22:49 +0000

    Add package tremolo/0.0.23/tremolo-0.0.23.tar.gz

---
+++ packages/tremolo/tremolo-0.0.23.tar.gz/tremolo-0.0.23/tremolo/lib/request.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2023 nggit
+
+import asyncio
+
+class Request:
+    def __init__(self, protocol):
+        self._protocol = protocol
+        self._loop = self._protocol.loop
+
+    async def read_finished(self):
+        return
+
+    async def read_started(self):
+        return
+
+    async def read_timeout(self, timeout):
+        self._protocol.options['logger'].info('read timeout after {:d}s'.format(timeout))
+
+    async def read(self):
+        await self.read_started()
+
+        while True:
+            cancel_read_timeout = self._loop.create_future()
+            self._loop.create_task(self._protocol.set_timeout(cancel_read_timeout,
+                                                          timeout_cb=self.read_timeout))
+
+            data = await self._protocol.queue[0].get()
+            self._protocol.queue[0].task_done()
+            cancel_read_timeout.set_result(None)
+
+            if data is None:
+                await self.read_finished()
+                break
+
+            yield data
