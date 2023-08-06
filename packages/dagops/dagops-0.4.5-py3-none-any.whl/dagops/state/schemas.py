a389b05a15460afee664fe7ec3b70f0c72883fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:25 +0000

    Add package dagops/0.4.5/dagops-0.4.5-py3-none-any.whl

---
+++ packages/dagops/dagops-0.4.5-py3-none-any.whl/dagops/state/schemas.py
@@ -0,0 +1,180 @@
+import datetime
+import uuid
+from uuid import UUID
+
+from pydantic import BaseModel
+from pydantic import Field
+from pydantic import root_validator
+
+from dagops.task_status import TaskStatus
+
+# =============================================================================
+
+
+class WithDuration(BaseModel):
+    created_at: datetime.datetime
+
+    duration_seconds: float | None
+
+    @root_validator
+    def validate_duration(cls, values):
+        started_at = values.get('started_at')
+        stopped_at = values.get('stopped_at')
+        if started_at is not None:
+            if stopped_at is not None:
+                values['duration_seconds'] = (stopped_at - started_at).total_seconds()
+            else:
+                values['duration_seconds'] = (datetime.datetime.now(tz=datetime.timezone.utc) - started_at).total_seconds()
+        return values
+
+# =============================================================================
+
+
+class WithWorkerName(BaseModel):
+    worker_name: str
+
+
+class ShellTaskInputData(BaseModel):
+    command: list[str]
+    env: dict[str, str] | None = None
+
+    def __hash__(self):
+        command = tuple(self.command)
+        env = frozenset(self.env.items()) if self.env is not None else None
+        return hash((command, env))
+
+
+class TaskMessage(BaseModel):
+    id: str
+    input_data: ShellTaskInputData
+
+
+class TaskStatusMessage(BaseModel):
+    id: str
+    status: TaskStatus
+    output_data: dict | None = None
+
+
+class TaskInfo(ShellTaskInputData, WithWorkerName):
+    pass
+
+
+InputDataDag = dict[TaskInfo, list[TaskInfo]]
+
+type_TO_INPUT_DATA_SCHEMA = {
+    'shell': ShellTaskInputData,
+    'dag': None,
+}
+
+
+class TaskCreate(WithWorkerName):
+    id: UUID = Field(default_factory=uuid.uuid4)
+    dag_id: UUID | None = None
+    upstream: list[UUID] = []
+    type: str | None = None
+    input_data: dict | None = None
+    worker_id: UUID | None = None
+
+    @root_validator(pre=True)
+    def validate_type_and_input_data(cls, values):
+        type = values['type']
+        if type not in type_TO_INPUT_DATA_SCHEMA:
+            raise ValueError(f'unsupported task type {type}')
+        input_data_schema = type_TO_INPUT_DATA_SCHEMA[type]
+
+        if input_data_schema is None:
+            return values
+
+        if 'input_data' not in values:
+            raise ValueError('input_data must be set for type {type}')
+
+        input_data_schema.validate(values['input_data'])
+        return values
+
+
+class Task(TaskCreate, WithDuration):
+    dag_id: UUID | None
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
+    started_at: datetime.datetime | None
+    stopped_at: datetime.datetime | None
+    status: TaskStatus
+    downstream: list[UUID] = []
+    output_data: dict | None = None
+
+    class Config:
+        orm_mode = True
+
+
+class TaskUpdate(BaseModel):
+    started_at: datetime.datetime | None
+    stopped_at: datetime.datetime | None
+    status: TaskStatus | None
+    output_data: dict | None = None
+    worker_id: UUID | None = None
+    running_worker_id: UUID | None = None
+    upstream: list[UUID] | None = None
+
+
+# =============================================================================
+
+
+class DagCreate(BaseModel):
+    type: str | None = None
+    tasks_input_data: list[dict]
+    graph: dict[int, list[int]]
+    daemon_id: UUID
+
+    @root_validator
+    def validate_graph(cls, values):
+        graph = values['graph']
+        n_tasks = len(values['tasks_input_data'])
+        for node, childs in graph.items():
+            if not (0 <= node < n_tasks):
+                raise ValueError(f'node={node} not in graph, n_tasks={n_tasks}')
+            for child in childs:
+                if not (0 <= child < n_tasks):
+                    raise ValueError(f'child={child} of node={node} not in graph, n_tasks={n_tasks}')
+                if child not in graph:
+                    raise ValueError(f'child={child} of node={node } not in graph')
+        return values
+
+
+# =============================================================================
+
+
+class FileCreate(BaseModel):
+    directory: str
+    file: str
+
+
+class FileUpdate(BaseModel):
+    dag_id: UUID | None
+
+
+class File(FileCreate, FileUpdate):
+    id: UUID
+
+    class Config:
+        orm_mode = True
+
+# =============================================================================
+
+
+class WorkerCreate(BaseModel):
+    name: str
+    maxtasks: int | None = None
+
+
+class WorkerUpdate(BaseModel):
+    name: str | None = None
+    maxtasks: int | None = None
+
+
+class Worker(WorkerCreate):
+    id: UUID
+    tasks: list[UUID]
+    running_tasks: list[UUID]
+
+    class Config:
+        orm_mode = True
---
+++ packages/dagops/dagops-0.4.5-py3-none-any.whl/dagops/util.py
@@ -0,0 +1,65 @@
+import datetime
+import operator
+from pathlib import Path
+
+import aiofiles.os
+import humanize
+
+
+def dirset(
+    path: str | Path,
+    glob: str = '*',
+    stem: bool = False,
+    absolute: bool = False,
+) -> set[str | Path]:
+    it = Path(path).glob(glob)
+    if stem and absolute:
+        raise ValueError('stem and absolute are mutually exclusive')
+    if stem:
+        return {p.stem for p in it}
+    if absolute:
+        return {str(p.absolute()) for p in it}
+    return set(it)
+
+
+async def path_stat(path: str) -> dict[str, float]:
+    p = await aiofiles.os.stat(path)
+    return {
+        'created': datetime.datetime.fromtimestamp(p.st_mtime),
+        'size': humanize.naturalsize(p.st_size),
+    }
+
+
+async def dirstat(
+    path: str,
+    sort_by: str | None = None,
+    reverse: bool = False,
+) -> list[dict]:
+    out = [
+        {
+            'name': p.name,
+            'created_at': datetime.datetime.fromtimestamp(p.stat().st_mtime, tz=datetime.timezone.utc),
+            'size': humanize.naturalsize(p.stat().st_size),
+        }
+        for p in await aiofiles.os.scandir(path)
+    ]
+    if sort_by is not None:
+        if sort_by not in {'name', 'created_at', 'size'}:
+            raise ValueError(f'Invalid sort_by: {sort_by}')
+        out.sort(key=operator.itemgetter(sort_by), reverse=reverse)
+
+    return out
+
+
+def format_time(
+    t: datetime.datetime,
+    absolute: bool = False,
+    pad: bool = False,
+) -> str:
+    if absolute or (datetime.datetime.now(tz=datetime.timezone.utc) - t).days > 30:
+        return t.strftime('%Y %b %d %H:%M')
+    t = datetime.datetime.fromtimestamp(t.timestamp())
+    out = humanize.naturaltime(t)
+    if pad:
+        out = out.rjust(17)
+    return out
