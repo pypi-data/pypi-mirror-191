df6068d765bbd5e60acac0f9c4a83f259672fe5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:50 +0000

    Add package djangobuk-envsettings/0.3.2/djangobuk_envsettings-0.3.2.tar.gz

---
+++ packages/djangobuk-envsettings/djangobuk_envsettings-0.3.2.tar.gz/djangobuk_envsettings-0.3.2/djangobuk_envsettings/__init__.py
@@ -0,0 +1,53 @@
+from typing import Iterable, Callable, Dict, Any, Tuple
+
+from djangobuk_envsettings.conversion import MAPPING
+from djangobuk_envsettings.utils import gather_settings, eval_settings
+import logging
+
+logger = logging.getLogger('djangobuk_envsettings.root')
+
+
+def update_from_env(
+        module,
+        pre: str = "DJANGO_",
+        mapping: Dict[str, Callable[[str], Any]] = MAPPING,
+        extra_mapping: Dict[str, Callable[[str], Any]] = None,
+        allowed: Iterable[str] = MAPPING,
+        extra_allowed: Iterable[str] = None,
+        hook: Callable[[str, Any], Tuple[str, Any]] = None
+):
+    """
+    :param module `sys.modules[__name__]`
+    :param pre: prefix for environment variables
+    :param mapping: mapping of `setting name`: `conversion function`
+    :param extra_mapping: same as mapping but will update defaults
+    :param allowed: iterable of settings that are allowed to be set from env
+    :param extra_allowed: same as allowed but will update defaults
+    :param hook: function that takes and returns `setting name` and `setting value`
+    """
+
+    mapping = mapping.copy()
+
+    # update mapping
+    if extra_mapping:
+        mapping.update(extra_mapping)
+
+    # convert allowed
+    allowed = list(allowed)
+
+    # update allowed
+    if extra_allowed:
+        allowed.extend(list(extra_allowed))
+
+    # gather all environment settings
+    env_settings = gather_settings(pre)
+
+    # evaluate settings
+    converted_settings = eval_settings(env_settings, mapping)
+
+    for k, v in converted_settings.items():
+        if k in allowed:
+            if hook:
+                k, v = hook(k, v)
+            logger.debug(f"setting {k} on {module}")
+            setattr(module, k, v)
---
+++ packages/djangobuk-envsettings/djangobuk_envsettings-0.3.2.tar.gz/djangobuk_envsettings-0.3.2/djangobuk_envsettings/utils.py
@@ -0,0 +1,49 @@
+from djangobuk_envsettings.conversion import MAPPING
+
+
+def get_mapping(extra_map: dict = None) -> dict:
+    mapping = MAPPING.copy()
+    if extra_map:
+        mapping.update(extra_map)
+    return mapping
+
+
+def settings_to_dict(settings_module=None) -> dict:
+    """
+    Convert Django settings module into dict.
+    """
+    return {
+        k: type(getattr(settings_module, k)) for k in dir(settings_module) if
+        not k.startswith('_') and
+        k.upper() == k
+    }
+
+
+def eval_settings(settings: dict, mapping=None) -> dict:
+    """
+    Given settings map ex. {SECRET_KEY: "asdasd"}
+    evaulate values into coresponding Python types.
+    """
+    if not mapping:
+        return {}
+    # for opt, value in settings.items():
+    #     print(opt, value)
+    #     print(type(mapping[opt](value)))
+    return {
+        opt: mapping[opt](value) for (opt, value) in settings.items()
+    }
+
+
+def gather_settings(pre='DJANGO_') -> dict:
+    """
+    Collect dict of settings from env variables.
+    """
+    import os
+    if pre:
+        return {
+            k[len(pre):]: v for (k, v) in os.environ.items() if
+            k.startswith(pre)
+        }
+    return {
+        k: v for (k, v) in os.environ.items()
+    }
