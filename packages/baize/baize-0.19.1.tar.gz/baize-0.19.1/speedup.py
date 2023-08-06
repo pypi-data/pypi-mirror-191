05f6452f4f673006ede02b419913d3c5216874d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:23:12 +0000

    Add package baize/0.19.1/baize-0.19.1.tar.gz

---
+++ packages/baize/baize-0.19.1.tar.gz/baize-0.19.1/speedup.py
@@ -0,0 +1,42 @@
+import os
+from pathlib import Path
+
+if os.environ.get("WITHOUT_MYPYC", "False") != "False":
+
+    def build(setup_kwargs):
+        pass
+
+else:
+
+    def build(setup_kwargs):
+        try:
+            from mypyc.build import mypycify
+        except ImportError:
+            print("Error in import mypyc.build, skip build.", flush=True)
+            return
+
+        modules = list(
+            filter(
+                lambda path: path.replace("\\", "/")
+                not in (
+                    "baize/multipart_helper.py",
+                    # ASGI
+                    "baize/asgi/requests.py",
+                    "baize/asgi/responses.py",
+                    "baize/asgi/routing.py",
+                    "baize/asgi/staticfiles.py",
+                    "baize/asgi/websocket.py",
+                    # WSGI
+                    "baize/wsgi/requests.py",
+                    "baize/wsgi/responses.py",
+                    "baize/wsgi/routing.py",
+                    "baize/wsgi/staticfiles.py",
+                ),
+                map(str, Path("baize").glob("**/*.py")),
+            )
+        )
+        setup_kwargs.update(
+            {
+                "ext_modules": mypycify(["--ignore-missing-imports", *modules]),
+            }
+        )
