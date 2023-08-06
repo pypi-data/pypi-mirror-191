542a407e959bd9d8914a436fa40c47747c4ececb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:00 +0000

    Add package railroad-diagrams/3.0.1/railroad-diagrams-3.0.1.tar.gz

---
+++ packages/railroad-diagrams/railroad-diagrams-3.0.1.tar.gz/railroad-diagrams-3.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README-py.md", "r") as fh:
+    long_description = fh.read()
+with open("semver.txt", "r") as fh:
+    semver = fh.read().strip()
+
+setup(
+    name='railroad-diagrams',
+    py_modules=['railroad'],
+    version=semver,
+    description='Generate SVG railroad syntax diagrams, like on JSON.org.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    author='Tab Atkins',
+    author_email='jackalmage@gmail.com',
+    url='https://github.com/tabatkins/railroad-diagrams',
+    keywords=['diagrams', 'syntax', 'grammar', 'railroad diagrams'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+    python_requires=">=3.7",
+)
