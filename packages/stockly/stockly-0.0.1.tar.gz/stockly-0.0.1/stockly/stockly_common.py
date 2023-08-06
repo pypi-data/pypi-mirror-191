fa88a38a8a5d2619cdc9492ea88ffd21af7d3c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:42 +0000

    Add package stockly/0.0.1/stockly-0.0.1.tar.gz

---
+++ packages/stockly/stockly-0.0.1.tar.gz/stockly-0.0.1/stockly/stockly_common.py
@@ -0,0 +1,7 @@
+
+class StocklyCommon:
+    def __init__(self, a) -> None:
+        self.a = a
+    
+    def onion(self):
+        return f"Onion is very exoecnsive these dayas. It cost around {self.a}Rs per Kg."
---
+++ packages/stockly/stockly-0.0.1.tar.gz/stockly-0.0.1/tests/test_stockly_common.py
@@ -0,0 +1,5 @@
+# from stockly.stockly_common import StocklyCommon
+
+# obj = StocklyCommon(4578)
+
+# obj.onion()
