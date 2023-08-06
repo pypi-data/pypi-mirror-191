7421d5e5c9c30d4207d0f7a264c493c18298929e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:22 +0000

    Add package random-generate/0.1.1/random_generate-0.1.1.tar.gz

---
+++ packages/random-generate/random_generate-0.1.1.tar.gz/random_generate-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='random_generate',
+    version='0.1.1',
+    description='This is a package created for random generating JSON, Numbers and passwords',
+    author='Varun Katiyar',
+    author_email='varunkatiyar819@gmail.com',
+    url='https://github.com/varunkatiyar819/random_generate',
+    packages=find_packages(),
+    install_requires=[
+       
+    ],
+)
