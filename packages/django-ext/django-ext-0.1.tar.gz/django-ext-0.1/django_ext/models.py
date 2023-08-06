6ab7a9c1b3cb271e81c9db67aa367981a34b4743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:44 +0000

    Add package django-ext/0.1/django-ext-0.1.tar.gz

---
+++ packages/django-ext/django-ext-0.1.tar.gz/django-ext-0.1/django_ext/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_currentuser.db.models import CurrentUserField
+
+
+class TimestampModel(models.Model):
+    created_at = models.DateTimeField(_('created_at'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated_at'), auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class TimeRangeModel(models.Model):
+    started_at = models.DateTimeField(_('started_at'), null=True, blank=True)
+    ended_at = models.DateTimeField(_('ended_at'), null=True, blank=True)
+
+
+class UserTrackModel(models.Model):
+    created_by = CurrentUserField(on_delete=models.PROTECT, verbose_name=_('created_by'))
+    updated_by = CurrentUserField(on_delete=models.PROTECT, on_update=True, verbose_name=_('updated_by'))
+
+    class Meta:
+        abstract = True
