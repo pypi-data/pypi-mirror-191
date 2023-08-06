6b614bee7934b72700cd8ff5e3f0eb78039a5339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:51 +0000

    Add package utype/0.2.2/utype-0.2.2-py3-none-any.whl

---
+++ packages/utype/utype-0.2.2-py3-none-any.whl/utype/__init__.py
@@ -0,0 +1,38 @@
+from .decorator import apply, dataclass, handle, parse, raw
+from .parser.field import Field, Param
+from .parser.options import Options
+from .parser.rule import Lax, Rule
+from .schema import DataClass, LogicalMeta, Schema
+from .utils import exceptions as exc
+from .utils.encode import register_encoder
+from .utils.transform import (TypeTransformer, type_transform)
+
+register_transformer = TypeTransformer.registry.register
+
+
+VERSION = (0, 2, 2, None)
+
+
+def _get_version():
+    pre_release = VERSION[3] if len(VERSION) > 3 else ""
+    version = ".".join([str(v) for v in VERSION[:3]])
+    if pre_release:
+        version += f"-{pre_release}"
+    return version
+
+
+__version__ = _get_version()
+
+
+def version_info() -> str:
+    import platform
+    import sys
+    from pathlib import Path
+
+    info = {
+        'utype version': __version__,
+        'installed path': Path(__file__).resolve().parent,
+        'python version': sys.version,
+        'platform': platform.platform(),
+    }
+    return '\n'.join('{:>30} {}'.format(k + ':', str(v).replace('\n', ' ')) for k, v in info.items())
