f1767f095cb2740bf33679a1dd9d7d637c4dd715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:20 +0000

    Add package benchling-api-client/2.0.104/benchling_api_client-2.0.104.tar.gz

---
+++ packages/benchling-api-client/benchling_api_client-2.0.104.tar.gz/benchling_api_client-2.0.104/benchling_api_client/v2/stable/models/container_write_base.py
@@ -0,0 +1,160 @@
+from typing import Any, cast, Dict, List, Optional, Type, TypeVar, Union
+
+import attr
+
+from ..extensions import NotPresentError
+from ..models.fields import Fields
+from ..types import UNSET, Unset
+
+T = TypeVar("T", bound="ContainerWriteBase")
+
+
+@attr.s(auto_attribs=True, repr=False)
+class ContainerWriteBase:
+    """  """
+
+    _fields: Union[Unset, Fields] = UNSET
+    _name: Union[Unset, str] = UNSET
+    _parent_storage_id: Union[Unset, str] = UNSET
+    additional_properties: Dict[str, Any] = attr.ib(init=False, factory=dict)
+
+    def __repr__(self):
+        fields = []
+        fields.append("fields={}".format(repr(self._fields)))
+        fields.append("name={}".format(repr(self._name)))
+        fields.append("parent_storage_id={}".format(repr(self._parent_storage_id)))
+        fields.append("additional_properties={}".format(repr(self.additional_properties)))
+        return "ContainerWriteBase({})".format(", ".join(fields))
+
+    def to_dict(self) -> Dict[str, Any]:
+        fields: Union[Unset, Dict[str, Any]] = UNSET
+        if not isinstance(self._fields, Unset):
+            fields = self._fields.to_dict()
+
+        name = self._name
+        parent_storage_id = self._parent_storage_id
+
+        field_dict: Dict[str, Any] = {}
+        field_dict.update(self.additional_properties)
+        # Allow the model to serialize even if it was created outside of the constructor, circumventing validation
+        if fields is not UNSET:
+            field_dict["fields"] = fields
+        if name is not UNSET:
+            field_dict["name"] = name
+        if parent_storage_id is not UNSET:
+            field_dict["parentStorageId"] = parent_storage_id
+
+        return field_dict
+
+    @classmethod
+    def from_dict(cls: Type[T], src_dict: Dict[str, Any], strict: bool = False) -> T:
+        d = src_dict.copy()
+
+        def get_fields() -> Union[Unset, Fields]:
+            fields: Union[Unset, Union[Unset, Fields]] = UNSET
+            _fields = d.pop("fields")
+
+            if not isinstance(_fields, Unset):
+                fields = Fields.from_dict(_fields)
+
+            return fields
+
+        try:
+            fields = get_fields()
+        except KeyError:
+            if strict:
+                raise
+            fields = cast(Union[Unset, Fields], UNSET)
+
+        def get_name() -> Union[Unset, str]:
+            name = d.pop("name")
+            return name
+
+        try:
+            name = get_name()
+        except KeyError:
+            if strict:
+                raise
+            name = cast(Union[Unset, str], UNSET)
+
+        def get_parent_storage_id() -> Union[Unset, str]:
+            parent_storage_id = d.pop("parentStorageId")
+            return parent_storage_id
+
+        try:
+            parent_storage_id = get_parent_storage_id()
+        except KeyError:
+            if strict:
+                raise
+            parent_storage_id = cast(Union[Unset, str], UNSET)
+
+        container_write_base = cls(
+            fields=fields,
+            name=name,
+            parent_storage_id=parent_storage_id,
+        )
+
+        container_write_base.additional_properties = d
+        return container_write_base
+
+    @property
+    def additional_keys(self) -> List[str]:
+        return list(self.additional_properties.keys())
+
+    def __getitem__(self, key: str) -> Any:
+        return self.additional_properties[key]
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.additional_properties[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        del self.additional_properties[key]
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.additional_properties
+
+    def get(self, key, default=None) -> Optional[Any]:
+        return self.additional_properties.get(key, default)
+
+    @property
+    def fields(self) -> Fields:
+        if isinstance(self._fields, Unset):
+            raise NotPresentError(self, "fields")
+        return self._fields
+
+    @fields.setter
+    def fields(self, value: Fields) -> None:
+        self._fields = value
+
+    @fields.deleter
+    def fields(self) -> None:
+        self._fields = UNSET
+
+    @property
+    def name(self) -> str:
+        if isinstance(self._name, Unset):
+            raise NotPresentError(self, "name")
+        return self._name
+
+    @name.setter
+    def name(self, value: str) -> None:
+        self._name = value
+
+    @name.deleter
+    def name(self) -> None:
+        self._name = UNSET
+
+    @property
+    def parent_storage_id(self) -> str:
+        """ ID of containing parent inventory, can also specify a coordinate for plates and boxes (e.g. plt_2bAks9dx:a2). """
+        if isinstance(self._parent_storage_id, Unset):
+            raise NotPresentError(self, "parent_storage_id")
+        return self._parent_storage_id
+
+    @parent_storage_id.setter
+    def parent_storage_id(self, value: str) -> None:
+        self._parent_storage_id = value
+
+    @parent_storage_id.deleter
+    def parent_storage_id(self) -> None:
+        self._parent_storage_id = UNSET
