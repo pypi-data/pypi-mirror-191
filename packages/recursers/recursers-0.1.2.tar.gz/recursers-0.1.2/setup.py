061d1d7a8bb822d0acb024e36d78ee73d19424c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:20 +0000

    Add package recursers/0.1.2/recursers-0.1.2.tar.gz

---
+++ packages/recursers/recursers-0.1.2.tar.gz/recursers-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='recursers',
+    version='0.1.2',    
+    description='Reduce VRAM usage on transformer models',
+    url='',
+    author='Max Ng',
+    author_email='maxnghello@gmail.com',
+    license='',
+    packages=['recursers'],
+    install_requires=["accelerate>=0.16.0",
+                        "tiktoken>=0.1.2",
+                        "torch>=1.12.1",
+                        "transformers>=4.26.0"               
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.10'
+    ],
+)
