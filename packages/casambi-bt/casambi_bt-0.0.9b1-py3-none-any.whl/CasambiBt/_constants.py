87a22cfe9fdc72e6b7cb4f93632eeeb604d51ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:06 +0000

    Add package casambi-bt/0.0.9b1/casambi_bt-0.0.9b1-py3-none-any.whl

---
+++ packages/casambi-bt/casambi_bt-0.0.9b1-py3-none-any.whl/CasambiBt/_constants.py
@@ -0,0 +1,10 @@
+import os
+from pathlib import Path
+from typing import Final
+
+BASE_PATH: Final = Path(os.getcwd()) / "casambi-bt-store"
+
+DEVICE_NAME: Final = "Casambi BT Python"
+
+CASA_UUID: Final = "0000fe4d-0000-1000-8000-00805f9b34fb"
+CASA_AUTH_CHAR_UUID: Final = "c9ffde48-ca5a-0001-ab83-8f519b482f77"
