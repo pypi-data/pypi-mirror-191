cf7a784d39e2c8e6e6d5f8b072015658d9753a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:36 +0000

    Add package python-deco/0.3.11/python-deco-0.3.11.tar.gz

---
+++ packages/python-deco/python-deco-0.3.11.tar.gz/python-deco-0.3.11/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyDeco', 'pyDeco.cli', 'pyDeco.dev', 'pyDeco.dev.debug', 'pyDeco.time']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0', 'pytest>=7.2.1,<8.0.0']
+
+setup_kwargs = {
+    'name': 'python-deco',
+    'version': '0.3.11',
+    'description': '',
+    'long_description': '# py_decorators\n\nAn OSS that has something to do with decorators in python\n\n# credits\n\nhttps://bytepawn.com/python-decorators-for-data-scientists.html\n',
+    'author': 'adrian',
+    'author_email': 'tamkayeung.adrian@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
