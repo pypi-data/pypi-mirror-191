0e76ad217d254d49933f0b1a17fee3d06d2bcd64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:22:00 +0000

    Add package jeng/0.0.9/jeng-0.0.9.tar.gz

---
+++ packages/jeng/jeng-0.0.9.tar.gz/jeng-0.0.9/note.md
@@ -0,0 +1,1 @@
+coverage xml && sonar-scanner.bat -D"sonar.projectKey=jeng" -D"sonar.sources=." -D"sonar.host.url=http://localhost:9000" -D"sonar.login=sqp_e08265acaccfb9234538686b1eb04d32706657dd"
---
+++ packages/jeng/jeng-0.0.9.tar.gz/jeng-0.0.9/setup.py
@@ -0,0 +1,50 @@
+from setuptools import find_packages, setup
+
+with open("readme.md", "r") as doc:
+    long_description = doc.read()
+
+setup(
+    name="jeng",
+    version="0.0.9",
+    license="MIT",
+    description="A simple WITSML client with utilities.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Nazeb Zurati",
+    author_email="nazeb04@gmail.com",
+    url="https://github.com/nazebzurati/jeng",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    py_modules=[
+        "jeng",
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    keywords=[
+        "python",
+        "witsml",
+        "soap-client",
+        "pandas",
+    ],
+    install_requires=[
+        "zeep>=4.2.1",
+        "xmltodict>=0.13.0",
+        "pandas>=1.5.2",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=7.2.0",
+            "pytest-dependency>=0.5.1",
+            "pytest-env>=0.8.1",
+            "black>=22.12.0",
+            "isort>=5.11.4",
+            "check-manifest>=0.49",
+            "twine>=4.0.2",
+            "coverage>=7.0.1",
+        ],
+    },
+)
