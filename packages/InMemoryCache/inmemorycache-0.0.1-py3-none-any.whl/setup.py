6691a42886dfacf1befe53ae1e872cd172c70312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:20 +0000

    Add package InMemoryCache/0.0.1/inmemorycache-0.0.1-py3-none-any.whl

---
+++ packages/InMemoryCache/inmemorycache-0.0.1-py3-none-any.whl/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cache',
+    version='1.0.2',
+    description='',
+    author='Amirreza Riahi',
+    author_email='riahiamirreza79@gmail.com',
+    url='',
+    packages=['cache'],
+)
