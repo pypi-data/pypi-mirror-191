bc515fad7f8c4105d9f94149739d1b0a5a37b9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:53 +0000

    Add package anymail-history/0.1.3/anymail_history-0.1.3.tar.gz

---
+++ packages/anymail-history/anymail_history-0.1.3.tar.gz/anymail_history-0.1.3/anymail_history/apps.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from django.apps import AppConfig
+
+
+class MyAppConfig(AppConfig):
+    name = "anymail_history"
+    verbose_name = "Anymail History"
+
+    default_auto_field = "django.db.models.BigAutoField"
+
+    def ready(self) -> None:
+        from . import receivers  # noqa: F401
