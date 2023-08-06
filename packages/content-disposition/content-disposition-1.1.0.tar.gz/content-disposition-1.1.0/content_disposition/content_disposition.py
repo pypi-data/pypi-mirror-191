e405d2d72a4de2b468bc7b67668c417fd34ae3b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:42 +0000

    Add package content-disposition/1.1.0/content-disposition-1.1.0.tar.gz

---
+++ packages/content-disposition/content-disposition-1.1.0.tar.gz/content-disposition-1.1.0/content_disposition/content_disposition.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import unicodedata
+from urllib.parse import quote
+
+__all__ = [
+    "rfc5987_content_disposition",
+]
+
+
+def rfc5987_content_disposition(file_name, disposition_type="inline"):
+    """
+    Set content headers according to RFC 5987
+
+    :param file_name: str|unicode
+    :param disposition_type: str
+    :return:
+    """
+    ascii_name = unicodedata.normalize("NFKD", file_name).encode("ascii", "ignore").decode()
+    header = '{}; filename="{}"'.format(disposition_type, ascii_name)
+    if ascii_name != file_name:
+        quoted_name = quote(file_name)
+        header += "; filename*=UTF-8''{}".format(quoted_name)
+
+    return header
