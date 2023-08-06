cc0ab6a88b5fe92499101e3f33ba63931c0e932d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:14 +0000

    Add package hvm/0.2.15/hvm-0.2.15-py3-none-manylinux1_x86_64.whl

---
+++ packages/hvm/hvm-0.2.15-py3-none-manylinux1_x86_64.whl/hercules/include/hercules/pipeline/signature_constants.h
@@ -0,0 +1,12 @@
+// Copyright 2022 The Hercules Authors.
+#pragma once
+
+namespace hercules {
+namespace runtime {
+namespace constants {
+
+constexpr char kServingOutputSignatureDefKey[] = "output_names";
+
+}  // namespace constants
+}  // namespace runtime
+}  // namespace hercules
