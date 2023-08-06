a398d9b7a852dc67fa6ad69b098f5ee3f794584c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:59 +0000

    Add package MyyyyyyCalculator/0.1/MyyyyyyCalculator-0.1-py3-none-any.whl

---
+++ packages/MyyyyyyCalculator/MyyyyyyCalculator-0.1-py3-none-any.whl/MyyyyyyCalculator/__init__.py
@@ -0,0 +1,53 @@
+"""This File contains all the methods of Calculator."""
+
+
+def addition(a: int, b: int):
+    """Addition Method.
+
+    Args:
+        a (Int): First number
+        b (int): Second Number
+
+    Returns:
+        int: sum of a and b
+    """
+    return a + b
+
+
+def multiplication(a: int, b: int):
+    """Multiplication Method.
+
+    Args:
+        a (int): Multiplicand
+        b (int): Multiplier
+
+    Returns:
+        int : Multiplication of a and b
+    """
+    return a * b
+
+
+def Subtraction(a: int, b: int):
+    """Subtraction Method.
+
+    Args:
+        a (int): First Number
+        b (int): Second Number
+
+    Returns:
+        int: Subtraction of a and b
+    """
+    return a - b
+
+
+def division(a: int, b: int):
+    """Division Method.
+
+    Args:
+        a (int): Dividend
+        b (int): Divisor
+
+    Returns:
+        float: The result of division
+    """
+    return a/b
