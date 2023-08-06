1f615f79b4f31fa2901c31b2e37c49ac0d287df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:29:51 +0000

    Add package randomGenjnp/1.2.2/randomGenjnp-1.2.2.tar.gz

---
+++ packages/randomGenjnp/randomGenjnp-1.2.2.tar.gz/randomGenjnp-1.2.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='randomGenjnp',
+    version='1.2.2',
+    description='This is a package created for random generating JSON, Numbers and passwords',
+    author='Varun Katiyar',
+    author_email='varunkatiyar819@gmail.com',
+    url='https://github.com/varunkatiyar819/randomGenjnp',
+    packages=find_packages(),
+    install_requires=[
+       
+    ],
+)
