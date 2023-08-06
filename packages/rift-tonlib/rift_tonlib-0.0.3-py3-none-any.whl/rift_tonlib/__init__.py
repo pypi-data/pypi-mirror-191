c122b8cf2d86ca4a16b87e2f3f664cd5d87f1d07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:13 +0000

    Add package rift-tonlib/0.0.3/rift_tonlib-0.0.3-py3-none-any.whl

---
+++ packages/rift-tonlib/rift_tonlib-0.0.3-py3-none-any.whl/rift_tonlib/__init__.py
@@ -0,0 +1,10 @@
+from rift_tonlib.client import TonlibClient
+from rift_tonlib.tonlibjson import (
+    BlockDeleted,
+    BlockNotFound,
+    ExternalMessageNotAccepted,
+    LiteServerTimeout,
+    TonlibError,
+    TonlibException,
+    TonlibNoResponse,
+)
