eb3e6577c4fdac9118043cb00730dedacabbc8a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:56 +0000

    Add package rhttp-python/0.0.1/rhttp_python-0.0.1.tar.gz

---
+++ packages/rhttp-python/rhttp_python-0.0.1.tar.gz/rhttp_python-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='rhttp_python',
+    version='0.0.1',
+    description='RHTTP python interface',
+    url='https://github.com/pedramcode/RHTTP-python',
+    author='Pedram Dehghanpour',
+    author_email='dev.dehghanpour@gmail.com',
+    license='MIT',
+    packages=['rhttp_python'],
+    install_requires=['redis'],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
+        'Topic :: System :: Networking',
+    ],
+)
