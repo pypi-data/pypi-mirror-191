ff5b375ca2e3edff69f6d2f9cd35af28a646f637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:10 +0000

    Add package PyCustomFocus/2.3/PyCustomFocus-2.3.tar.gz

---
+++ packages/PyCustomFocus/PyCustomFocus-2.3.tar.gz/PyCustomFocus-2.3/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+setup(
+  name = 'PyCustomFocus',         
+  packages = ['PyFocus'],   
+  version = '2.3',      
+  license='MIT',        
+  description = 'Full vectorial calculation of focused electromagnetic fields moduled by a custom phase mask',   
+  author = 'Caprile Fernando',                  
+  author_email = 'fcaprile@gmail.com',      
+  url = 'https://github.com/fcaprile/PyFocus',  
+  download_url = 'https://github.com/fcaprile/PyFocus/archive/refs/tags/2.3.tar.gz',    
+  keywords = ['User interface', 'Custom phase mask'],   # Keywords that define your package best
+  install_requires=[            
+],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
