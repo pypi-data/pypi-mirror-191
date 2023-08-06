44059ba556720ed6d8acae567ba98da01798c527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:37 +0000

    Add package TyroBot/2.0.4/TyroBot-2.0.4-py3-none-any.whl

---
+++ packages/TyroBot/TyroBot-2.0.4-py3-none-any.whl/TyroBot/__init__.py
@@ -0,0 +1,7 @@
+from .rubika import bot as Bot, bot_async as Bot_async , message as Message
+library_info = {
+    "version": "1.0.0",
+    "author": "mohammd saeed salari",
+    "github": "https://github.com/mrsalari/TyroBot",
+    "rubika": "@mr_tyro_code"
+}
