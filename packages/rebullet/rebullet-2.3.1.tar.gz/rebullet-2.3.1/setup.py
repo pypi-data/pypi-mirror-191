7d193a69df90d98a53c52ace1fa12f581e5a2aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:35 +0000

    Add package rebullet/2.3.1/rebullet-2.3.1.tar.gz

---
+++ packages/rebullet/rebullet-2.3.1.tar.gz/rebullet-2.3.1/setup.py
@@ -0,0 +1,26 @@
+""""Setup imports"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name="rebullet",
+    version="2.3.1",
+    description="Beautiful Python prompts made simple.",
+    long_description="README.md",
+    long_description_content_type="text/markdown",
+    url="https://github.com/h4rldev/rebullet",
+    keywords="cli list prompt customize colors",
+    author="bchao1, h4rldev and Maintainers",
+    license="MIT",
+    include_package_data=True,
+    packages=find_packages(),
+    python_requires=">=3.10",
+    install_requires=["python-dateutil"],
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
+
