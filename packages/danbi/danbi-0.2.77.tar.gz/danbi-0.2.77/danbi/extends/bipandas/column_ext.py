1e5c8f4cd2fe50c20725f958aef5c3ec35f689db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:23 +0000

    Add package danbi/0.2.77/danbi-0.2.77.tar.gz

---
+++ packages/danbi/danbi-0.2.77.tar.gz/danbi-0.2.77/danbi/extends/bipandas/column_ext.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+from typing import Union, List, Callable
+
+@pd.api.extensions.register_series_accessor("bi")
+class DanbiExtendSeries:
+    def __init__(self, column):
+        self._col = column
+        self._len = len(column)
+
+    def rollapply(self, window: int, func: Callable, test: int = 0, future: bool = False):
+        results = []
+        if test > 0:
+            for idx, df in enumerate(self._col.rolling(window)):
+                if len(df) < window:
+                    continue
+                if idx + 1 >= window + test:
+                    break
+                results.append(func(df))
+
+            return results
+        else:
+            for df in self._col.rolling(window):
+                if len(df) < window:
+                    continue
+                results.append(func(df))
+            
+            if len(results) > 0:
+                if isinstance(results[0], tuple):
+                    nan = [[np.nan for _ in range(len(results[0]))] for _ in range(window-1)]
+                else:
+                    nan = [np.nan for _ in range(window-1)]
+            
+            return results + nan if future else nan + results
+
+    def getStateUpDn(self, window: int, up_rate: float = 1, dn_rate: float = 1, future: bool = False) -> List:
+        if future:
+            ups = self._col == self._col.rolling(window).min().shift(-window+1)
+            dns = self._col == self._col.rolling(window).max().shift(-window+1)
+        else:
+            ups = self._col == self._col.rolling(window).min()
+            dns = self._col == self._col.rolling(window).max()
+        updn, term, is_up, is_change, s_val, e_val = [], [], False, False, 0, 0
+
+        for up, dn, val in zip(ups, dns, self._col):
+            if not np.isnan(val):
+                e_val = val
+            if is_change:
+                updn += term
+                term, is_change = [], False
+                s_val = val
+            term.append(is_up)
+
+            if not is_up and up: # dn -> up 으로 변환
+                is_up, is_change = True, True
+                if s_val != 0 and s_val * up_rate <= val: # dn상태가 rate_dn 조건을 만족하지 않음.
+                    term = list(np.logical_not(term))
+            elif is_up and dn: # up -> dn 으로 변환
+                is_up, is_change = False, True
+                if s_val != 0 and s_val * dn_rate > val: # up상태가 rate_up 조건을 만족하지 않음.
+                    term = list(np.logical_not(term))
+
+        if (is_up and s_val * rate > e_val) or (not is_up and s_val * rate <= e_val):
+            term = list(np.logical_not(term))
+        updn += term
+        
+        return np.array(updn)
+
+
+
+
