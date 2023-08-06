89a2fd38d4f2e5f2861573840b6a5792573337a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:44 +0000

    Add package mpl-sphinx-theme/3.7.0/mpl_sphinx_theme-3.7.0.tar.gz

---
+++ packages/mpl-sphinx-theme/mpl_sphinx_theme-3.7.0.tar.gz/mpl_sphinx_theme-3.7.0/CONTRIBUTING.md
@@ -0,0 +1,11 @@
+Matplotlib is a community maintained project. We welcome contributions in the form of bug reports, documentation, code, design proposals, and more.
+
+For general information on how to contribute see https://matplotlib.org/stable/devel/.
+
+Installing the theme itself:
+
+After following the contribution guidelines in the Matplotlib docs, you will still need to install the theme itself to see the changes you made. At the root level of the repository run:
+
+`python -m pip install -e .`
+
+After that you will be able to build the docs locally by doing `make html` in the `docs` directory.
---
+++ packages/mpl-sphinx-theme/mpl_sphinx_theme-3.7.0.tar.gz/mpl_sphinx_theme-3.7.0/docs/conf.py
@@ -0,0 +1,65 @@
+import os
+import time
+import datetime
+import matplotlib  # noqa: F401
+# Configuration file for the Sphinx documentation builder for
+# matplotlib projects.
+
+# Release mode enables optimizations and other related options.
+is_release_build = tags.has('release')  # noqa
+
+# Parse year using SOURCE_DATE_EPOCH, falling back to current time.
+# https://reproducible-builds.org/specs/source-date-epoch/
+build_date = datetime.datetime.utcfromtimestamp(
+    int(os.environ.get('SOURCE_DATE_EPOCH', time.time()))
+)
+
+# -- Project information -----------------------------------------------------
+
+project = "Matplotlib Sphinx Theme"
+copyright = (
+    f"2012 - {build_date.year} The Matplotlib development team"
+)
+author = "Matplotlib Developers"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'matplotlib.sphinxext.plot_directive',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "mpl_sphinx_theme"
+html_favicon = "_static/favicon.ico"
+html_theme_options = {
+    "logo": {"link": "https://matplotlib.org/stable/",
+             "image_light": "images/logo2.svg",
+             "image_dark": "images/logo_dark.svg"},
+    # collapse_navigation in pydata-sphinx-theme is slow, so skipped for local
+    # and CI builds https://github.com/pydata/pydata-sphinx-theme/pull/386
+    "collapse_navigation": not is_release_build,
+    "show_prev_next": False,
+    # Determines the type of links produced in the navigation header:
+    # - absolute: Links point to the URL https://matplotlib.org/...
+    # - server-stable: Links point to top-level of the server /stable/...
+    # - internal: Links point to the internal files as expanded by the `pathto`
+    #   template function in Sphinx.
+    "navbar_links": "absolute",
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ["static"]
