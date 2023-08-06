66ef222b2b7fe99ea73e436142ea05bdca0de513
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:25 +0000

    Add package stallion-python-sdk/1.2.4/stallion-python-sdk-1.2.4.tar.gz

---
+++ packages/stallion-python-sdk/stallion-python-sdk-1.2.4.tar.gz/stallion-python-sdk-1.2.4/src/stallion/__init__.py
@@ -0,0 +1,10 @@
+"""
+SDK for Stallion message broker clients.
+
+Check Stallion message broker: https://github.com/amirhnajafiz/stallion
+
+@version v1.2.4
+@author amirhnajafiz
+@email najafizadeh21@gmail.com
+@year 2022
+"""
