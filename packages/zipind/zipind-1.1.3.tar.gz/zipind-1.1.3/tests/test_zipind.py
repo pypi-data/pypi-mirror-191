272f92ba8b33a429e81ca9387ac2254bdf343574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:31 +0000

    Add package zipind/1.1.3/zipind-1.1.3.tar.gz

---
+++ packages/zipind/zipind-1.1.3.tar.gz/zipind-1.1.3/tests/test_zipind.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""Tests for `zipind` package."""
+
+
+import unittest
+from click.testing import CliRunner
+
+from zipind import zipind
+from zipind import cli
+
+
+class TestZipind(unittest.TestCase):
+    """Tests for `zipind` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
+
+    def test_command_line_interface(self):
+        """Test the CLI."""
+        runner = CliRunner()
+        result = runner.invoke(cli.main)
+        assert result.exit_code == 0
+        assert 'zipind.cli.main' in result.output
+        help_result = runner.invoke(cli.main, ['--help'])
+        assert help_result.exit_code == 0
+        assert '--help  Show this message and exit.' in help_result.output
