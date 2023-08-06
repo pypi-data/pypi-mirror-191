5fa0ed770e31a60f0c69208fe4fb59ddd83f1a48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:20 +0000

    Add package onsite/0.3.0/onsite-0.3.0.tar.gz

---
+++ packages/onsite/onsite-0.3.0.tar.gz/onsite-0.3.0/onsite/observation.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+
+
+class Observation():
+    """
+    vehicle_info={
+        "ego":{
+            "x":,
+            "y":,
+            "v":,
+            "yaw":,
+            "width":,
+            "length":,
+        },
+        "0":{...},
+        ...
+    }
+    light_info =
+    test_setting = {
+            "t":,
+            "dt":,
+            "max_t",
+            "goal":,
+            "end":
+        }
+    """
+
+    def __init__(self):
+        self.vehicle_info = {
+            "ego": {
+                "x": -1,
+                "y": -1,
+                "v": -1,
+                "a": -1,
+                "yaw": -1,
+                "width": -1,
+                "length": -1
+            },
+        }
+        self.road_info = {}
+        self.light_info = None
+        self.test_setting = {
+            "scenario_name": "name",
+            "scenario_type": "replay",
+            "t": 0.00,
+            "dt": -1,
+            "max_t": -1,
+            "goal": {
+                "x": [-1, -1],
+                "y": [-1, -1]
+            },
+            "end": -1
+        }
+
+
+if __name__ == "__main__":
+    pass
