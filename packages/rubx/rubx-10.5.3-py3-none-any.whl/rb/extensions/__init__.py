c6c0047706f5cd80d3c7fb3aaba641e978da12ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:56 +0000

    Add package rubx/10.5.3/rubx-10.5.3-py3-none-any.whl

---
+++ packages/rubx/rubx-10.5.3-py3-none-any.whl/rb/extensions/__init__.py
@@ -0,0 +1,7 @@
+from .tools import (UpToDate, __Top, Clean,
+                    Scanner, Maker, Login, Thumbnail)
+from .installer import PyPi
+
+__all__ = ['UpToDate', '__Top', 'Clean',
+           'Scanner', 'Maker', 'Login',
+           'Thumbnail', 'PyPi']
