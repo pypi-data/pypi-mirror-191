a6282d5b2c7cf29de20bdca7fd51f0ab055e4b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:26 +0000

    Add package algo-auto-ml/0.1.6/algo-auto-ml-0.1.6.tar.gz

---
+++ packages/algo-auto-ml/algo-auto-ml-0.1.6.tar.gz/algo-auto-ml-0.1.6/algo/ml/__init__.py
@@ -0,0 +1,9 @@
+from .base import BaseAlgo
+from .regression import RegressionAlgo
+from .classification import ClassificationAlgo
+
+__all__ = [
+    BaseAlgo,
+    RegressionAlgo,
+    ClassificationAlgo
+]
