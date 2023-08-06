6812eaddfe5897a7c439b7feda1c5646c1dfabb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:12 +0000

    Add package streamlit-wallet-connect/0.1.0/streamlit-wallet-connect-0.1.0.tar.gz

---
+++ packages/streamlit-wallet-connect/streamlit-wallet-connect-0.1.0.tar.gz/streamlit-wallet-connect-0.1.0/wallet_connect/__init__.py
@@ -0,0 +1,46 @@
+import streamlit.components.v1 as components
+import streamlit as st
+import os
+
+
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join(parent_dir, "frontend/build")
+_wallet_connect = components.declare_component("wallet_connect", path=build_dir)
+# _wallet_connect = components.declare_component("wallet_connect", url="http://localhost:3001")
+
+# default contract address is for ocean token
+def wallet_connect(label, key=None, message="Connect Wallet", contract_address="0xCfDdA22C9837aE76E0faA845354f33C62E03653a", amount="0.01", to_address="", message_to_encrypt="", encrypted_string="", encrypted_symmetric_key="", auth_token_contract_address="", chain_name="polygon", contract_type="ERC1155", num_tokens="0", price=10, supply=100, uri="", token_id=0):
+    return _wallet_connect(
+        label=label,
+        default="not",
+        key=key,
+        message=message,
+        contract_address=contract_address,
+        amount=amount, to_address=to_address,
+        message_to_encrypt=message_to_encrypt,
+        encrypted_string=encrypted_string,
+        encrypted_symmetric_key=encrypted_symmetric_key,
+        auth_token_contract_address=auth_token_contract_address,
+        chain_name=chain_name,
+        contract_type=contract_type,
+        num_tokens=num_tokens,
+        price=price,
+        supply=supply,
+        uri=uri,
+        token_id=token_id
+        )
+
+
+wallet_button = wallet_connect(label="wallet", key="wallet")
+st.write(f"Wallet {wallet_button} connected.")
+
+button = wallet_connect(message="Create Token", label="create_token", key="create_token", price="0.01", supply=1000, uri="https://gateway.pinata.cloud/ipfs/QmZrFfBGmUmXYUVeTrKdKC1aFeBBEEXQPGhsJtX45GwCC5", chain_name="goerli")
+st.write(f"TokenId is {button}")
+
+mint_button = wallet_connect(message="Login Algovera", label="mint_and_login_algovera", key="mint_and_login_algovera", price="0.01", token_id="1", chain_name="goerli")
+
+if mint_button == True:
+    st.write("Logged in!")
+    st.image("dog.jpeg")
+else:
+    st.write("Not authorized to access this application.")
---
+++ packages/streamlit-wallet-connect/streamlit-wallet-connect-0.1.0.tar.gz/streamlit-wallet-connect-0.1.0/wallet_connect/frontend/build/precache-manifest.10cbbbcfa92f5f61a0f90ce5aca4fbf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8466a4c433da209207b32ff761c349ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e007c08981e811e93923",
+    "url": "./static/js/2.58bc7d7a.chunk.js"
+  },
+  {
+    "revision": "49c7851b7c64ecc9408709042dce2a90",
+    "url": "./static/js/2.58bc7d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "286e3b1a59a2c1544c68",
+    "url": "./static/js/main.f7c8e7f9.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.f7c8e7f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "675ebed85599bcec64c0",
+    "url": "./static/js/runtime-main.57a1a595.js"
+  }
+]);
