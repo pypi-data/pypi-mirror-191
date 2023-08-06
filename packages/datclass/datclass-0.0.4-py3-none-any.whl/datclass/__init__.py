6e3db267a745a3b94bb9f7f0cd469d455f179ce1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:43 +0000

    Add package datclass/0.0.4/datclass-0.0.4-py3-none-any.whl

---
+++ packages/datclass/datclass-0.0.4-py3-none-any.whl/datclass/__init__.py
@@ -0,0 +1,16 @@
+__title__ = 'datclass'
+__author__ = 'foyoux'
+__version__ = '0.0.4'
+__all__ = ['main', 'Nested', 'Extra']
+
+import argparse
+
+from datclass.datclass import Nested, Extra
+
+
+def main():
+    epilog = f'%(prog)s({__version__}) by foyoux(https://github.com/foyoux/datclass)'
+    parser = argparse.ArgumentParser(prog='datclass', description='', epilog=epilog)
+    parser.add_argument('-v', '--version', action='version', version=epilog)
+
+    parser.parse_args()
