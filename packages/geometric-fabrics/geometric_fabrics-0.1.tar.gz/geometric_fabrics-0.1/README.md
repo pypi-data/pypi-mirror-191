00d819e2e6d16711c809f6209aef43f344bf4dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:07 +0000

    Add package geometric-fabrics/0.1/geometric_fabrics-0.1.tar.gz

---
+++ packages/geometric-fabrics/geometric_fabrics-0.1.tar.gz/geometric_fabrics-0.1/README.md
@@ -0,0 +1,3 @@
+# Geometric Fabrics
+
+This repository is a python implementation of the paper Geometric Fabrics for Acceleration Based Design of Robot Motion paper.
---
+++ packages/geometric-fabrics/geometric_fabrics-0.1.tar.gz/geometric_fabrics-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(     
+    name='geometric_fabrics',
+    author='Daniel Marew',
+    author_email='dmarew@umass.edu',
+    version='0.1',     
+    install_requires=[         
+        'autograd',
+    ],
+)
