702587452717e8b5c8c1c741d518376de624e773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:57 +0000

    Add package django-fast-builder/0.2/django_fast_builder-0.2.tar.gz

---
+++ packages/django-fast-builder/django_fast_builder-0.2.tar.gz/django_fast_builder-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='django_fast_builder',
+      version='0.2',
+      description='Quick deployment of a Django project',
+      packages=['django_fast_builder'],
+      author_email='sergey_wilson@mail.ru',
+      zip_safe=False)
