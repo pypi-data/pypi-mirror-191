81fdd12f412af0959b45b0d38ef3abc564a6c477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:29 +0000

    Add package ipmike/0.1.2/ipmike-0.1.2.tar.gz

---
+++ packages/ipmike/ipmike-0.1.2.tar.gz/ipmike-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ipmike',
+    version='0.1.2',
+    description='A package for IP-related utilities',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/ipmike',
+    packages=find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
