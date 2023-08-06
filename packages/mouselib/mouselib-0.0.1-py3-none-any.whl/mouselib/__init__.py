7dda80081d6a5863a862c553bb560eace7994acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:36 +0000

    Add package mouselib/0.0.1/mouselib-0.0.1-py3-none-any.whl

---
+++ packages/mouselib/mouselib-0.0.1-py3-none-any.whl/mouselib/__init__.py
@@ -0,0 +1,14 @@
+def add_numbers(x, y):
+    return x + y
+
+def multiply_numbers(x, y):
+    return x * y
+
+def divide_numbers(x, y):
+    return x / y
+
+def subtract_numbers(x, y):
+    return x - y
+
+
+# print(add_numbers(1, 2))
