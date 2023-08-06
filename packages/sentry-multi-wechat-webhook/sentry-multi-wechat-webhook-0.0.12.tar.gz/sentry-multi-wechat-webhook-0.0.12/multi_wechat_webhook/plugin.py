821718a42f561de24d9cba7a65dd2c124f3e8e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:22 +0000

    Add package sentry-multi-wechat-webhook/0.0.12/sentry-multi-wechat-webhook-0.0.12.tar.gz

---
+++ packages/sentry-multi-wechat-webhook/sentry-multi-wechat-webhook-0.0.12.tar.gz/sentry-multi-wechat-webhook-0.0.12/multi_wechat_webhook/plugin.py
@@ -0,0 +1,81 @@
+# coding: utf-8
+import json
+import requests
+
+from django import forms
+from sentry.plugins.bases import notify
+
+WECHAT_WEBHOOK_URL = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={key}"
+
+
+class WechatWebhookOptionsForm(notify.NotificationConfigurationForm):
+    key = forms.CharField(
+        max_length=255,
+        help_text='默认企业微信webHookKey',
+        required=True
+    )
+    prod_key = forms.CharField(
+        max_length=255,
+        help_text="生产企业微信webHookKey,为空则通过默认Key发送消息",
+        required=False
+    )
+    noticeUser = forms.CharField(
+        max_length=255,
+        help_text="群里面@指定人,仅支持企业微信用户ID,提醒多人,号隔开",
+        required=False
+    )
+
+
+class MultiWechatWebhookPlugin(notify.NotificationPlugin):
+    author = 'badx'
+    author_url = 'https://github.com/badx/sentry-multi-webhook'
+    version = '0.0.12'
+    description = u'Sentry 企业微信 Webhook 插件'
+    resource_links = [
+        ('Source', 'https://github.com/badx/sentry-multi-webhook'),
+        ('Bug Tracker', 'https://github.com/badx/sentry-multi-webhook/issues'),
+        ('README', 'https://github.com/badx/sentry-multi-webhook/blob/master/README.md'),
+    ]
+
+    slug = 'multi_wechat_webhook'
+    title = 'Multi Wechat Webhook'
+    conf_key = slug
+    conf_title = title
+    project_conf_form = WechatWebhookOptionsForm
+
+    def is_configured(self, project, **kwargs):
+        return bool(self.get_option('key', project))
+
+    def notify_users(self, group, event, fail_silently=False, **kwargs):
+        key = self.get_option('key', group.project)
+        prod_key = self.get_option("prod_key", group.project)
+        noticeUser = self.get_option("noticeUser", group.project)
+        url = WECHAT_WEBHOOK_URL.format(key=key)
+        environment = event.get_tag("environment") or ""
+        slug = event.project.slug
+
+        if prod_key and slug.endswith("prod") or \
+                (environment and environment.startswith("prod")):
+            url = WECHAT_WEBHOOK_URL.format(key=prod_key)
+
+        title = u"有新的通知来自 {} 项目".format(slug +
+                                        (environment if (not slug.endswith("prod") and
+                                                         not slug.endswith("int")
+                                                         and environment) else ""))
+
+        data = {
+            "msgtype": "markdown",
+            "markdown": {
+                "content": u"#### {title} \n > {message} \n [查看]({url})".format(
+                    title=title,
+                    message=event.message,
+                    url=u"{}events/{}/".format(group.get_absolute_url(), event.event_id),
+                ) + ("\n<@{noticeUser}>".format(noticeUser=noticeUser.replace(",", ",@")) if noticeUser else "")
+            }
+        }
+
+        requests.post(
+            url=url,
+            headers={"Content-Type": "application/json"},
+            data=json.dumps(data).encode("utf-8")
+        )
