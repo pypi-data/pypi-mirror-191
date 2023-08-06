63f432c14c84c85045e01da5308a5d72cb2cb742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:22:46 +0000

    Add package dgg-bot/0.9.0/dgg-bot-0.9.0.tar.gz

---
+++ packages/dgg-bot/dgg-bot-0.9.0.tar.gz/dgg-bot-0.9.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="dgg-bot",
+    version="0.9.0",
+    author="Fritz",
+    description="A library for making a bot in Destiny.gg chat.",
+    long_description_content_type="text/x-rst",
+    url="https://github.com/Fritz-02/dgg-bot/",
+    project_urls={
+        "Bug Tracker": "https://github.com/Fritz-02/dgg-bot/issues",
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Communications :: Chat",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.9",
+)
