55059275875e73da319517493d472cd398baf17e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:27 +0000

    Add package damagescanner/0.6.0/damagescanner-0.6.0.tar.gz

---
+++ packages/damagescanner/damagescanner-0.6.0.tar.gz/damagescanner-0.6.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='damagescanner',
+    version='0.6.0',
+    install_requires=[
+    "geopandas",
+    "matplotlib",	
+    "numpy",
+    "pyproj",
+    "rasterio",
+    "xarray",
+    "rioxarray",
+    "packaging",
+    "pandas",
+    "shapely",
+    ]
+)
