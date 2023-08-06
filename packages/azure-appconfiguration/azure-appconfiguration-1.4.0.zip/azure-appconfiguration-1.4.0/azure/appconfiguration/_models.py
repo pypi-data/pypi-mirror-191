5986c309c44b739e9d86e3f6d1ccbd275e7a1108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:04 +0000

    Add package azure-appconfiguration/1.4.0/azure-appconfiguration-1.4.0.zip

---
+++ packages/azure-appconfiguration/azure-appconfiguration-1.4.0.zip/azure-appconfiguration-1.4.0/azure/appconfiguration/_models.py
@@ -0,0 +1,359 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+import json
+from typing import Any, Union, List, Dict, Optional
+from ._generated._serialization import Model
+from ._generated.models import KeyValue
+
+
+PolymorphicConfigurationSetting = Union[
+    "ConfigurationSetting", "SecretReferenceConfigurationSetting", "FeatureFlagConfigurationSetting"
+]
+
+
+class ConfigurationSetting(Model):
+    """A configuration value.
+    Variables are only populated by the server, and will be ignored when
+    sending a request.
+
+    :ivar value: The value of the configuration setting
+    :vartype value: str
+    :ivar etag: Entity tag (etag) of the object
+    :vartype etag: str
+    :param key:
+    :type key: str
+    :param label:
+    :type label: str
+    :param content_type:
+    :type content_type: str
+    :ivar last_modified:
+    :vartype last_modified: datetime
+    :ivar read_only:
+    :vartype read_only: bool
+    :param tags:
+    :type tags: Dict[str, str]
+    """
+
+    _attribute_map = {
+        "etag": {"key": "etag", "type": "str"},
+        "key": {"key": "key", "type": "str"},
+        "label": {"key": "label", "type": "str"},
+        "content_type": {"key": "content_type", "type": "str"},
+        "value": {"key": "value", "type": "str"},
+        "last_modified": {"key": "last_modified", "type": "iso-8601"},
+        "read_only": {"key": "read_only", "type": "bool"},
+        "tags": {"key": "tags", "type": "{str}"},
+    }
+
+    kind = "Generic"
+    content_type = None
+
+    def __init__(self, **kwargs) -> None:
+        super(ConfigurationSetting, self).__init__(**kwargs)
+        self.key = kwargs.get("key", None)
+        self.label = kwargs.get("label", None)
+        self.value = kwargs.get("value", None)
+        self.etag = kwargs.get("etag", None)
+        self.content_type = kwargs.get("content_type", self.content_type)
+        self.last_modified = kwargs.get("last_modified", None)
+        self.read_only = kwargs.get("read_only", None)
+        self.tags = kwargs.get("tags", {})
+
+    @classmethod
+    def _from_generated(cls, key_value: KeyValue) -> PolymorphicConfigurationSetting:
+        if key_value is None:
+            return key_value
+        if key_value.content_type is not None:
+            try:
+                if key_value.content_type.startswith(
+                    FeatureFlagConfigurationSetting._feature_flag_content_type  # pylint:disable=protected-access
+                ) and key_value.key.startswith(  # type: ignore
+                    FeatureFlagConfigurationSetting._key_prefix  # pylint: disable=protected-access
+                ):
+                    return FeatureFlagConfigurationSetting._from_generated(  # pylint: disable=protected-access
+                        key_value
+                    )
+                if key_value.content_type.startswith(
+                    SecretReferenceConfigurationSetting._secret_reference_content_type  # pylint:disable=protected-access
+                ):
+                    return SecretReferenceConfigurationSetting._from_generated(  # pylint: disable=protected-access
+                        key_value
+                    )
+            except (KeyError, AttributeError):
+                pass
+
+        return cls(
+            key=key_value.key,
+            label=key_value.label,
+            value=key_value.value,
+            content_type=key_value.content_type,
+            last_modified=key_value.last_modified,
+            tags=key_value.tags,
+            read_only=key_value.locked,
+            etag=key_value.etag,
+        )
+
+    def _to_generated(self) -> KeyValue:
+        return KeyValue(
+            key=self.key,
+            label=self.label,
+            value=self.value,
+            content_type=self.content_type,
+            last_modified=self.last_modified,
+            tags=self.tags,
+            locked=self.read_only,
+            etag=self.etag,
+        )
+
+
+class FeatureFlagConfigurationSetting(ConfigurationSetting): # pylint: disable=too-many-instance-attributes
+    """A feature flag configuration value.
+    Variables are only populated by the server, and will be ignored when
+    sending a request.
+
+    :ivar etag: Entity tag (etag) of the object
+    :vartype etag: str
+    :ivar feature_id:
+    :vartype feature_id: str
+    :ivar value: The value of the configuration setting
+    :vartype value: str
+    :keyword enabled:
+    :paramtype enabled: bool
+    :keyword filters:
+    :paramtype filters: List[Dict[str, Any]]
+    :param label:
+    :type label: str
+    :param display_name:
+    :type display_name: str
+    :param description:
+    :type description: str
+    :param content_type:
+    :type content_type: str
+    :ivar last_modified:
+    :vartype last_modified: datetime
+    :ivar read_only:
+    :vartype read_only: bool
+    :param tags:
+    :type tags: Dict[str, str]
+    """
+
+    _attribute_map = {
+        "etag": {"key": "etag", "type": "str"},
+        "feature_id": {"key": "feature_id", "type": "str"},
+        "label": {"key": "label", "type": "str"},
+        "content_type": {"key": "_feature_flag_content_type", "type": "str"},
+        "value": {"key": "value", "type": "str"},
+        "last_modified": {"key": "last_modified", "type": "iso-8601"},
+        "read_only": {"key": "read_only", "type": "bool"},
+        "tags": {"key": "tags", "type": "{str}"},
+    }
+    _key_prefix = ".appconfig.featureflag/"
+    _feature_flag_content_type = (
+        "application/vnd.microsoft.appconfig.ff+json;charset=utf-8"
+    )
+    kind = "FeatureFlag"
+
+    def __init__( # pylint: disable=super-init-not-called
+        self,
+        feature_id: str,
+        *,
+        enabled: Optional[bool] = None,
+        filters: Optional[List[Dict[str, Any]]] = None,
+        **kwargs
+    ) -> None:
+        if "key" in kwargs.keys() or "value" in kwargs.keys():
+            raise TypeError("Unexpected keyword argument, do not provide 'key' or 'value' as a keyword-arg")
+        self.feature_id = feature_id
+        self.key = self._key_prefix + self.feature_id
+        self.label = kwargs.get("label", None)
+        self.content_type = kwargs.get("content_type", self._feature_flag_content_type)
+        self.last_modified = kwargs.get("last_modified", None)
+        self.tags = kwargs.get("tags", {})
+        self.read_only = kwargs.get("read_only", None)
+        self.etag = kwargs.get("etag", None)
+        self.description = kwargs.get("description", None)
+        self.display_name = kwargs.get("display_name", None)
+        self.filters = [] if filters is None else filters
+        self.enabled = enabled
+        self._value = json.dumps({"enabled": self.enabled, "conditions": {"client_filters": self.filters}})
+
+    @property
+    def value(self):
+        try:
+            temp = json.loads(self._value)
+            temp["enabled"] = self.enabled
+
+            if "conditions" not in temp.keys():
+                temp["conditions"] = {}
+            temp["conditions"]["client_filters"] = self.filters
+            self._value = json.dumps(temp)
+            return self._value
+        except (json.JSONDecodeError, ValueError):
+            return self._value
+
+    @value.setter
+    def value(self, new_value):
+        try:
+            temp = json.loads(new_value)
+            self._value = new_value
+            self.enabled = temp.get("enabled", None)
+            self.filters = None
+            conditions = temp.get("conditions", None)
+            if conditions:
+                self.filters = conditions.get("client_filters", None)
+        except (json.JSONDecodeError, ValueError):
+            self._value = new_value
+            self.enabled = None
+            self.filters = None
+
+    @classmethod
+    def _from_generated(cls, key_value: KeyValue) -> Union["FeatureFlagConfigurationSetting", ConfigurationSetting]:
+        if key_value is None:
+            return key_value
+        enabled = None
+        filters = None
+        try:
+            temp = json.loads(key_value.value)  # type: ignore
+            if isinstance(temp, dict):
+                enabled = temp.get("enabled")
+                if "conditions" in temp.keys():
+                    filters = temp["conditions"].get("client_filters")
+        except (ValueError, json.JSONDecodeError):
+            pass
+
+        return cls(
+            feature_id=key_value.key.lstrip(".appconfig.featureflag").lstrip("/"),  # type: ignore
+            label=key_value.label,
+            content_type=key_value.content_type,
+            last_modified=key_value.last_modified,
+            tags=key_value.tags,
+            read_only=key_value.locked,
+            etag=key_value.etag,
+            enabled=enabled,
+            filters=filters
+        )
+
+    def _to_generated(self) -> KeyValue:
+        return KeyValue(
+            key=self.key,
+            label=self.label,
+            value=self.value,
+            content_type=self.content_type,
+            last_modified=self.last_modified,
+            tags=self.tags,
+            locked=self.read_only,
+            etag=self.etag,
+        )
+
+
+class SecretReferenceConfigurationSetting(ConfigurationSetting):
+    """A configuration value that references a KeyVault Secret
+    Variables are only populated by the server, and will be ignored when
+    sending a request.
+
+    :ivar etag: Entity tag (etag) of the object
+    :vartype etag: str
+    :ivar key:
+    :vartype key: str
+    :ivar secret_id:
+    :vartype secret_id: str
+    :param label:
+    :type label: str
+    :param content_type:
+    :type content_type: str
+    :ivar value: The value of the configuration setting
+    :vartype value: Dict[str, Any]
+    :ivar last_modified:
+    :vartype last_modified: datetime
+    :ivar read_only:
+    :vartype read_only: bool
+    :param tags:
+    :type tags: Dict[str, str]
+    """
+
+    _attribute_map = {
+        "etag": {"key": "etag", "type": "str"},
+        "key": {"key": "key", "type": "str"},
+        "label": {"key": "label", "type": "str"},
+        "content_type": {"key": "content_type", "type": "str"},
+        "value": {"key": "value", "type": "str"},
+        "last_modified": {"key": "last_modified", "type": "iso-8601"},
+        "read_only": {"key": "read_only", "type": "bool"},
+        "tags": {"key": "tags", "type": "{str}"},
+    }
+    _secret_reference_content_type = (
+        "application/vnd.microsoft.appconfig.keyvaultref+json;charset=utf-8"
+    )
+    kind = "SecretReference"
+
+    def __init__(self, key: str, secret_id: str, **kwargs) -> None: # pylint: disable=super-init-not-called
+        if "value" in kwargs.keys():
+            raise TypeError("Unexpected keyword argument, do not provide 'value' as a keyword-arg")
+        self.key = key
+        self.label = kwargs.pop("label", None)
+        self.content_type = kwargs.get(
+            "content_type", self._secret_reference_content_type
+        )
+        self.etag = kwargs.get("etag", None)
+        self.last_modified = kwargs.get("last_modified", None)
+        self.read_only = kwargs.get("read_only", None)
+        self.tags = kwargs.get("tags", {})
+        self.secret_id = secret_id
+        self._value = json.dumps({"uri": secret_id})
+
+    @property
+    def value(self):
+        try:
+            temp = json.loads(self._value)
+            temp["uri"] = self.secret_id
+            self._value = json.dumps(temp)
+            return self._value
+        except (json.JSONDecodeError, ValueError):
+            return self._value
+
+    @value.setter
+    def value(self, new_value):
+        try:
+            temp = json.loads(new_value)
+            self._value = new_value
+            self.secret_id = temp.get("uri")
+        except(json.JSONDecodeError, ValueError):
+            self._value = new_value
+            self.secret_id = None
+
+    @classmethod
+    def _from_generated(cls, key_value: KeyValue) -> "SecretReferenceConfigurationSetting":
+        if key_value is None:
+            return key_value
+        secret_uri = None
+        try:
+            temp = json.loads(key_value.value)  # type: ignore
+            secret_uri = temp.get("uri")
+            if not secret_uri:
+                secret_uri = temp.get("secret_uri")
+        except (ValueError, json.JSONDecodeError):
+            pass
+
+        return cls(
+            key=key_value.key,  # type: ignore
+            label=key_value.label,
+            secret_id=secret_uri,  # type: ignore
+            last_modified=key_value.last_modified,
+            tags=key_value.tags,
+            read_only=key_value.locked,
+            etag=key_value.etag,
+        )
+
+    def _to_generated(self) -> KeyValue:
+        return KeyValue(
+            key=self.key,
+            label=self.label,
+            value=self.value,
+            content_type=self.content_type,
+            last_modified=self.last_modified,
+            tags=self.tags,
+            locked=self.read_only,
+            etag=self.etag,
+        )
---
+++ packages/azure-appconfiguration/azure-appconfiguration-1.4.0.zip/azure-appconfiguration-1.4.0/samples/README.md
@@ -0,0 +1,45 @@
+---
+page_type: sample
+languages:
+  - python
+products:
+  - azure
+  - azure-app-configuration
+---
+
+# Azure App Configuration Data Library Python Samples
+
+## Prerequisites
+
+* Python 3.7 or later is required to use this package.
+* You must have an [Azure subscription][azure_sub], and a [Configuration Store][configuration_store] to use this package.
+
+To create a Configuration Store, you can either use [Azure Portal](https://ms.portal.azure.com/#create/Microsoft.Azconfig) or if you are using [Azure CLI][azure_cli] you can simply run the following snippet in your console:
+
+```Powershell
+az appconfig create --name <config-store-name> --resource-group <resource-group-name> --location eastus
+```
+
+## Setup
+
+Install the Azure App Configuration client library for Python with pip:
+
+```commandline
+pip install azure-appconfiguration
+```
+
+## Contents
+
+| File | Description |
+|-------------|-------------|
+| hello_world_sample.py / hello_world_sample_async.py       | demos set/get/delete operations |
+| hello_world_advanced_sample.py / hello_world_advanced_sample_async.py | demos add/set with label/list operations |
+| conditional_operation_sample.py / conditional_operation_sample_async.py | demos conditional set/get/delete operations |
+| read_only_sample.py / read_only_sample_async.py | demos set_read_only operations |
+| list_revision_sample.py / list_revision_sample_async.py | demos list revision operations |
+| sync_token_samples.py / sync_token_sample_asyncs.py | demos the `update_sync_token` method |
+
+<!-- LINKS -->
+[azure_sub]: https://azure.microsoft.com/free/
+[azure_cli]: https://docs.microsoft.com/cli/azure
+[configuration_store]: https://azure.microsoft.com/services/app-configuration/
