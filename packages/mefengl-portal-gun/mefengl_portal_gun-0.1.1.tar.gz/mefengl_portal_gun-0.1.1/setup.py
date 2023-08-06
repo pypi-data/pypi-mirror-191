f05c702731342e7044bc0ff8e1ae837ac1858144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:36 +0000

    Add package mefengl-portal-gun/0.1.1/mefengl_portal_gun-0.1.1.tar.gz

---
+++ packages/mefengl-portal-gun/mefengl_portal_gun-0.1.1.tar.gz/mefengl_portal_gun-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mefengl_portal_gun']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['typer[all]>=0.7.0,<0.8.0']
+
+entry_points = \
+{'console_scripts': ['mefengl-portal-gun = mefengl_portal_gun.main:app']}
+
+setup_kwargs = {
+    'name': 'mefengl-portal-gun',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '# Portal Gun\n\nThe awesome Portal Gun\n',
+    'author': '冯不游',
+    'author_email': 'meme.fenglei@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
