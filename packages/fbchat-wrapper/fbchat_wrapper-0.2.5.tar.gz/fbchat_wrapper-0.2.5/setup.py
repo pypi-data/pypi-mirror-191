6651df1bbf98809dba0f9c3a9a2ded8f8be5380d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:26 +0000

    Add package fbchat-wrapper/0.2.5/fbchat_wrapper-0.2.5.tar.gz

---
+++ packages/fbchat-wrapper/fbchat_wrapper-0.2.5.tar.gz/fbchat_wrapper-0.2.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import os
+VERSION = '0.2.5'
+DESCRIPTION = 'Wrapper for fbchat'
+LONG_DESCRIPTION = 'Simple Wrapper Package to make Programming with fbchat easier and simpler to read.'
+
+# Setting up
+setup(
+    name="fbchat_wrapper",
+    version=VERSION,
+    author="SneznyKocur",
+    author_email="kocursnezny@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=["py-fbchat","validators","pillow","ffmpeg-python","wget","pytube"],
+    keywords=['python', 'messaging', 'wrapper'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
