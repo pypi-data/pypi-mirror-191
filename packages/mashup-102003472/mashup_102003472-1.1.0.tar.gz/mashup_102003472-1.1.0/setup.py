4a651ebc280ac3aa9a13b4d8318d5676fe593a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:33 +0000

    Add package mashup-102003472/1.1.0/mashup_102003472-1.1.0.tar.gz

---
+++ packages/mashup-102003472/mashup_102003472-1.1.0.tar.gz/mashup_102003472-1.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='mashup_102003472',
+    version='1.1.0',
+    author='Rohan Bhandula',
+    author_email='rbhandula_be20@thapar.edu',
+    description='A python package to create a mashup of N songs of your desired singer',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+],
+)
