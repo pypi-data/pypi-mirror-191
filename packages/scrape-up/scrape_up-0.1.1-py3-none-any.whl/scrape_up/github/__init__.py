be026b5c06fd841576e4712f9df9e31ccdd35483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:32 +0000

    Add package scrape-up/0.1.1/scrape_up-0.1.1-py3-none-any.whl

---
+++ packages/scrape-up/scrape_up-0.1.1-py3-none-any.whl/scrape_up/github/__init__.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class Users:
+
+    def __init__(self, username: str):
+        self.username = username
+
+    def __scrape_page(self):
+        username = self.username
+        data = requests.get(f"https://github.com/{username}")
+        data = BeautifulSoup(data.text, "html.parser")
+        return data
+
+    def followers(self):
+        """
+        Fetch the number of followers of a GitHub user.
+        """
+        page = self.__scrape_page()
+        try:
+            followers = page.find(class_="text-bold color-fg-default")
+            return followers.text
+        except:
+            message = f"{self.username} not found !"
+            return message
+
+    def get_avatar(self):
+        """
+        Fetch the avatar URL of a GitHub user.
+        """
+        page = self.__scrape_page()
+        try:
+            avatar = page.find(
+                class_="avatar avatar-user width-full border color-bg-default")
+            return avatar["src"]
+        except:
+            message = f"Avatart not found for username {self.username}"
+            return message
