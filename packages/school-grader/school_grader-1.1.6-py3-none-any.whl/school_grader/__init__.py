c2da1fa4acb43576480e133c0db626d47084b199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:45 +0000

    Add package school-grader/1.1.6/school_grader-1.1.6-py3-none-any.whl

---
+++ packages/school-grader/school_grader-1.1.6-py3-none-any.whl/school_grader/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "Marc-Olivier Derouin"
+__email__ = "marcolivier.derouin@poulet-frit.com"
+
+from school_grader.html_test_result import HTMLTestResult
+from school_grader.test_case import AlmostEqualNumber, FileTestCase, FunctionTestCase, run_test, TestCase, timeout
+from school_grader.run_all_test_recursively import run_all_test_recursively
