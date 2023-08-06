9b9c4f4490bac97bd60e09cbcd25fe6bcaccf30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:39 +0000

    Add package plaited/0.0.1/plaited-0.0.1.tar.gz

---
+++ packages/plaited/plaited-0.0.1.tar.gz/plaited-0.0.1/src/plaited/plaited.py
@@ -0,0 +1,27 @@
+import sys
+import json
+import panflute as pf
+from .main import Plait
+
+
+def main():
+    # a Pandoc filter should always be called with one argument: the target format
+    # https://pandoc.org/filters.html#arguments
+    if len(sys.argv) > 1:
+        filter_to = sys.argv[1]
+    else:
+        print("Warning: no target format specified", file=sys.stderr)
+        filter_to = "json"
+
+    # load JSON AST from standard input
+    plaiter = Plait(pf.load())
+
+    output_doc = plaiter.plait_ast()
+
+    # "dump" a panflute Doc element
+    # defaults to standard output, which is what we want
+    pf.dump(output_doc)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/plaited/plaited-0.0.1.tar.gz/plaited-0.0.1/tests/test_options.py
@@ -0,0 +1,52 @@
+import json
+import panflute as pf
+
+import pytest
+from textwrap import dedent
+from plaited import Plait
+
+
+@pytest.fixture
+def code_block_doc():
+    input_text = dedent(
+        """\
+    ---
+    title: My Title
+    standalone: False
+    plaited-options:
+      warning: False
+      error: raise
+    abstract: |
+      This is the abstract.
+
+      It consists of two paragraphs.
+    ---
+
+    # Hail and well met
+    """
+    )
+    input_doc = pf.convert_text(input_text, standalone=True)
+    p = Plait(input_doc)
+    out_doc = p.plait_ast()
+
+    assert p.get_option() is False
+
+
+def get_option(self, elem, option, default):
+    assert out_doc.error == "raise"
+    assert getattr(out_doc, "abstract", None) is None
+
+
+def test_fig_cap():
+    input_text = dedent(
+        """\
+    ```{python, fig.cap="This is a caption"}
+    import matplotlib.pyplot as plt
+    plt.plot(range(4), range(4))
+    ```"""
+    )
+    input_doc = pf.convert_text(input_text, standalone=True)
+    p = Plait(input_doc)
+    out_doc = p.plait_ast()
+    # assert result == "This is a caption"
+    assert True
