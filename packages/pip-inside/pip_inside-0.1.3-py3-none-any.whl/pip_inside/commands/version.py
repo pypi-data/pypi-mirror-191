22e62a6d65cbe23414e9cb8f0b03a328315dbc54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:27 +0000

    Add package pip-inside/0.1.3/pip_inside-0.1.3-py3-none-any.whl

---
+++ packages/pip-inside/pip_inside-0.1.3-py3-none-any.whl/pip_inside/commands/version.py
@@ -0,0 +1,15 @@
+from importlib.util import module_from_spec, spec_from_file_location
+
+import click
+
+from pip_inside.utils.pyproject import PyProject
+
+
+def handle_version(short: bool = False):
+    pyproject = PyProject.from_toml()
+    module = pyproject.get('project.name')
+    s = spec_from_file_location(module, f"{module.lower().replace('-', '_')}/__init__.py")
+    m = module_from_spec(s)
+    s.loader.exec_module(m)
+    version = m.__version__ if short else f"{module}: {m.__version__}"
+    click.secho(version, fg='bright_cyan')
