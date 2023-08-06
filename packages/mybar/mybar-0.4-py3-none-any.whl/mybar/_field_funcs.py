ad4d366536bddc2cfb73605b2b1b29fd0eea78ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:59 +0000

    Add package mybar/0.4/mybar-0.4-py3-none-any.whl

---
+++ packages/mybar/mybar-0.4-py3-none-any.whl/mybar/_field_funcs.py
@@ -0,0 +1,400 @@
+__all__ = (
+    'get_datetime',
+    'get_hostname',
+    'get_uptime',
+    'get_cpu_usage',
+    'get_cpu_temp',
+    'get_mem_usage',
+    'get_disk_usage',
+    'get_battery_info',
+    'get_net_stats',
+    # 'get_audio_volume'
+)
+
+
+import os
+import re
+import shlex
+import time
+from asyncio import subprocess as aiosp
+from datetime import datetime
+from string import Formatter
+
+import psutil
+
+from .errors import *
+from .formatable import ElapsedTime, DynamicFormatStr
+from .utils import join_options
+from ._types import Contents, FieldName, FormatStr
+
+from collections.abc import Callable, Iterable
+from typing import Literal, TypeAlias
+
+
+FormatterLiteral: TypeAlias = str|None
+FormatterFname: TypeAlias = str|None
+FormatterFormatSpec: TypeAlias = str|None
+FormatterConversion: TypeAlias = str|None
+FmtStrStructure: TypeAlias = tuple[tuple[tuple[
+    FormatterLiteral,
+    FormatterFname,
+    FormatterFormatSpec,
+    FormatterConversion
+]]]
+
+NmConnIDSpecifier: TypeAlias = Literal['id', 'uuid', 'path', 'apath']
+NmConnFilterSpec: TypeAlias = Iterable[NmConnIDSpecifier]
+
+Duration: TypeAlias = Literal['secs', 'mins', 'hours', 'days', 'weeks']
+
+POWERS_OF_1K = {
+    'G': 3,
+    'M': 2,
+    'K': 1
+}
+
+
+
+##class Func:
+##    f: Callable
+##    s: Setup
+##    pass
+
+# Field functions
+
+async def get_audio_volume(fmt="{:02.0f}{state}", *args, **kwargs) -> Contents:
+    '''Currently awaiting a more practical implementation that supports
+    instantaneous updates. A socket would be quite nice for that.'''
+##    '''Returns system audio volume from ALSA amixer. SIGUSR1 is used to
+##    notify the thread of volume changes from button presses.'''
+    pat = re.compile(r'.*\[(\d+)%\] \[(\w+)\]')
+
+    cmd = await aiosp.create_subprocess_shell(
+        "amixer sget Master",
+        stdout=aiosp.PIPE
+    )
+    out, err = await cmd.communicate()
+    state_list = (l.strip() for l in reversed(out.decode().splitlines()))
+
+    pcts, states = zip(*(
+        m.groups()
+        for l in state_list
+        if (m := pat.match(l))
+    ))
+    avg_pct = sum(int(p) for p in pcts) // len(pcts)
+    is_on = any(s == 'on' for s in states)
+    # return is_on, avg_pct
+    state = "" if is_on else "M"
+    return fmt.format(avg_pct, state=state)
+
+async def get_battery_info(
+    prec: int = 0, 
+    fmt: str = "{:02.{}f}",
+    *args,
+    **kwargs
+) -> Contents:
+    '''Returns battery capacity as a percent and whether it is charging
+    or discharging.'''
+
+    # Progressive/dynamic battery icons!
+        # 
+        # 
+        # 
+        # 
+        # 
+
+    # if not (battery := psutil.sensors_battery()):
+    battery = psutil.sensors_battery()
+    if not battery:
+        return ""
+##        # return (None, None)
+    info = fmt.format(battery.percent, prec)
+    state = "CHG" if battery.power_plugged else ''
+##    return battery.power_plugged, info
+    return state + info
+
+async def get_cpu_temp(
+    fmt: str = "{:02.0f}{}",
+    in_fahrenheit=False,
+    *args,
+    **kwargs
+) -> Contents:
+    '''Returns current CPU temperature in Celcius or Fahrenheit.'''
+    symbols = ('C', 'F')
+    symbol = symbols[in_fahrenheit]
+
+    temps = psutil.sensors_temperatures(in_fahrenheit)
+    match temps:
+        case {'k10temp': t} | {'coretemp': t}:
+            current = t[0].current
+        case _:
+            current = '??'
+    return fmt.format(current, symbol)
+
+async def get_cpu_usage(
+    interval: float = None,
+    fmt = "{:02.0f}%",
+    *args,
+    **kwargs
+) -> Contents:
+    '''Returns system CPU usage in percent over a specified interval.'''
+    return fmt.format(psutil.cpu_percent(interval))
+
+async def get_datetime(
+    fmt: str = "%Y-%m-%d %H:%M:%S",
+    *args,
+    **kwargs
+) -> Contents:
+    '''Return the current time as formatted with `fmt`.'''
+    return datetime.now().strftime(fmt)
+
+def precision_datetime(
+    fmt: str = "%Y-%m-%d %H:%M:%S.%f",
+    *args,
+    **kwargs
+) -> Contents:
+    '''Return the current time as formatted with `fmt`.
+    Being synchronous, a threaded Field can run this with
+    align_to_seconds and see less than a millisecond of offset.
+    '''
+    return datetime.now().strftime(fmt)
+
+async def get_disk_usage(
+    path='/',
+    measure='free',
+    unit='G',
+    fmt: str = "{:.{}f}{}",
+    prec: int = 1,
+    *args,
+    **kwargs
+) -> Contents:
+    '''Returns disk usage for a given path.
+    Measure can be 'total', 'used', 'free', or 'percent'.
+    Units can be 'G', 'M', or 'K'.'''
+
+    if unit not in POWERS_OF_1K:
+        raise InvalidArgError(
+            f"Invalid unit: {unit!r}\n"
+            f"'unit' must be one of "
+            f"{join_options(POWERS_OF_1K)}."
+        )
+
+    disk = psutil.disk_usage(path)
+    if measure not in disk._fields:
+        raise InvalidArgError(
+            f"Invalid measure on this operating system: {measure!r}.\n"
+            f"measure must be one of "
+            f"{join_options(disk._fields)}"
+        )
+
+    statistic = getattr(disk, measure, None)
+    converted = statistic / 1024**POWERS_OF_1K[unit]
+    usage = fmt.format(converted, prec, unit)
+    return usage
+
+
+async def get_hostname(*args, **kwargs) -> Contents:
+    return os.uname().nodename
+
+async def get_mem_usage(
+    prec: int = 1,
+    measure: str = 'used',
+    unit: str = 'G',
+    fmt: str = "{:.{}f}{}",
+    *args,
+    **kwargs
+) -> Contents:
+    '''Returns total RAM used including buffers and cache in GiB.'''
+
+    if unit not in POWERS_OF_1K:
+        raise InvalidArgError(
+            f"Invalid unit: {unit!r}\n"
+            f"'unit' must be one of "
+            f"{join_options(POWERS_OF_1K)}."
+        )
+
+    memory = psutil.virtual_memory()
+
+    if measure not in memory._fields:
+        raise InvalidArgError(
+            f"Invalid measure on this operating system: {measure!r}\n"
+            f"'measure' must be one of "
+            f"{join_options(memory._fields)}."
+        )
+
+    statistic = getattr(memory, measure, None)
+    converted = statistic / 1024 ** POWERS_OF_1K[unit]
+    mem = fmt.format(converted, prec, unit)
+    return mem
+
+#NOTE: This is most optimal as a threaded function.
+async def get_net_stats(
+    # device: str = None,
+    nm: bool = True,
+    nm_filt: NmConnFilterSpec = None,
+    fmt: FormatStr = "{name}",
+    default: str = "",
+    *args,
+    **kwargs
+) -> Contents:
+    '''Returns active network name (and later, stats) from either
+    NetworkManager or iwconfig.
+    '''
+    # If the user has NetworkManager, get the active connections:
+    if nm:
+
+        if nm_filt:
+            # We use --terse for easier parsing.
+            cmd = f"nmcli --terse connection show {shlex.join(nm_filt)}"
+        else:
+            cmd = "nmcli --terse connection show --active"
+
+        proc = await aiosp.create_subprocess_shell(
+            cmd,
+            stdout=aiosp.PIPE,
+            stderr=aiosp.PIPE
+        )
+        out, err = await proc.communicate()
+        results = out.decode().splitlines()
+
+        conns = tuple(
+            # Split in reverse to avoid butchering names with colons:
+            c.rsplit(':', 3)
+            for c in results
+        )
+
+        # Don't bother parsing an empty tuple:
+        if not conns:
+            keynames = ('name', 'uuid', 'typ', 'device')
+            return fmt.format_map({key: default for key in keynames})
+
+        # By default, nmcli sorts connections by type:
+        #   ethernet, wifi, tun, ...
+        # We only need the top result.
+        match conns[0]:
+            case (name, uuid, typ, device):
+                profile = {
+                    # The command output duplicates all backslashes to
+                    # escape them and adds more before colons in names.
+                    # Take special care to preserve literal \ and : in
+                    # original names while removing extra backslashes:
+                    'name': name.replace('\\\\', '\\').replace('\\:', ':'),
+                    'uuid': uuid,
+                    'type': typ,
+                    'device': device,
+                }
+                return fmt.format_map(profile)
+
+            case _:
+                return ""
+
+    else:
+        cmd = await aiosp.create_subprocess_shell(
+            "iwconfig",
+            stdout=aiosp.PIPE
+        )
+        out, err = await cmd.communicate()
+        if_list = out.decode().splitlines()
+
+        ssid = next(
+            line.split(':"')[1].strip('" ')
+            for line in if_list
+            if line.find("SSID")
+        )
+        return ssid
+
+
+# Uptime
+
+async def get_uptime(
+    *,
+    fmt: FormatStr,
+    dynamic: bool = True,
+    sep: str = ':',
+    setupvars = None,
+) -> Contents:
+    secs = time.time() - psutil.boot_time()
+
+    if not setupvars:
+        fnames, groups = DynamicFormatStr(fmt, sep).deconstruct()
+
+        setupvars = {
+            'fnames': fnames,
+            'groups': groups,
+            'sep': sep,
+        }
+    lookup_table = ElapsedTime.in_desired_units(secs, setupvars['fnames'])
+    setupvars['namespace'] = lookup_table
+
+    if dynamic:
+        out = format_uptime(secs, **setupvars)
+        return out
+
+    return fmt.format_map(setupvars['namespace'])
+
+
+def format_uptime(
+    secs: int,
+    sep: str,
+    namespace: dict[str],
+    groups,
+    *args, **kwargs
+) -> str:
+    '''Fornat a dict of numbers according to a format string by parsing
+    fields delineated by a separator.
+    '''
+    newgroups = []
+    for i, group in enumerate(groups):
+        if not group:
+            # Just an extraneous separator.
+            newgroups.append(())
+            continue
+
+        newgroup = []
+        
+        for maybe_field in group:
+            # Skip groups that should appear blank:
+            if (val := namespace.get(maybe_field[1])
+                ) == 0:
+                break
+
+            buf = ""
+
+            match maybe_field:
+                case [lit, None, None, None]:
+                    # A trailing literal.
+                    buf += lit
+
+                case [lit, field, spec, conv]:
+                    # A veritable format string field!
+                    # Add the text right before the field:
+                    if lit is not None:
+                        buf += lit
+
+                    # Format the value if necessary:
+                    if spec:
+                        buf += format(val, spec)
+                    else:
+                        try:
+                            # Round floats by default:
+                            buf += str(round(val))
+                        except TypeError:
+                            buf += str(val)
+
+                case _:
+                    raise ValueError(
+                        f"\n"
+                        f"Invalid structure in tuple\n"
+                        f"  {i} {maybe_field}:\n"
+                        f"  {spam!r}"
+                    )
+
+            if buf:
+                newgroup.append(buf)
+        if newgroup:
+            newgroups.append(newgroup)
+
+    # Join everything.
+    return sep.join(''.join(g) for g in newgroups)
+
+
---
+++ packages/mybar/mybar-0.4-py3-none-any.whl/mybar/formatable.py
@@ -0,0 +1,227 @@
+from string import Formatter
+
+from .errors import InvalidFormatStringFieldError
+from .utils import join_options, make_error_message
+from ._types import Duration, FormatStr, FmtStrStructure, FormatterFname 
+
+from collections.abc import Callable, Iterable
+from typing import Any
+
+
+class Icon(dict):
+# class Icon:
+
+    def __init__(
+        self,
+        statemap: dict[Any, str] = {},
+        # default: 
+    ) -> None:
+        self._registry = statemap
+
+        # return self
+
+##    def __setitem__(self, state: Any, var: str) -> None:
+##        self._registry[id(state)] = var
+        # self[id(state)] = var
+
+##    def __getitem__(self, state: Any, ) -> str:
+##        return self._registry.get(state)
+
+    def __repr__(self) -> str:
+        # return self._registry[self._default]
+        return ', '.join(' on '.join((repr(v), repr(k))) for k, v in self.items())
+
+    # def __str__(self) -> str:
+        # return self._registry[self._default]
+
+
+class DynamicFormatStr:
+    '''
+    '''
+        #TODO: Docstring!!
+    def __init__(self,
+        fmt: FormatStr,
+        sep: str = ':'  # Other common values are , /
+    ) -> None:
+        self.fmt = fmt
+        self.sep = sep
+        self.fnames, self.deconstructed = self.deconstruct()
+
+    def deconstruct(self,
+        sep: str = None
+    ) -> tuple[tuple[FormatterFname], FmtStrStructure]:
+        '''
+        Break up a format string using its format fields and a separator.
+        '''
+        fmt = self.fmt
+        if sep is None:
+            sep = self.sep
+
+        sections = []
+        # Split fmt for parsing, but join any format specs that get broken:
+        pieces = (p for p in fmt.split(sep))
+
+        def _is_malformed(piece: FormatStr):
+            '''Return whether a format string is malformed.'''
+            try:
+                tuple(Formatter().parse(piece))
+            except ValueError:
+                return True
+            else:
+                return False
+
+        try:
+            for piece in pieces:
+                while _is_malformed(piece):
+                    # Raise StopIteration if a valid field end is not found:
+                    piece = sep.join((piece, next(pieces)))
+                sections.append(piece)
+
+        except StopIteration:
+            exc = make_error_message(
+                BrokenFormatStringError,
+                doing_what=f"parsing {self!r} format string {fmt!r}",
+                details=[
+                    f"Invalid fmt substring begins near ->{piece!r}"
+                ]
+            )
+            raise exc from None
+
+        deconstructed = tuple(
+            tuple(Formatter().parse(section))
+            for section in sections
+        )
+
+        fnames = tuple(
+            name
+            for section in deconstructed
+            for parsed in section
+            if (name := parsed[1])
+        )
+
+        return fnames, deconstructed
+
+    def format(self,
+        namespace: dict[FormatterFname],
+        predicate: Callable = bool,
+        sep: str = None,
+        replace: str = None,
+    ) -> str:
+        '''Fornat a dict of numbers according to a format string by parsing
+        fields delineated by a separator `sep`.
+        Field groups which fail the `predicate` are not shown in the
+        final format string.
+        If specified or ``None``, `replace` will be shown instead.
+        '''
+        if sep is None:
+            sep = self.sep
+
+        newgroups = []
+        for i, group in enumerate(self.deconstructed):
+            if not group:
+                # Just an extraneous separator.
+                newgroups.append(())
+                continue
+
+            newgroup = []
+            
+            for maybe_field in group:
+                # Skip over sections that do not pass the predicate:
+                if (val := namespace.get(maybe_field[1])
+                    ) is not None and not predicate(val):
+                    if replace is not None:
+                        newgroups.append(replace)
+                    break
+
+                buf = ""
+
+                match maybe_field:
+                    case [lit, None, None, None]:
+                        # A trailing literal.
+                        # Only append if the previous field was valid:
+                        buf += lit
+
+                    case [lit, field, spec, conv]:
+                        # A veritable format string field!
+                        # Add the text right before the field:
+                        if lit is not None:
+                            buf += lit
+
+                        # Format the value if necessary:
+                        if spec:
+                            buf += format(val, spec)
+                        else:
+                            buf += str(val)
+
+                    case weird:
+                        raise ValueError(
+                            f"\n"
+                            f"Invalid structure in tuple\n"
+                            f"  {i} {maybe_field}:\n"
+                            f"  {weird!r}"
+                        )
+
+                if buf:
+                    newgroup.append(buf)
+            if newgroup:
+                newgroups.append(newgroup)
+
+        # Join everything.
+        return sep.join(''.join(g) for g in newgroups)
+
+
+class ElapsedTime:
+
+    conversions_to_secs = {
+        'years': 12*4*7*24*60*60,
+        'months': 4*7*24*60*60,
+        'weeks': 7*24*60*60,
+        'days': 24*60*60,
+        'hours': 60*60,
+        'mins': 60,
+        'secs': 1
+    }
+
+    @classmethod
+    def in_desired_units(cls,
+        secs: int,
+        units: tuple[Duration]
+    ) -> dict[Duration, int]:
+        '''
+        '''
+        #TODO: Docstring!!
+
+        if not all(u in cls.conversions_to_secs for u in units):
+            exptd = join_options(cls.conversions_to_secs)
+            valid = set(cls.conversions_to_secs)
+            unrec = join_options(set(units) - valid)
+            exc = make_error_message(
+                LookupError,
+                # InvalidFormatStringFieldError,
+                # doing_what="finding units to convert",
+                blame=repr(units),
+                expected=f"a sequence of unit names from {exptd}",
+                details=[
+                    f"The following unit names are not recognized:",
+                    f"{unrec}",
+                ]
+            )
+            raise exc
+
+        # Get the units in order of largest first:
+        ordered = tuple(u for u in cls.conversions_to_secs if u in units)
+
+        table = {}
+        if len(ordered) == 1:
+            unit = ordered[0]
+            # Avoid robbing the only unit of its precision. Just divide:
+            table[unit] = secs / cls.conversions_to_secs[unit]
+            return table
+
+        for unit in ordered[:-1]:
+            table[unit], secs = divmod(secs, cls.conversions_to_secs[unit])
+        # Give the least significant unit a precise value:
+        last_u = ordered[-1]
+        table[last_u] = secs / cls.conversions_to_secs[last_u]
+        return table
+
