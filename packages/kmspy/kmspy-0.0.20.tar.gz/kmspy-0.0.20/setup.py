699bec2b3ed3ae466aeb040af3a3626179ebfc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:22 +0000

    Add package kmspy/0.0.20/kmspy-0.0.20.tar.gz

---
+++ packages/kmspy/kmspy-0.0.20.tar.gz/kmspy-0.0.20/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kmspy",
+    version="0.0.20",
+    author="KMS",
+    author_email="su4651@gmail.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "numpy"
+    ],
+)
