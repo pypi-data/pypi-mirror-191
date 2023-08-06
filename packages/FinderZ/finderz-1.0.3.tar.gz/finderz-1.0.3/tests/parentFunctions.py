6dc884cbff78666e41465e5d44179875fa7e6120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:33 +0000

    Add package FinderZ/1.0.3/finderz-1.0.3.tar.gz

---
+++ packages/FinderZ/finderz-1.0.3.tar.gz/finderz-1.0.3/tests/parentFunctions.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+#Functions that will be used frequently:
+import os
+
+#Function to scan files, and for each file, replace that keyword for every line.
+def findAndReplace(filePath, keyWord, replacementKeyword):
+	#Scan for files:
+	
+	for folder, dirs, files in os.walk(filePath):
+		Check = False
+		if Check == True:
+			break
+		for file in files:
+			
+			fullpath = os.path.join(folder, file)
+			
+			readable = False
+			#Open the file and start scanning:
+			fileObject = open(fullpath, 'r')
+			
+			#Read the lines:
+			try:
+				fileLines = fileObject.readlines()
+				readable = True
+			except UnicodeDecodeError:
+				print("I: Bypassing some un-decodable files...")
+				continue
+			#Get the line amount:
+			if readable == True:
+				lineamount = len(fileLines)
+				linecount = 0
+				for i in range(lineamount):
+					#Line location
+					
+					
+					#Check if the keyword is in the lineCount:
+					if keyWord in fileLines[linecount]:
+						Check = True
+						
+						#Get the info in that line.
+						foundLocation = fileLines[linecount]
+						
+						#Replace the word in the line with the new word.
+						replacement = foundLocation.replace(keyWord, replacementKeyword)
+						
+						#Set the line equal to the new value:
+						fileLines[linecount] = replacement
+					linecount += 1
+			#If the check is True, write the file, else, go back to loop and scan for other files.
+				if Check == True:
+					f = open(fullpath, 'w')
+					f.writelines(fileLines)
