f4231c94e7d63715a9552131d4bb69db4db00f33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:26 +0000

    Add package python-clock-timer/0.4.0/python_clock_timer-0.4.0-py3-none-any.whl

---
+++ packages/python-clock-timer/python_clock_timer-0.4.0-py3-none-any.whl/clock_timer/base.py
@@ -0,0 +1,17 @@
+"""
+clock_timer base module.
+
+This is the principal module of the clock_timer project.
+here you put your main classes and objects.
+
+Be creative! do whatever you want!
+
+If you want to replace this with a Flask application run:
+
+    $ make init
+
+and then choose `flask` as template.
+"""
+
+# example constant variable
+NAME = "clock_timer"
