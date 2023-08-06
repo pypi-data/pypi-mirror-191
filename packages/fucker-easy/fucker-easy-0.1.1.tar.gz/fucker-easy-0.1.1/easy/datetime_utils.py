a94b16c901e1eff4e857b0d111e889c7dbb915be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:41 +0000

    Add package fucker-easy/0.1.1/fucker-easy-0.1.1.tar.gz

---
+++ packages/fucker-easy/fucker-easy-0.1.1.tar.gz/fucker-easy-0.1.1/easy/datetime_utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author   : 玛卡玛卡啊
+# @Time     : 2022/11/28 20:55
+# @desc     : 作者的所有代码均属于学习使用,不可商用或用在非法用途上。
+import time
+
+
+def ts13():
+    """
+    create 13 timestamp
+    :return:
+    """
+    return int(time.time()*13)
+
+
+def ts10():
+    """
+    create 10 timestamp
+    :return:
+    """
+    return int(time.time()*10)
