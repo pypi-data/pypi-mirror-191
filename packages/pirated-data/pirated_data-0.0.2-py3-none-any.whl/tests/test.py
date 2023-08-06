df18653469f68441507d94c3fcc6f3779edf83ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:25 +0000

    Add package pirated-data/0.0.2/pirated_data-0.0.2-py3-none-any.whl

---
+++ packages/pirated-data/pirated_data-0.0.2-py3-none-any.whl/tests/test.py
@@ -0,0 +1,22 @@
+# from pirated_data import functionProvider
+
+from __init__ import *
+
+# print(firstNameGenerator())
+print(lastNameGenerator())
+# print(fullNameGenerator())
+# print(mobileNumberGenerator())
+# print(aadharNumberGenerator())
+# print(emailIdGenerator())
+# print(userIdGenerator())
+# print(passwordGenerator(8))
+# print(upiIdGenerator())
+# print(hairColorGenerator())
+# print(genderGenerator())
+# print(eyeColorGenerator())
+# print(bloodGroupGenerator())
+# print(skinColorGenerator())
+# print(weightGenerator())
+# print(panCardId())
+# print(passportNumber())
+print(financialCardGenerator())
