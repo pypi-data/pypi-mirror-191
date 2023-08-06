f8693eb751b780d1cc3e79f67c8476cd1efa50ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:20:22 +0000

    Add package glinux-welcome/53/glinux-welcome-53.tar.gz

---
+++ packages/glinux-welcome/glinux-welcome-53.tar.gz/glinux-welcome-53/setup.py
@@ -0,0 +1,4 @@
+from setuptools import setup
+import reporter
+
+setup(name='glinux-welcome', version='53', description='Ethical research - dep confusion package. This is not a real package, do not install.')
