dfc911ffc5d6cf435d73b9dfdf4ad9b691e51678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:55 +0000

    Add package u4nf/1.0/u4nf-1.0.tar.gz

---
+++ packages/u4nf/u4nf-1.0.tar.gz/u4nf-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0'
+DESCRIPTION = 'Python Tools - (Log)'
+
+setup(name='u4nf',
+      version=VERSION,
+      description=DESCRIPTION,
+      author='4nf',
+      packages=find_packages()
+     )
