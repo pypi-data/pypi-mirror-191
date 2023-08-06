222d23462dc14aeea666c43a6372e80d28af32b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:01 +0000

    Add package algo-auto-ml/0.1.3/algo-auto-ml-0.1.3.tar.gz

---
+++ packages/algo-auto-ml/algo-auto-ml-0.1.3.tar.gz/algo-auto-ml-0.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="algo-auto-ml",
+    version="0.1.3",
+    description="AutoML library for binary classification and regression tasks",
+    url="https://github.com/lllchak/algo",
+    author="Pavel Lyulchak",
+    author_email="mediumchak@yandex.ru",
+    license="MIT",
+    packages=[
+        "algo",
+
+    ],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "lightgbm",
+        "xgboost"
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python"
+    ]
+)
