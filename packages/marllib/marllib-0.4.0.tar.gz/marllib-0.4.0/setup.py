fe2a89f6850b0ef03207a5dd035bb2e36ed734b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:27 +0000

    Add package marllib/0.4.0/marllib-0.4.0.tar.gz

---
+++ packages/marllib/marllib-0.4.0.tar.gz/marllib-0.4.0/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import find_packages, setup
+
+with open("VERSION.txt", "r") as file_handler:
+    __version__ = file_handler.read().strip()
+
+setup(
+    name="marllib",
+    version=__version__,
+    packages=find_packages(),
+    license="MIT",
+    python_requires=">=3.8",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
