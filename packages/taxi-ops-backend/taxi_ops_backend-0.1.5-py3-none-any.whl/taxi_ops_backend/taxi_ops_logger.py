80b5bceeddb3c156c55c3dded8ba2f0db7270fa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:17 +0000

    Add package taxi-ops-backend/0.1.5/taxi_ops_backend-0.1.5-py3-none-any.whl

---
+++ packages/taxi-ops-backend/taxi_ops_backend-0.1.5-py3-none-any.whl/taxi_ops_backend/taxi_ops_logger.py
@@ -0,0 +1,32 @@
+"""
+Logging module to be used in the project
+"""
+
+import logging
+
+
+class TaxiOpsLogger(logging.Logger):
+    """ Overriden Logger class to be used in the project """
+    class TaxiOpsLogStreamHandler(logging.StreamHandler):
+        """ Overriden StreamHandler class to be used in the project """
+        def __init__(self):
+            logging.StreamHandler.__init__(self)
+            log_format = '%(asctime)s module:%(module)-15s func:%(funcName)s' \
+                         ' line:%(lineno)d - %(message)s'
+            fmt_date = '%Y-%m-%dT%T%Z'
+            formatter = logging.Formatter(log_format, fmt_date)
+            self.setFormatter(formatter)
+
+    class TaxiOpsLogFileHandler(logging.FileHandler):
+        """ Overriden FileHandler class to be used in the project """
+        def __init__(self, filename):
+            logging.FileHandler.__init__(self, filename)
+            log_format = '%(asctime)s module:%(module)-15s func:%(funcName)s' \
+                         ' line:%(lineno)d - %(message)s'
+            fmt_date = '%Y-%m-%dT%T%Z'
+            formatter = logging.Formatter(log_format, fmt_date)
+            self.setFormatter(formatter)
+
+    def __init__(self):
+        logging.Logger.__init__(self, 'TaxiOps', 'DEBUG')
+        self.addHandler(TaxiOpsLogger.TaxiOpsLogStreamHandler())
