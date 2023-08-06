538203590649a1220fcfadbabd5c624f41f49c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:58 +0000

    Add package revsymg/0.3.4/revsymg-0.3.4-py3-none-any.whl

---
+++ packages/revsymg/revsymg-0.3.4-py3-none-any.whl/revsymg/hash_ids/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding=utf-8 -*-
+
+"""Just for module."""
+
+# flake8: noqa
+
+# ============================================================================ #
+#                         READS IDENTIFIERS CONTAINERS                         #
+# ============================================================================ #
+from revsymg.hash_ids.abstract_ids import *
+from revsymg.hash_ids.hashable_ids import *
+from revsymg.hash_ids.indices_ids import *
