9b479ee6ab79bc0d31eb39341234fc169c0debbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:24 +0000

    Add package trigger-count/0.1.0/trigger_count-0.1.0.tar.gz

---
+++ packages/trigger-count/trigger_count-0.1.0.tar.gz/trigger_count-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['trigger_count']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['labjack-ljm>=1.21.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'trigger-count',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'Mathis',
+    'author_email': 'mathis.bassler@protonmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
