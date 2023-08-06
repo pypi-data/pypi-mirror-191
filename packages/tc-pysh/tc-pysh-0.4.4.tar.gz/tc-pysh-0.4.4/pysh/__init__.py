76a5d557faf0351ac22f99be9dd7b7a7cfadc592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:26 +0000

    Add package tc-pysh/0.4.4/tc-pysh-0.4.4.tar.gz

---
+++ packages/tc-pysh/tc-pysh-0.4.4.tar.gz/tc-pysh-0.4.4/pysh/__init__.py
@@ -0,0 +1,28 @@
+from .path import *
+from .utils import *
+
+
+__version__ = "0.4.4"
+
+
+# I use that for the automatic doc
+__all__ = [
+    "AbsolutePath",
+    "archive",
+    "cat",
+    "cd",
+    "cp",
+    "cwd",
+    "ensure_abs_path",
+    "ensure_path",
+    "find,"
+    "in_dir",
+    "ls",
+    "mkdir",
+    "mv",
+    "Query",
+    "RelativePath",
+    "rm",
+    "sh",
+    "sh_with_stdout",
+]
---
+++ packages/tc-pysh/tc-pysh-0.4.4.tar.gz/tc-pysh-0.4.4/pysh/query.py
@@ -0,0 +1,83 @@
+import re
+
+from typing import Union, Optional, Iterable, Callable
+
+from .path import AbsolutePath, RelativePath, Path, ensure_abs_path
+
+
+PathPred = Callable[[AbsolutePath], bool]
+
+
+class Query:
+    "Filter a collection of path"
+
+    def __init__(self, source: Iterable[Path], filter: Optional[PathPred] = None):
+        self.source = source
+        self._filter = filter
+
+    def __iter__(self):
+        for p in self.source:
+            if isinstance(p, RelativePath):
+                _p = AbsolutePath.from_rel(p)
+            else:
+                _p = p
+            if self._filter is None or self._filter(_p):
+                yield p
+
+    def isfile(self):
+        """Filter paths to keep only the files."""
+
+        def isfile(p):
+            return p.isfile()
+
+        return Query(self, isfile)
+
+    def isdir(self):
+        """Filter the elements of the query to keep only the directories."""
+
+        def isdir(p):
+            return p.isdir()
+
+        return Query(self, isdir)
+
+    def name(self, pattern: Union[str, re.Pattern]):
+        """Filter the elements of the query to keep only the ones matching `pattern`.
+
+        :param pattern: a `str` or a `re.Pattern`, used as a regex to match the paths
+        """
+        if isinstance(pattern, re.Pattern):
+            regex: re.Pattern = pattern
+        else:
+            regex = re.compile(pattern)
+
+        def match(path: AbsolutePath):
+            return bool(regex.fullmatch(path.base))
+
+        return Query(self, match)
+
+    def first(self):
+        """Return the first element of the query."""
+        return next(iter(self), None)
+
+    def last(self):
+        """Return the last element of the query."""
+        it = None
+
+        for it in self:
+            pass
+
+        return it
+
+    def filter(self, pred: PathPred):
+        """Create an arbitrary query filter from a predicate.
+
+        :param pred: keep only the elements for which `pred(elem)` is true.
+        """
+        return Query(self, pred)
+
+    def sort(self, key=None):
+        """Sort the elements of the query.
+
+        :param key: (optional) see :py:func:`sorted`
+        """
+        return Query(sorted(self, key=key))
