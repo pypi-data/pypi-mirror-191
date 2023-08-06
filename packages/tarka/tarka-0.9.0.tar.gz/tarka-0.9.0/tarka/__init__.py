9465450467db6fec6c12a1a2ba9aa8a3a388cb89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:58 +0000

    Add package tarka/0.9.0/tarka-0.9.0.tar.gz

---
+++ packages/tarka/tarka-0.9.0.tar.gz/tarka-0.9.0/tarka/__init__.py
@@ -0,0 +1,10 @@
+"""
+Tarka
+
+:copyright: 2022 Nándor Mátravölgyi
+:license: Apache2, see LICENSE for more details.
+"""
+
+__copyright__ = "Copyright 2022 Nándor Mátravölgyi"
+__credits__ = ["Nándor Mátravölgyi"]
+__version__ = "0.9.0"
