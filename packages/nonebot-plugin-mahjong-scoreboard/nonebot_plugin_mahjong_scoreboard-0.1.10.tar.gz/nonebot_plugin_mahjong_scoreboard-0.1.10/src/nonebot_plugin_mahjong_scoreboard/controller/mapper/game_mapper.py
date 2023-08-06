9e9a43fd828053e9d899d0e2de25298bf81049cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:33 +0000

    Add package nonebot-plugin-mahjong-scoreboard/0.1.10/nonebot_plugin_mahjong_scoreboard-0.1.10.tar.gz

---
+++ packages/nonebot-plugin-mahjong-scoreboard/nonebot_plugin_mahjong_scoreboard-0.1.10.tar.gz/nonebot_plugin_mahjong_scoreboard-0.1.10/src/nonebot_plugin_mahjong_scoreboard/controller/mapper/game_mapper.py
@@ -0,0 +1,119 @@
+from io import StringIO
+
+from nonebot.adapters.onebot.v11 import MessageSegment, Message
+
+from nonebot_plugin_mahjong_scoreboard.controller.mapper import player_and_wind_mapping, game_state_mapping, \
+    digit_mapping, \
+    wind_mapping, map_datetime
+from nonebot_plugin_mahjong_scoreboard.model.enums import GameState
+from nonebot_plugin_mahjong_scoreboard.model.orm import data_source
+from nonebot_plugin_mahjong_scoreboard.model.orm.game import GameOrm, GameProgressOrm
+from nonebot_plugin_mahjong_scoreboard.model.orm.group import GroupOrm
+from nonebot_plugin_mahjong_scoreboard.model.orm.season import SeasonOrm
+from nonebot_plugin_mahjong_scoreboard.model.orm.user import UserOrm
+from nonebot_plugin_mahjong_scoreboard.service.group_service import get_user_nickname
+from nonebot_plugin_mahjong_scoreboard.utils.rank import ranked
+
+
+def map_game_progress(progress: GameProgressOrm) -> str:
+    with StringIO() as io:
+        if progress.round <= 4:
+            io.write('东')
+            io.write(digit_mapping[progress.round])
+        else:
+            io.write('南')
+            io.write(digit_mapping[progress.round - 4])
+        io.write('局')
+        io.write(str(progress.honba))
+        io.write('本场')
+
+        return io.getvalue()
+
+
+async def map_game(game: GameOrm, *, detailed: bool = False) -> Message:
+    session = data_source.session()
+
+    group = await session.get(GroupOrm, game.group_id)
+
+    with StringIO() as io:
+        # 对局22090901  四人南
+        io.write('对局')
+        io.write(str(game.code))
+        io.write('  ')
+        io.write(player_and_wind_mapping[game.player_and_wind])
+        io.write('\n')
+
+        if detailed:
+            # 所属赛季：Season Name
+            io.write('所属赛季：')
+            if game.season_id is None:
+                io.write('无')
+            else:
+                season = await session.get(SeasonOrm, game.season_id)
+                io.write(season.name)
+            io.write('\n')
+
+            promoter = await session.get(UserOrm, game.promoter_user_id)
+            io.write('创建者：')
+            io.write(await get_user_nickname(promoter, group))
+            io.write('\n')
+
+        # 状态：未完成
+        io.write('状态：')
+        io.write(game_state_mapping[GameState(game.state)])
+        if game.state != GameState.completed:
+            io.write("  （合计")
+            io.write(str(sum(map(lambda r: r.score, game.records))))
+            io.write("点）")
+        io.write('\n')
+
+        if detailed and game.state == GameState.completed:
+            io.write('完成时间：')
+            io.write(map_datetime(game.complete_time))
+            io.write('\n')
+
+        progress = await session.get(GameProgressOrm, game.id)
+        if progress is not None:
+            io.write('进度：')
+            io.write(map_game_progress(progress))
+            io.write('\n')
+
+        if len(game.records) > 0:
+            # [空行]
+            io.write('\n')
+
+            # #1 [东]    Player Name    10000点  (+5)
+            # [...]
+            for rank, r in ranked(game.records, key=lambda r: r.point, reverse=True):
+                user = await session.get(UserOrm, r.user_id)
+                name = await get_user_nickname(user, group)
+                io.write('#')
+                io.write(str(rank))
+                if r.wind is not None:
+                    io.write(' [')
+                    io.write(wind_mapping[r.wind])
+                    io.write(']')
+                io.write('    ')
+                io.write(name)
+                io.write('    ')
+                io.write(str(r.score))
+                io.write('点')
+
+                if game.state == GameState.completed:
+                    io.write('  (')
+                    if r.point > 0:
+                        io.write('+')
+                    elif r.point == 0:
+                        io.write('±')
+                    io.write(str(r.point))
+                    io.write(')')
+
+                io.write('\n')
+
+        if game.comment:
+            io.write('\n')
+            io.write("备注：")
+            io.write(game.comment)
+            io.write('\n')
+
+        return Message(MessageSegment.text(io.getvalue().strip()))
---
+++ packages/nonebot-plugin-mahjong-scoreboard/nonebot_plugin_mahjong_scoreboard-0.1.10.tar.gz/nonebot_plugin_mahjong_scoreboard-0.1.10/src/nonebot_plugin_mahjong_scoreboard/model/orm/season.py
@@ -0,0 +1,87 @@
+from datetime import datetime
+from typing import TYPE_CHECKING, Optional
+
+from sqlalchemy import Column, Integer, DateTime, String, Enum, ForeignKey, Boolean, Index, JSON
+from sqlalchemy.orm import relationship
+
+from . import data_source
+from ..enums import SeasonState, SeasonUserPointChangeType
+
+if TYPE_CHECKING:
+    from .game import GameOrm
+    from .group import GroupOrm
+    from .user import UserOrm
+
+
+@data_source.registry.mapped
+class SeasonOrm:
+    __tablename__ = 'seasons'
+
+    id: int = Column(Integer, primary_key=True, autoincrement=True)
+
+    group_id: int = Column(Integer, ForeignKey('groups.id'), nullable=False)
+    group: 'GroupOrm' = relationship('GroupOrm', foreign_keys='SeasonOrm.group_id')
+
+    state: SeasonState = Column(Enum(SeasonState), nullable=False, default=SeasonState.initial)
+
+    code: str = Column(String, nullable=False)
+    name: str = Column(String, nullable=False)
+
+    start_time: Optional[datetime] = Column(DateTime)
+    finish_time: Optional[datetime] = Column(DateTime)
+
+    config: dict = Column(JSON, nullable=False)
+
+    accessible: bool = Column(Boolean, nullable=False, default=True)
+    create_time: datetime = Column('create_time', DateTime, nullable=False, default=datetime.utcnow)
+    update_time: datetime = Column('update_time', DateTime, nullable=False, default=datetime.utcnow)
+    delete_time: Optional[datetime] = Column('delete_time', DateTime)
+
+    __table_args__ = (
+        Index("seasons_group_id_code_idx", "group_id", "code"),
+    )
+
+
+@data_source.registry.mapped
+class SeasonUserPointOrm:
+    __tablename__ = 'season_user_points'
+
+    season_id: int = Column(Integer, ForeignKey('seasons.id'), nullable=False, primary_key=True)
+    season: 'SeasonOrm' = relationship('SeasonOrm', foreign_keys='SeasonUserPointOrm.season_id')
+
+    user_id: int = Column(Integer, ForeignKey('users.id'), nullable=False, primary_key=True)
+    user: 'UserOrm' = relationship('UserOrm', foreign_keys='SeasonUserPointOrm.user_id')
+
+    point: int = Column(Integer, nullable=False, default=0)
+
+    create_time: datetime = Column('create_time', DateTime, nullable=False, default=datetime.utcnow)
+    update_time: datetime = Column('update_time', DateTime, nullable=False, default=datetime.utcnow)
+
+
+@data_source.registry.mapped
+class SeasonUserPointChangeLogOrm:
+    __tablename__ = 'season_user_point_change_logs'
+
+    id: int = Column(Integer, primary_key=True, autoincrement=True)
+
+    season_id: int = Column(Integer, ForeignKey('seasons.id'), nullable=False)
+    season: 'SeasonOrm' = relationship('SeasonOrm', foreign_keys='SeasonUserPointChangeLogOrm.season_id')
+
+    user_id: int = Column(Integer, ForeignKey('users.id'), nullable=False)
+    user: 'UserOrm' = relationship('UserOrm', foreign_keys='SeasonUserPointChangeLogOrm.user_id')
+
+    change_type: SeasonUserPointChangeType = Column(Enum(SeasonUserPointChangeType), nullable=False)
+    change_point: int = Column(Integer, nullable=False)
+
+    related_game_id: Optional[int] = Column(Integer, ForeignKey('games.id'))
+    related_game: Optional['GameOrm'] = relationship('GameOrm',
+                                                     foreign_keys='SeasonUserPointChangeLogOrm.related_game_id')
+
+    create_time: datetime = Column('create_time', DateTime, nullable=False, default=datetime.utcnow)
+
+    __table_args__ = (
+        Index("seasons_related_game_id_idx", "related_game_id"),
+    )
+
+
+__all__ = ("SeasonOrm", "SeasonUserPointOrm", "SeasonUserPointChangeLogOrm")
