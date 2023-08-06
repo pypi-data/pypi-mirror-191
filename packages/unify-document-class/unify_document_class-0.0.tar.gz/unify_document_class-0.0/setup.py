e3855a4519a6676015afb948622969afd84407f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:02 +0000

    Add package unify-document-class/0.0/unify_document_class-0.0.tar.gz

---
+++ packages/unify-document-class/unify_document_class-0.0.tar.gz/unify_document_class-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='unify_document_class',
+      version='0.0',
+      description='Unified classes representing the structural elements of a text document',
+      packages=['unify_document_class'],
+      author_email='slavamarcin@yandex.ru',
+      zip_safe=False)
---
+++ packages/unify-document-class/unify_document_class-0.0.tar.gz/unify_document_class-0.0/unify_document_class/__init__.py
@@ -0,0 +1,7 @@
+from .Table import Table
+from .DocumentClass import Class
+from .Paragraph import Paragraph
+from .TableRow import TableRow
+from .TableCell import TableCell
+from .TableColumn import TableColumn
+from .DrawFrame import DrawFrame
