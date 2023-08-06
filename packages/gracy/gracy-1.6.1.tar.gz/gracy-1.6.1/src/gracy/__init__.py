1e8badc6dcadbe3e5893439fa51784bdf6de52a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:07 +0000

    Add package gracy/1.6.1/gracy-1.6.1.tar.gz

---
+++ packages/gracy/gracy-1.6.1.tar.gz/gracy-1.6.1/src/gracy/__init__.py
@@ -0,0 +1,42 @@
+"""Gracefully manage your API interactions"""
+import logging
+
+from . import exceptions
+from ._core import Gracy, graceful
+from ._models import (
+    DEFAULT_CONFIG,
+    BaseEndpoint,
+    GracefulRetry,
+    GracefulThrottle,
+    GracyConfig,
+    GracyRequestContext,
+    LogEvent,
+    LogLevel,
+    ThrottleRule,
+)
+from ._reports._models import GracyAggregatedRequest, GracyAggregatedTotal, GracyReport
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
+
+__version__ = "1.6.1"
+
+__all__ = [
+    "exceptions",
+    # Core
+    "Gracy",
+    "graceful",
+    # Models
+    "BaseEndpoint",
+    "GracefulRetry",
+    "GracyRequestContext",
+    "LogEvent",
+    "LogLevel",
+    "GracefulThrottle",
+    "ThrottleRule",
+    "GracyConfig",
+    "DEFAULT_CONFIG",
+    # Reports
+    "GracyReport",
+    "GracyAggregatedTotal",
+    "GracyAggregatedRequest",
+]
