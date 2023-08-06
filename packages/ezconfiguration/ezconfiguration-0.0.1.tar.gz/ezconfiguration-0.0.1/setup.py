1e5598dfbc546dbb511bbb5b1f83589dd0801e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:34 +0000

    Add package ezconfiguration/0.0.1/ezconfiguration-0.0.1.tar.gz

---
+++ packages/ezconfiguration/ezconfiguration-0.0.1.tar.gz/ezconfiguration-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+# README.md must be all capital letters
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+# Setting up
+setup(
+    name="ezconfiguration",
+    version="0.0.1",
+    license='MIT',    
+    author="datanooblol",
+    author_email="data.noob.lol@gmail.com",
+    url="https://github.com/datanooblol/ezconfiguration",
+    description="This is an easy way to a configuration file in json format.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",    
+    packages=find_packages(),
+    install_requires=[],
+    keywords="['python3', 'ez', 'config']",
+    include_package_data=False,
+    package_data={"": ["*.txt", "*.rst", ".csv"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
