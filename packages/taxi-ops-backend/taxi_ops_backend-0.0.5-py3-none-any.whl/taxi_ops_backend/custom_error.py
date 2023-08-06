28992777a1399886473205e296ba007c7d6f275d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:05 +0000

    Add package taxi-ops-backend/0.0.5/taxi_ops_backend-0.0.5-py3-none-any.whl

---
+++ packages/taxi-ops-backend/taxi_ops_backend-0.0.5-py3-none-any.whl/taxi_ops_backend/custom_error.py
@@ -0,0 +1,9 @@
+"""Module for exception class"""
+from src.taxi_ops_backend.taxi_ops_logger import TaxiOpsLogger
+
+
+class IllegalTaxiStatusState(Exception):
+    """Error Raised when Taxi status is not correct for an action to happen"""
+    def __init__(self, error_string):
+        TaxiOpsLogger().error(error_string)
+        super().__init__(error_string)
