ff4f4c0254bd9b9230ae1175263c2a3aaa8b5b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:33 +0000

    Add package asuna/0.0.1/asuna-0.0.1.tar.gz

---
+++ packages/asuna/asuna-0.0.1.tar.gz/asuna-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='asuna',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='summer',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
