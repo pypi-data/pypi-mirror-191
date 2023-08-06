e119518c2cd5849d67e8f9198fa700917923d390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:37 +0000

    Add package splight-lib/2.0.19/splight-lib-2.0.19.tar.gz

---
+++ packages/splight-lib/splight-lib-2.0.19.tar.gz/splight-lib-2.0.19/remote_splight_lib/communication/classmap.py
@@ -0,0 +1,6 @@
+from splight_models import CommunicationContext, CommunicationEvent
+
+CLASSMAP = {
+    CommunicationContext: "/communication/context/",
+    CommunicationEvent: "/communication/",
+}
