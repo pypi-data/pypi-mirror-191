5845a95fe769eccd964dcc1ad5fc3805ae902c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:07 +0000

    Add package pywaterkotte/0.1.2/pywaterkotte-0.1.2-py3-none-any.whl

---
+++ packages/pywaterkotte/pywaterkotte-0.1.2-py3-none-any.whl/pywaterkotte/__init__.py
@@ -0,0 +1,11 @@
+name = "pywaterkotte"
+
+from .ecotouch import (
+    AuthenticationException,
+    ConnectionException,
+    Ecotouch,
+    EcotouchTags,
+    InvalidResponseException,
+    InvalidValueException,
+    TagData,
+)
