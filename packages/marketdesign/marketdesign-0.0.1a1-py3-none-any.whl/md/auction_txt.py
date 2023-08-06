ebe5810cbf247b185b4f2139045f75bf95199fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:03 +0000

    Add package marketdesign/0.0.1a1/marketdesign-0.0.1a1-py3-none-any.whl

---
+++ packages/marketdesign/marketdesign-0.0.1a1-py3-none-any.whl/md/auction_txt.py
@@ -0,0 +1,129 @@
+from md.auction import Bid, Bidder, Divisibility
+from md.auction_csv import to_number, encode_csv_solution
+import re
+from io import StringIO
+from tabulate import tabulate
+
+def parse_bidders(s):
+    s = s.strip()
+    lines = re.split('\n\\s*', s)
+    bidders = []
+    names = set()
+    trader_id = 1
+    last_n_goods = None
+    for line in lines:
+        if not line:
+            continue
+
+        # Optional bidder name is separated from bids by ':'
+        if ':' in line:
+            parts = line.split(':')
+            if len(parts) > 2:
+                raise ValueError('More than one colon in "{}"'.format(line))
+            name = parts[0].strip()
+            line = parts[1].strip()
+        else:
+            name = 'trader {}'.format(trader_id)
+            trader_id += 1
+        if name in names:
+            raise ValueError('Two bidders with name {}'.format(name))
+        else:
+            names.add(name)
+        bidder = Bidder(name)
+        bidders.append(bidder)
+
+        # XOR groups are separated by '|', bids by ',' and entries by ' '
+        xor_groups = [x.strip() for x in line.split('|')]
+        group_id = 1
+        for group in xor_groups:
+            bids = [x.strip() for x in group.split(',')]
+            for bid_str in bids:
+                items = bid_str.split()
+                if items[0].lower() == 'd':
+                    divisible = Divisibility.DIVISIBLE
+                    items.pop(0)
+                elif items[0].lower() == 'm':
+                    divisible = Divisibility.MIXED
+                    items.pop(0)
+                else:
+                    divisible = Divisibility.INDIVISIBLE
+
+                n_goods = len(items)
+
+                value = to_number(items[0])
+                q = {}
+                for i in range(1, len(items)):
+                    good = 'good {}'.format(i)
+                    q[good] = to_number(items[i])
+                if len(bids) > 1:
+                    xor_group = "xor_{}".format(group_id)
+                else:
+                    xor_group = None
+                bidder.bids.append(Bid(value, q, divisible=divisible, xor_group=xor_group))
+
+                if last_n_goods is not None:
+                    if last_n_goods != n_goods:
+                        raise ValueError(
+                            "Not all bids have the same number of entries ({} and {})".format(last_n_goods, n_goods))
+                last_n_goods = n_goods
+            group_id += 1
+    if len(bidders) < 2:
+        raise ValueError("There must be at least 2 bidders.")
+    return bidders
+
+def to_txt(solution):
+    si = StringIO()
+    encode_csv_solution(solution, si, delimiter=',')
+
+    data = []
+    for row in si.getvalue().strip().split('\n'):
+        data.append(row.strip().split(','))
+    note = 'Pricing = {}, total surplus = {}, sum of payments = {}'.format(solution.rule, solution.surplus,
+                                                                           solution.sum_payments())
+    table = tabulate(data, headers='firstrow', tablefmt='rst')
+    return table + '\n' + note
+
+def encode_problem(problem):
+    si = StringIO()
+    goods = problem.list_goods()
+    def write_bid(bid):
+        if bid.divisibility == Divisibility.DIVISIBLE:
+            si.write(" d")
+        elif bid.divisibility == Divisibility.MIXED:
+            si.write(" m")
+
+        si.write(str(bid.v))
+        for good in goods:
+            if good in bid.q:
+                si.write(" " + str(bid.q[good]))
+            else:
+                si.write(" 0")
+
+    for bidder in problem.bidders:
+        si.write(bidder.name)
+        si.write(": ")
+        xor_groups = bidder.xor_groups()
+
+        first_group = True
+        # Write xor groups
+        for key, bid_ids in xor_groups.items():
+            if not first_group:
+                si.write("| ")
+            first_bid = True
+            for bid_id in bid_ids:
+                if not first_bid:
+                    si.write(", ")
+                bid = bidder.bids[bid_id]
+                write_bid(bid)
+                first_bid = False
+                first_group = False
+        # Write bids not in xor groups.
+        for bid in bidder.bids:
+            if bid.xor_group is None:
+                if not first_group:
+                    si.write("| ")
+                write_bid(bid)
+                first_group = False
+
+        si.write("\n")
+    return si.getvalue()
---
+++ packages/marketdesign/marketdesign-0.0.1a1-py3-none-any.whl/md/lindsay2018.py
@@ -0,0 +1,96 @@
+from md.auction import *
+import math
+from random import Random
+import itertools
+from collections import defaultdict
+import time
+
+# Implementation of the payment rule described in the paper "Shapley value based pricing for auctions and exchanges"
+# https://doi.org/10.1016/j.geb.2017.10.020
+
+class Lindsay2018(PaymentRule):
+
+    def __init__(self, max_perms_to_generate: int = 100000, max_perms_to_consider: int = 1024, seed: int = 7777):
+        self.max_perms_to_generate = max_perms_to_generate
+        self.max_perms_to_consider = max_perms_to_consider
+        self.seed = seed
+
+    def calc_surplus_shares(self, sol: Solution):
+
+        #Used to give progress updates for slow problems.
+        start_time = time.time()
+        last_time = start_time
+
+        winners = sol.winner_indexes()
+        losers = sol.loser_indexes()
+
+        permutations = []
+        rnd = Random(self.seed)
+        if math.factorial(len(winners)) > self.max_perms_to_generate:
+            for i in range(self.max_perms_to_consider):
+                perm = copy(winners)
+                rnd.shuffle(perm)
+                permutations.append(perm)
+        else:
+            permutations = list(itertools.permutations(winners))
+
+        if len(permutations) > self.max_perms_to_consider:
+            rnd.shuffle(permutations)
+            permutations = permutations[0:self.max_perms_to_consider]
+
+        # Stores running sum of shares for each winner.
+        shares_sums = defaultdict(lambda: 0)
+
+        # Cache surplus values to avoid repeatedly solving the same MIP problem.
+        cache = {frozenset(winners): sol.surplus}
+
+        problem = copy(sol.problem)
+        auction = Auction()
+        n_perms = len(permutations)
+
+        for j, perm in enumerate(permutations):
+            current_time = time.time()
+            if current_time > last_time + 10:
+                last_time = current_time
+                print('Permutation {} of {}'.format(j, n_perms))
+            last_surplus = 0
+            problem.bidders = [sol.problem.bidders[i] for i in losers]
+            for position, i in enumerate(perm):
+                problem.bidders.append(sol.problem.bidders[i])
+
+                key = frozenset(perm[0:position + 1])
+                if key in cache.keys():
+                    surplus = cache[key]
+                else:
+                    surplus = auction.surplus_determination(problem)
+                    cache[key] = surplus
+
+                share = surplus - last_surplus
+                shares_sums[i] = shares_sums[i] + share
+                last_surplus = surplus
+
+
+        for i in winners:
+            surplus_share = shares_sums[i] / n_perms
+            sol.populate_surplus_and_payment(sol.problem.bidders[i], surplus_share)
+
+        sol.rule = 'lindsay2018'
+        current_time = time.time()
+        if current_time > start_time + 10:
+            n_winners = len(winners)
+            n_bidders = n_winners + len(losers)
+            solve_time = round(current_time-start_time)
+            print('{} bidders and {} winners.  Calculating prices took {} seconds with {} permutations.'.format(n_bidders, n_winners,solve_time, n_perms))
+
+
+def get_rule(rule_name,  max_perms_to_consider: int = 1024):
+    rule_name = rule_name.lower().strip()  # ignore case
+    if rule_name == 'lindsay2018':
+        rule = Lindsay2018(max_perms_to_consider=max_perms_to_consider)
+    elif rule_name == 'vcg':
+        rule = VCG()
+    elif rule_name == 'pab':
+        rule = PaB()
+    else:
+        raise ValueError('Unrecognised payment rule "{}".'.format(rule_name))
+    return rule
