f3968bba510052710e5fc2e05dd4c2cd9ef91335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:34 +0000

    Add package apache-age-dijkstra/0.2.1/apache-age-dijkstra-0.2.1.tar.gz

---
+++ packages/apache-age-dijkstra/apache-age-dijkstra-0.2.1.tar.gz/apache-age-dijkstra-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from age_dijkstra import VERSION 
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+setup(
+    name             = 'apache-age-dijkstra',
+    version          = VERSION.VERSION,
+    description      = 'Dijkstra shortest path algorithm using apache age graph database',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author           = 'Moontasir Mahmood',
+    author_email     = 'moontasir042@gmail.com',
+    url              = 'https://github.com/Munmud/apache-age-dijkstra',
+    license          = 'Apache2.0',
+    install_requires = [ 'apache-age-python==0.0.4', "antlr4-python3-runtime==4.9.3"],
+    packages         = ['age_dijkstra',],
+    keywords         = ['Graph Database', 'Apache AGE', 'PostgreSQL','Shotrtest path', 'Dijkstra'],
+    python_requires  = '>=3.9',
+    classifiers      = [
+        'Programming Language :: Python :: 3.9'
+    ]
+)
