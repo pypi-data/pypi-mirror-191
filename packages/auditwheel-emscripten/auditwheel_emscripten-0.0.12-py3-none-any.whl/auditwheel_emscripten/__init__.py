94c192148b830928bb25eb247ca427282420d013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:33 +0000

    Add package auditwheel-emscripten/0.0.12/auditwheel_emscripten-0.0.12-py3-none-any.whl

---
+++ packages/auditwheel-emscripten/auditwheel_emscripten-0.0.12-py3-none-any.whl/auditwheel_emscripten/__init__.py
@@ -0,0 +1,17 @@
+from .exports import get_exports
+from .imports import get_imports
+from .repair import copylib, repair, repair_extracted, resolve_sharedlib
+from .show import show, show_dylib, show_wheel
+
+__version__ = "0.0.12"
+__all__ = [
+    "repair",
+    "repair_extracted",
+    "resolve_sharedlib",
+    "show",
+    "show_wheel",
+    "show_dylib",
+    "copylib",
+    "get_exports",
+    "get_imports",
+]
