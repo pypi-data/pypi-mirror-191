ec96159a90ad82c9f6ac325575c0cab8a35db8f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:17 +0000

    Add package SuperTelBot/0.1.8/SuperTelBot-0.1.8.tar.gz

---
+++ packages/SuperTelBot/SuperTelBot-0.1.8.tar.gz/SuperTelBot-0.1.8/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='SuperTelBot',
+    version='0.1.8',
+    packages=find_packages(include=['supertelbot*']),
+    include_package_data=True,
+    package_data={
+        'supertelbot/bots': ['passwords.json'],
+        '': ['requirements.txt']
+    },
+    install_requires=open('requirements.txt').read().splitlines(),
+    data_files=[('supertelbot', ['supertelbot/bots/*.json'])],
+    description='Easier way to create Telegram Bots',
+    author='Connor & Lil Homer',
+    license='MIT',
+    zip_safe=False,
+)
