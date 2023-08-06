268b3c95c452ad700b5addc510418c9e824d42a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:39 +0000

    Add package brownian-motion-generator/1.0.7/brownian_motion_generator-1.0.7.tar.gz

---
+++ packages/brownian-motion-generator/brownian_motion_generator-1.0.7.tar.gz/brownian_motion_generator-1.0.7/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name='brownian_motion_generator',
+    version='1.0.7',
+    author='MattyTokenomics',
+    author_email='mattytokenomics@protonmail.com',
+    description='Generating brownian motion random walks with custom skew, kurtosis, mean reversion, correlation, and non-normality.',
+    readme = "README.md",
+    url='https://github.com/mattyTokenomics/brownian_motion_generator',
+    project_urls = {},
+    license='GNU General Public License v3.0',
+    packages=['brownian_motion_generator'],
+    install_requires=['typing',
+    'dataclasses',
+    'sklearn',
+    'numpy',
+    'scipy',
+    'statsmodels',
+    'tqdm'],
+)
