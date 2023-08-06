445b866d1e1eaa60064f2c5a972fe81a7ca98ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:52 +0000

    Add package phronesitron/0.0.4/phronesitron-0.0.4.tar.gz

---
+++ packages/phronesitron/phronesitron-0.0.4.tar.gz/phronesitron-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='phronesitron',
+   version='0.0.4',
+   description='Commandline tool to request help from ML systems',
+   author='Reto Stamm',
+   author_email='phronesitron@retostamm.com',
+   packages=['phronesitron'],  
+   url="https://github.com/retospect/phronesitron",
+   install_requires=['wheel', 'openai', 'argparse', 'datetime', 'pyperclip'], 
+   scripts=['phronesitron/ph']
+)
+
+import openai
+import os
+import sys
+import textwrap as tr
+import argparse
+import time
+from termcolor import colored
+import datetime
+import pyperclip as pc
