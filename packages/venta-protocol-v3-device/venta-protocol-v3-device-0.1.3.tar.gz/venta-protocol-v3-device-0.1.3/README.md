d841a043c7f2197dc7b5c47526fe3328e21cf0fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:53 +0000

    Add package venta-protocol-v3-device/0.1.3/venta-protocol-v3-device-0.1.3.tar.gz

---
+++ packages/venta-protocol-v3-device/venta-protocol-v3-device-0.1.3.tar.gz/venta-protocol-v3-device-0.1.3/README.md
@@ -0,0 +1,9 @@
+Library to control Venta Air Humidifiers that use Protol Version 3 (via REST API on Port 80) 
+
+Example usage:
+``` { .py }
+from venta_protocol_v3_device import Venta_Protocol_v3_Device
+
+d = Venta_Protocol_v3_Device("192.168.178.87")
+print(d.toJSON())
+```
---
+++ packages/venta-protocol-v3-device/venta-protocol-v3-device-0.1.3.tar.gz/venta-protocol-v3-device-0.1.3/setup.py
@@ -0,0 +1,38 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# This call to setup() does all the work
+setup(
+    name="venta-protocol-v3-device",
+    version="0.1.3",
+    description="Control Venta Air Humidifiers that use Protocol Version 3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bobiboy/venta_protocol_v3_device",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
+    ],
+    packages=["venta_protocol_v3_device"],
+    include_package_data=True,
+    install_requires=["func_timeout"]
+)
