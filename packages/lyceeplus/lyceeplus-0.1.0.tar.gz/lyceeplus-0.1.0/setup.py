b579e5441b9a6d904ef251287045a1520193f4d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:26 +0000

    Add package lyceeplus/0.1.0/lyceeplus-0.1.0.tar.gz

---
+++ packages/lyceeplus/lyceeplus-0.1.0.tar.gz/lyceeplus-0.1.0/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['lyceeplus']
+setup_kwargs = {
+    'name': 'lyceeplus',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Pierre Lemaitre',
+    'author_email': 'oultetman@sfr.fr',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
