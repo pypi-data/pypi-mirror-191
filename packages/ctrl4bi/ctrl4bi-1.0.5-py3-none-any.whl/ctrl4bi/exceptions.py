3db20de8c5ea676bff3c3e3b30b16b615ca6d7e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:41 +0000

    Add package ctrl4bi/1.0.5/ctrl4bi-1.0.5-py3-none-any.whl

---
+++ packages/ctrl4bi/ctrl4bi-1.0.5-py3-none-any.whl/ctrl4bi/exceptions.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+About Ctrl4BI
+    Open Source Project Developed by VKreaT (www.vkreat.com)
+    Ctrl4BI has automated methods to automate Business Intelligence solutions
+
+About ctrl4bi.exceptions
+    The module has user defined exceptions that are used by other functions in the package
+
+Last Updated On: 14 Feb 2023
+"""
+
+class ExecutionError(Exception):
+    """
+    User Defined Exception
+    Please check the logs
+    """
+    pass
