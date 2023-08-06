317cb88eae065542496f4bff5947976b60d7a9ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:12 +0000

    Add package scratchconnect/4.5/scratchconnect-4.5-py3-none-any.whl

---
+++ packages/scratchconnect/scratchconnect-4.5-py3-none-any.whl/scratchconnect/scOnlineIDE.py
@@ -0,0 +1,22 @@
+"""
+Code to make it work in Online IDEs
+Don't use this code
+"""
+
+from urllib.parse import urlparse
+import requests
+from requests import request
+
+headers = {"Library": "ScratchConnect.py"}
+
+
+def _get(url, params=None, **kwargs):
+    if urlparse(url).netloc in ["api.scratch.mit.edu", "scratch.mit.edu", "cdn2.scratch.mit.edu"]:
+        url = "https://sc-proxy.deta.dev/get/" + url
+    
+    kwargs["headers"] = headers
+    return request("get", url, params=params, **kwargs)
+
+
+def _change_request_url():
+    requests.get = _get
