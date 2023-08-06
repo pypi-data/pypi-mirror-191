5be58db7184c2947ffad2d01ef81e9f155d141e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:51 +0000

    Add package tutubo/0.0.1a4/tutubo-0.0.1a4.tar.gz

---
+++ packages/tutubo/tutubo-0.0.1a4.tar.gz/tutubo-0.0.1a4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tutubo',
+    version='0.0.1a4',
+    packages=['tutubo'],
+    url='https://github.com/OpenJarbas/tutubo',
+    license='Apache',
+    author='jarbasAI',
+    install_requires=["bs4", "requests", "pytube", "ytmusicapi"],
+    author_email='jarbasai@mailfence.com',
+    description='wrapper around pytube with some new functionality'
+)
