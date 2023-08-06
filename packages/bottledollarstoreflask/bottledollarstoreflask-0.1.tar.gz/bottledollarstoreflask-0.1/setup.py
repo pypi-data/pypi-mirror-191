604e968c82274484ea198d7256069d4892448278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:22 +0000

    Add package bottledollarstoreflask/0.1/bottledollarstoreflask-0.1.tar.gz

---
+++ packages/bottledollarstoreflask/bottledollarstoreflask-0.1.tar.gz/bottledollarstoreflask-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup( name = 'bottledollarstoreflask',
+      version = '0.1',
+      description = 'dollar store flask',
+      url = 'https://github.com/rigityrooahhdev/bottle',
+      author = 'rigityrooahhdev',
+      author_email = 'rigityrooahh.dev@gmail.com',
+      license = 'MIT',
+      packages = ['bottledollarstoreflask'],
+      zip_safe = False )
