8fbcbdca428101d22a76169383d2eecca7c9ddb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:15 +0000

    Add package esses/0.0.1/esses-0.0.1.tar.gz

---
+++ packages/esses/esses-0.0.1.tar.gz/esses-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+VERSION = "0.0.1"
+
+
+setup(
+    name="esses",
+    version=VERSION,
+    description="Stupidly Simple Storage",
+    url="https://github.com/thomasms/sss",
+    author="Tom Stainer",
+    author_email="me@physicstom.com",
+    license="Apache License 2.0",
+    packages=[
+        "sss",
+    ],
+    install_requires=[],
+    python_requires=">=3.8",
+    scripts=[],
+    setup_requires=[
+        "pytest-runner",
+    ],
+    # test_suite="tests.testsuite",
+    # tests_require=[
+    #     "pytest",
+    # ],
+    include_package_data=True,
+    zip_safe=False,
+)
