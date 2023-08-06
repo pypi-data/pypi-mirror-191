24e6af29c1dae7a41131cb91d4a37182cfdb3d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:04 +0000

    Add package express-option-chain/1.0.3/express-option-chain-1.0.3.tar.gz

---
+++ packages/express-option-chain/express-option-chain-1.0.3.tar.gz/express-option-chain-1.0.3/src/expressoptionchain/redis_helper.py
@@ -0,0 +1,20 @@
+import redis
+
+
+def get_redis_config(db=0):
+    return {'db': 0}
+
+
+class RedisConfig:
+    def __init__(self, port=6739, db=0):
+        self.db = db
+        self.port = port
+
+
+def get_redis_instance(redis_config: RedisConfig = RedisConfig()):
+    r = redis.StrictRedis(decode_responses=True, port=redis_config.port, db=redis_config.db)
+    try:
+        r.ping()
+    except:
+        raise Exception(f'Redis is not running port {redis_config.port} ')
+
