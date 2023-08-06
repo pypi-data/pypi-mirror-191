2d770723a5c8a704d503b20da605538ba5f5699e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:29 +0000

    Add package mop-utils-test/1.6.5/mop_utils_test-1.6.5.tar.gz

---
+++ packages/mop-utils-test/mop_utils_test-1.6.5.tar.gz/mop_utils_test-1.6.5/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+   name='mop_utils_test',
+   version='1.6.5',
+   description='Utilities for MOP',
+   packages=['mop_utils'],
+   package_dir={'mop_utils': './mop_utils'},
+   python_requires='>=3.8.0',
+   install_requires = [  
+      'pyraisdk==0.1.0'
+   ]
+)
