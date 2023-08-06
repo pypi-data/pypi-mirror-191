6a836af081cecf427944efed8b72d9337dc26638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:15 +0000

    Add package openai-helper/0.1.30/openai-helper-0.1.30.tar.gz

---
+++ packages/openai-helper/openai-helper-0.1.30.tar.gz/openai-helper-0.1.30/openai_helper/__init__.py
@@ -0,0 +1,42 @@
+from typing import Optional
+from baseblock import EnvIO
+
+from .bp import *
+from .svc import *
+from .dmo import *
+
+from .bp.openai_completion import OpenAICompletion
+from .bp.extract_output import ExtractOutput
+
+
+def call(input_prompt: str,
+         max_tokens: int = 256,
+         temperature: float = 0.7) -> Optional[str]:
+    """ Call OpenAI
+
+    Args:
+        input_prompt (str): a defined input prompt
+        max_tokens (int, optional): max tokens to use. Defaults to 256.
+        temperature (float, optional): the temperature. Defaults to 0.7.
+
+    Returns:
+        Optional[str]: _description_
+    """
+
+    if not EnvIO.exists_as_true('USE_OPENAI'):
+        return None
+
+    bp = OpenAICompletion()
+
+    d_result = bp.run(
+        input_prompt=input_prompt,
+        engine='text-davinci-003',
+        temperature=temperature,
+        max_tokens=max_tokens)
+
+    if not d_result or not d_result['output']:
+        return None
+
+    return ExtractOutput().process(
+        input_text=input_prompt,
+        d_result=d_result)
