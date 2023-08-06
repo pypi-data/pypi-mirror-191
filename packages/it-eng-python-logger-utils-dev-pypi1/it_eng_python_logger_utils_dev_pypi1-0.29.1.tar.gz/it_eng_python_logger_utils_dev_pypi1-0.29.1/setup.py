c7be58ea4db6f9d4b86705942e46335d172ce91c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:27 +0000

    Add package it-eng-python-logger-utils-dev-pypi1/0.29.1/it_eng_python_logger_utils_dev_pypi1-0.29.1.tar.gz

---
+++ packages/it-eng-python-logger-utils-dev-pypi1/it_eng_python_logger_utils_dev_pypi1-0.29.1.tar.gz/it_eng_python_logger_utils_dev_pypi1-0.29.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="it_eng_python_logger_utils_dev_pypi1",
+    version="0.29.1",
+    author="Dinnermonster",
+    author_email="it-engineering@github.com",
+    description="A logger util helper for Azure app insights and structured logging",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/it-eng-packages/packages/it_eng_python_logger_utils",
+    packages=find_packages(),
+    install_requires=[
+        "opencensus-ext-requests",
+        "opencensus-ext-azure",
+        "requests",
+        "it_eng_python_logger_utils_dev_pypi1"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
