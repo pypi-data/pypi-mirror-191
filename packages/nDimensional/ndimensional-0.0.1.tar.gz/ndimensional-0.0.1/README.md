facdced03614bcbfa997466b85290fff5c0e1b02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:17 +0000

    Add package nDimensional/0.0.1/ndimensional-0.0.1.tar.gz

---
+++ packages/nDimensional/ndimensional-0.0.1.tar.gz/ndimensional-0.0.1/README.md
@@ -0,0 +1,22 @@
+# nd-core-lib
+
+[![standard-readme compliant](https://img.shields.io/badge/standard--readme-OK-green.svg?style=flat-square)](https://github.com/RichardLitt/standard-readme)
+
+Core library for nDimensional.
+
+## Table of Contents
+
+- [Install](#install)
+- [Usage](#usage)
+
+## Install
+
+```
+
+```
+
+## Usage
+
+```
+
+```
---
+++ packages/nDimensional/ndimensional-0.0.1.tar.gz/ndimensional-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'ndimensional',
+  packages = ['ndimensional'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'Core library for nDimensional',
+  author = 'nD',
+  author_email = 'dev@nd.com',
+  url = 'https://github.com/analytXbook/nd-core-lib',
+  download_url = 'https://github.com/analytXbook/nd-core-lib/archive/refs/tags/0.0.1.tar.gz',
+  keywords = ['ndimensional', 'nd'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
