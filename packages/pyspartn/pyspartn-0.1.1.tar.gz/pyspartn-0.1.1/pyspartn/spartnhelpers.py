96060ab1ae31e5aa46adee933982fee5bc3088eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:16 +0000

    Add package pyspartn/0.1.1/pyspartn-0.1.1.tar.gz

---
+++ packages/pyspartn/pyspartn-0.1.1.tar.gz/pyspartn-0.1.1/pyspartn/spartnhelpers.py
@@ -0,0 +1,31 @@
+"""
+Collection of SPARTN helper methods which can be used
+outside the SPARTNMessage or SPARTNReader classes
+
+Created on 10 Feb 2023
+
+:author: semuadmin
+:copyright: SEMU Consulting © 2023
+:license: BSD 3-Clause
+"""
+# pylint: disable=invalid-name
+
+
+def bitsval(bitfield: bytes, position: int, length: int) -> int:
+    """
+    Get unisgned integer value of masked bits in bitfield.
+
+    :param bytes bitfield: bytes
+    :param int position: position in bitfield, from leftmost bit
+    :param int length: length of masked bits
+    :return: value
+    :rtype: int
+    """
+
+    lbb = len(bitfield) * 8
+    if position + length > lbb:
+        return None
+
+    return int.from_bytes(bitfield, "big") >> (lbb - position - length) & (
+        pow(2, length) - 1
+    )
