21422dc7974364f51e1fc7a8dc7918d09b9e60e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:13 +0000

    Add package taktile-auth/0.0.19/taktile_auth-0.0.19.tar.gz

---
+++ packages/taktile-auth/taktile_auth-0.0.19.tar.gz/taktile_auth-0.0.19/taktile_auth/utils/cache.py
@@ -0,0 +1,16 @@
+import typing as t
+
+
+class Cache(t.Protocol):
+    """Cache Protocol
+    Keep this in sync with the package defined in flow_services/packages/cache
+    """
+
+    def get(self, key: str) -> t.Optional[str]:
+        """Get a value"""
+
+    def put(self, key: str, value: str) -> None:
+        """Put a value"""
+
+    def delete(self, key: str) -> None:
+        """Delete a values"""
