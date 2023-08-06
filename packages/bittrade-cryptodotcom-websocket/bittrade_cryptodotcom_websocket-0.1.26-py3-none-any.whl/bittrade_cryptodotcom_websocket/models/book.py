f29f464a3e7b3198f25b38dcf700ed0477273a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:04 +0000

    Add package bittrade-cryptodotcom-websocket/0.1.26/bittrade_cryptodotcom_websocket-0.1.26-py3-none-any.whl

---
+++ packages/bittrade-cryptodotcom-websocket/bittrade_cryptodotcom_websocket-0.1.26-py3-none-any.whl/bittrade_cryptodotcom_websocket/models/book.py
@@ -0,0 +1,15 @@
+from decimal import Decimal
+from typing import NamedTuple, TypedDict
+
+RawOrderbookEntry = tuple[str, str, str]
+RawOrderbook = dict[str, list[RawOrderbookEntry | int]]
+
+class OrderbookEntryNamedTuple(NamedTuple):
+    price: Decimal
+    quantity: Decimal
+    number_of_orders: int
+
+class OrderbookDict(TypedDict):
+    bids: list[OrderbookEntryNamedTuple]
+    asks: list[OrderbookEntryNamedTuple]
+    t: int
