33524f8ee88def1de6e8df3a1f5e736ebb08e6b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:23 +0000

    Add package django-admin-taggit-ui/0.1.dev0/django-admin-taggit-ui-0.1.dev0.tar.gz

---
+++ packages/django-admin-taggit-ui/django-admin-taggit-ui-0.1.dev0.tar.gz/django-admin-taggit-ui-0.1.dev0/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+
+def read(filename):
+    path = os.path.join(os.path.dirname(__file__), filename)
+    with open(path, encoding="utf-8") as file:
+        return file.read()
+
+
+version = __import__("taggit_ui").__version__
+if '-dev' in version:
+    dev_status = 'Development Status :: 3 - Alpha'
+elif '-beta' in version:
+    dev_status = 'Development Status :: 4 - Beta'
+else:
+    dev_status = 'Development Status :: 5 - Production/Stable'
+
+
+setup(
+    name="django-admin-taggit-ui",
+    version=version,
+    description="Filter and action to work with tags in the django-admin-backend.",
+    long_description=read("README.rst"),
+    author="Thomas Leichtfuß",
+    author_email="thomas.leichtfuss@posteo.de",
+    license="BSD License",
+    platforms=["OS Independent"],
+    packages=find_packages(exclude=["tests"]),
+    include_package_data=True,
+    install_requires=[
+        "Django<=3.2",
+        "django-taggit",
+        "djangorestframework",
+    ],
+    classifiers=[
+        dev_status,
+        "Framework :: Django",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    ],
+    zip_safe=True,
+)
