04f08caf44564283186f6ab825a71ad052e82cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:54 +0000

    Add package use-case-registry/1.0.1/use_case_registry-1.0.1-py3-none-any.whl

---
+++ packages/use-case-registry/use_case_registry-1.0.1-py3-none-any.whl/use_case_registry/use_case.py
@@ -0,0 +1,27 @@
+"""Interface for concrete usecases."""
+import abc
+from typing import Any, Type, Union
+
+from result import Result
+
+from .errors import IdentifiedError, NotIdentifiedError
+from .registry import UseCaseRegistry
+
+
+class IUsecase(abc.ABC):
+    """Use case interface."""
+
+    def __init__(
+        self,
+        write_ops_registry: UseCaseRegistry[Any],
+        errors_registry: UseCaseRegistry[Type[IdentifiedError]],
+    ) -> None:
+        """use case constructor."""
+        self.write_ops_registry = write_ops_registry
+        self.errors_registry = errors_registry
+
+    @abc.abstractmethod
+    def execute(
+        self,
+    ) -> Result[Any, Union[IdentifiedError, NotIdentifiedError]]:
+        """Workflow execution command to complete the use case."""
