23154f6068f1601ce7fa4351e5d0a67b3b894090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:37 +0000

    Add package flux-local/0.1.0/flux-local-0.1.0.tar.gz

---
+++ packages/flux-local/flux-local-0.1.0.tar.gz/flux-local-0.1.0/tests/test_flux_local.py
@@ -0,0 +1,33 @@
+"""Tests for the flux-local command line tool."""
+
+
+from pathlib import Path
+
+from flux_local.command import Command, run
+
+TESTDATA = Path("tests/testdata/cluster/")
+
+
+async def test_build() -> None:
+    """Test flux-local build command."""
+    await run(Command(["flux-local", "build", str(TESTDATA)]))
+
+
+async def test_build_helm() -> None:
+    """Test flux-local build command with helm."""
+    await run(Command(["flux-local", "build", "--enable-helm", str(TESTDATA)]))
+
+
+async def test_diff() -> None:
+    """Test flux-local diff command."""
+    await run(Command(["flux-local", "diff", str(TESTDATA)]))
+
+
+async def test_manifest() -> None:
+    """Test flux-local manifest command."""
+    await run(Command(["flux-local", "manifest", str(TESTDATA)]))
+
+
+async def test_test() -> None:
+    """Test flux-local test command."""
+    await run(Command(["flux-local", "test", str(TESTDATA)]))
