b540b7c6dd5af812473ec01f4c776df4fe08a4cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:02 +0000

    Add package PipBERT/4.0.6/PipBERT-4.0.6.tar.gz

---
+++ packages/PipBERT/PipBERT-4.0.6.tar.gz/PipBERT-4.0.6/src/pybert/__init__.py
@@ -0,0 +1,16 @@
+"""A package of Python modules, used by the *PyBERT* application.
+
+.. moduleauthor:: David Banas <capn.freako@gmail.com>
+
+Original Author: David Banas <capn.freako@gmail.com>
+
+Original Date:   17 June 2014
+
+Testing by:      Mark Marlett <mark.marlett@gmail.com>
+
+Copyright (c) 2014 by David Banas; All rights reserved World wide.
+"""
+__version__ = "4.0.6"
+__date__ = "February 13, 2023"
+__authors__ = "David Banas & David Patterson"
+__copy__ = "Copyright (c) 2014 David Banas, 2019 David Patterson"
