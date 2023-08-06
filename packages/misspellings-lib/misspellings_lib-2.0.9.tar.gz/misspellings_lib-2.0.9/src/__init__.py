1037ba11c53070819b48048e090529815a3b341f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:15 +0000

    Add package misspellings-lib/2.0.9/misspellings_lib-2.0.9.tar.gz

---
+++ packages/misspellings-lib/misspellings_lib-2.0.9.tar.gz/misspellings_lib-2.0.9/src/__init__.py
@@ -0,0 +1,7 @@
+from .misspelling_lib.misspelling_detector import MisspellingDetector
+from .misspelling_lib.misspelling_factory import MisspellingFactory
+
+__all__ = [
+    "MisspellingDetector",
+    "MisspellingFactory",
+]
