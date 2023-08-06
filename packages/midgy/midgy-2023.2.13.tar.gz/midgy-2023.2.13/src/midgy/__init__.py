e2416f44c6026b0c191294b1427d7e8a06f18a1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:23 +0000

    Add package midgy/2023.2.13/midgy-2023.2.13.tar.gz

---
+++ packages/midgy/midgy-2023.2.13.tar.gz/midgy-2023.2.13/src/midgy/__init__.py
@@ -0,0 +1,6 @@
+"""midgy translates (ie tangles) commonmark markdown to python code."""
+
+__all__ = "Python", "md_to_python"
+from .python import Python
+
+tangle = md_to_python = Python.code_from_string
