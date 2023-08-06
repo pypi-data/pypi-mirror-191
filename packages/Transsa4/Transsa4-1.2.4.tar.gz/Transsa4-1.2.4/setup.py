0f09ee7baa7573cd6323e04d9ba331808a46b008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:11 +0000

    Add package Transsa4/1.2.4/Transsa4-1.2.4.tar.gz

---
+++ packages/Transsa4/Transsa4-1.2.4.tar.gz/Transsa4-1.2.4/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from setuptools import find_packages, setup
+setup(
+    name='Transsa4',
+    version='1.2.4',
+    packages=find_packages(),
+    py_modules=['Transsa4']
+
+)
+
+
