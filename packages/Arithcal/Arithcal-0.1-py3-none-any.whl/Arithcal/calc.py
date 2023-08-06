166d5ed19deb31a64e41737ec6709d3e81cdc2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:02 +0000

    Add package Arithcal/0.1/Arithcal-0.1-py3-none-any.whl

---
+++ packages/Arithcal/Arithcal-0.1-py3-none-any.whl/Arithcal/calc.py
@@ -0,0 +1,53 @@
+from typing import Any
+
+def add(a: Any, b: Any) -> Any:
+    """
+    Returns the sum of two numbers.
+
+    Parameters:
+        a (Any): The first number.
+        b (Any): The second number.
+
+    Returns:
+        Any: The sum of a and b.
+    """
+    return a + b
+
+def subtract(a: Any, b: Any) -> Any:
+    """
+    Returns the difference of two numbers.
+
+    Parameters:
+        a (Any): The first number.
+        b (Any): The second number.
+
+    Returns:
+        Any: The difference of a and b.
+    """
+    return a - b
+
+def multiply(a: Any, b: Any) -> Any:
+    """
+    Returns the product of two numbers.
+
+    Parameters:
+        a (Any): The first number.
+        b (Any): The second number.
+
+    Returns:
+        Any: The product of a and b.
+    """
+    return a * b
+
+def divide(a: Any, b: Any) -> Any:
+    """
+    Returns the division of two numbers.
+
+    Parameters:
+        a (Any): The numerator.
+        b (Any): The denominator.
+
+    Returns:
+        Any: The division of a and b.
+    """
+    return a / b
