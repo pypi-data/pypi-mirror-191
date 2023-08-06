c1eeefd934b03590c3ca6b0d9c48a389f7abeb1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:41 +0000

    Add package Mashup-102003578/1.0.1/Mashup__102003578-1.0.1.tar.gz

---
+++ packages/Mashup-102003578/Mashup__102003578-1.0.1.tar.gz/Mashup__102003578-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='Mashup__102003578',
+    version='1.0.1',
+    author='Jatin Mudgal',
+    author_email='jmudgal_be20@thapar.edu',
+    description='Create Mashups from YouTube vidoes',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
