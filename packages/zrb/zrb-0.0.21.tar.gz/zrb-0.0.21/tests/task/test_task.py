d33d1498a32b6c44fa7ad09db792199befa33e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:13 +0000

    Add package zrb/0.0.21/zrb-0.0.21.tar.gz

---
+++ packages/zrb/zrb-0.0.21.tar.gz/zrb-0.0.21/tests/task/test_task.py
@@ -0,0 +1,77 @@
+from zrb.task.task import Task
+from zrb.task_input.str_input import StrInput
+
+
+def test_task_with_no_runner():
+    task = Task(
+        name='simple-no-error',
+    )
+    main_loop = task.create_main_loop()
+    result = main_loop()
+    assert result
+
+
+def test_task_with_predefined_runner():
+    def _run(*args, **kwargs) -> str:
+        return 'hello'
+    task = Task(
+        name='hello',
+        run=_run
+    )
+    main_loop = task.create_main_loop()
+    result = main_loop()
+    assert result == 'hello'
+
+
+def test_task_with_decorated_runner():
+    task = Task(
+        name='hello',
+    )
+
+    @task.should
+    def _run(*args, **kwargs) -> str:
+        return 'hello'
+
+    main_loop = task.create_main_loop()
+    result = main_loop()
+    assert result == 'hello'
+
+
+def test_task_with_predefined_and_decorated_runner():
+    def _run(*args, **kwargs) -> str:
+        return 'hello'
+    task = Task(
+        name='hello',
+        run=_run
+    )
+    # should throw error when add decorated runner
+    # to a task with pre-existing runner
+    is_error = False
+    try:
+        @task.should
+        def _run(*args, **kwargs) -> str:
+            return 'not-hello'
+    except Exception:
+        is_error = True
+    assert is_error
+    main_loop = task.create_main_loop()
+    result = main_loop()
+    assert result == 'hello'
+
+
+def test_task_with_input():
+    def _run(*args, **kwargs) -> str:
+        name = kwargs['name']
+        favorite_drink = kwargs['favorite_drink']
+        return f'hello {name}, your favorite drink is {favorite_drink}'
+    task = Task(
+        name='hello-name',
+        inputs=[
+            StrInput(name='name'),
+            StrInput(name='favorite-drink')
+        ],
+        run=_run
+    )
+    main_loop = task.create_main_loop()
+    result = main_loop(name='Dumbledore', favorite_drink='Elixir')
+    assert result == 'hello Dumbledore, your favorite drink is Elixir'
