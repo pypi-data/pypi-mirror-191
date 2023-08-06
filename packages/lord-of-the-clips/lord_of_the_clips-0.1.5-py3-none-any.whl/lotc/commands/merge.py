277fc894f446bc3c231c19fad7e7f4d80dd90dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:02 +0000

    Add package lord-of-the-clips/0.1.5/lord_of_the_clips-0.1.5-py3-none-any.whl

---
+++ packages/lord-of-the-clips/lord_of_the_clips-0.1.5-py3-none-any.whl/lotc/commands/merge.py
@@ -0,0 +1,36 @@
+import rich_click as click
+
+from lotc.utils import (
+    build_clip_objects_from_clip_names,
+    check_valid_file_extension,
+    get_concatenated_clip_names,
+    merge_clips_and_save,
+    print_in_tree,
+    print_rich,
+)
+
+
+@click.command()
+@click.argument('clips', nargs=-1, required=True)
+@click.option('-o', '--output', help='The output file name (e.g. "merged.mp4"). Should have the ".mp4" file extension.')
+def merge(clips, output):
+    """
+    Merges the specified video CLIPS (filenames).
+
+    \b[dim]
+    Examples:
+        [green]lotc[/] [cyan]merge[/] foo-1.mp4 foo-2.mp4
+        [green]lotc[/] [cyan]merge[/] --output bar.mp4 foo-1.mp4 foo-2.mp4 baz.mp4
+    """
+    if output:
+        check_valid_file_extension(output)
+
+    print_rich('Merging these video clips:')
+    print_in_tree(branches=clips)
+
+    clip_names = clips
+    clip_objects = build_clip_objects_from_clip_names(clip_names)
+    output_file = output or get_concatenated_clip_names(clip_names)
+
+    merge_clips_and_save(clip_objects, output_file)
+    print_rich(f'\nOutput file saved as [blue]{output_file}[/blue]')
