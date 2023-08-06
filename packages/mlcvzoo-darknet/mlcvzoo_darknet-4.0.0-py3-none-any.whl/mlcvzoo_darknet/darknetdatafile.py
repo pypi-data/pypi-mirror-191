745aed7ce06c7790026f4f4aaa013d8c76a0ebdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:54 +0000

    Add package mlcvzoo-darknet/4.0.0/mlcvzoo_darknet-4.0.0-py3-none-any.whl

---
+++ packages/mlcvzoo-darknet/mlcvzoo_darknet-4.0.0-py3-none-any.whl/mlcvzoo_darknet/darknetdatafile.py
@@ -0,0 +1,52 @@
+# Copyright 2021 Open Logistics Foundation
+#
+# Licensed under the Open Logistics License 1.0.
+# For details on the licensing terms, see the LICENSE file.
+
+"""
+Module for handling the writing of a darknet data file,
+which defines the main information that is needed for training
+a model in the darknet framework.
+"""
+
+from mlcvzoo_base.utils.file_utils import ensure_dir
+
+darknet_data_file_template = """classes = {classes}
+train   = {train}
+valid   = {valid}
+names   = {names}
+backup  = {backup}
+eval    = {eval}
+"""
+
+
+class DarknetDataFile:
+    """
+    Class that handles the writing of a darknet data file,
+    which defines the main information that is needed for training
+    a model in the darknet framework.
+    """
+
+    def __init__(
+        self,
+        classes: int,
+        names: str,
+        valid: str,
+        backup: str,
+        eval: str,
+        train: str,
+    ) -> None:
+        self.data = {
+            "classes": classes,
+            "names": names,
+            "train": train,
+            "valid": valid,
+            "backup": backup,
+            "eval": eval,
+        }
+
+    def write_file(self, path: str) -> None:
+        file_content = darknet_data_file_template.format(**self.data)
+        ensure_dir(file_path=path, verbose=True)
+        with open(path, "w") as f:
+            f.write(file_content)
