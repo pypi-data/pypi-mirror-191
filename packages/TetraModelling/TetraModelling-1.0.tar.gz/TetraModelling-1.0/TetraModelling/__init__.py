5c6c514bdd86616c52e87f6c9473070594d8b30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:51 +0000

    Add package TetraModelling/1.0/TetraModelling-1.0.tar.gz

---
+++ packages/TetraModelling/TetraModelling-1.0.tar.gz/TetraModelling-1.0/TetraModelling/__init__.py
@@ -0,0 +1,1 @@
+from Tetra.main import Tetra
---
+++ packages/TetraModelling/TetraModelling-1.0.tar.gz/TetraModelling-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+# Setting up
+setup(
+    name="TetraModelling",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=['alphashape']
+)
