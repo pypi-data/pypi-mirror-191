381de993965e0aa874a2aee39894772011e6b81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:12 +0000

    Add package quisp/0.1.6/quisp-0.1.6.tar.gz

---
+++ packages/quisp/quisp-0.1.6.tar.gz/quisp-0.1.6/quisp/_version.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) zigen.
+# Distributed under the terms of the Modified BSD License.
+
+__version__ = "0.1.6"
+version_info = tuple(map(int, __version__.split(".")))
