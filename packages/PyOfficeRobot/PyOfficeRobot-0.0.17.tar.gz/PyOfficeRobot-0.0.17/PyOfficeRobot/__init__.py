ab91aa96172fda99dc65a5f40423360218f7aa8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:29 +0000

    Add package PyOfficeRobot/0.0.17/PyOfficeRobot-0.0.17.tar.gz

---
+++ packages/PyOfficeRobot/PyOfficeRobot-0.0.17.tar.gz/PyOfficeRobot-0.0.17/PyOfficeRobot/__init__.py
@@ -0,0 +1,8 @@
+from PyOfficeRobot.api import chat
+from PyOfficeRobot.api import file
+from PyOfficeRobot.lib.CONST import SPLIT_LINE
+
+print(SPLIT_LINE)
+print('【PyOfficeRobot，微信机器人】')
+print('【全部功能】：https://www.python-office.com/office/robot.html')
+print(SPLIT_LINE)
