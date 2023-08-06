d5f2d384d92039acf4a14967bb7cd8dd16e3f838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:22:55 +0000

    Add package nanoqa/0.0.1/nanoqa-0.0.1-py3-none-any.whl

---
+++ packages/nanoqa/nanoqa-0.0.1-py3-none-any.whl/nanoqa/components/file_processing/__init__.py
@@ -0,0 +1,5 @@
+from nanoQA.components.file_processing.convertors import TxtConvertor
+from nanoQA.components.file_processing.convertors import PDFConvertor
+from nanoQA.components.file_processing.convertors import DocxConvertor
+from nanoQA.components.file_processing.text_processor import PreProcessor
+from nanoQA.components.file_processing.x2pdf import X2PDF
---
+++ packages/nanoqa/nanoqa-0.0.1-py3-none-any.whl/nanoqa/components/file_processing/convertors/__init__.py
@@ -0,0 +1,3 @@
+from nanoQA.components.file_processing.convertors.pdf import PDFConvertor
+from nanoQA.components.file_processing.convertors.txt import TxtConvertor
+from nanoQA.components.file_processing.convertors.docx import DocxConvertor
