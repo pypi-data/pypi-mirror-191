e4b958c8836616e7ab98301b7ade1e7947fe83be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:23:24 +0000

    Add package UtilsRL/0.5.0b2/UtilsRL-0.5.0b2-cp310-cp310-win_amd64.whl

---
+++ packages/UtilsRL/UtilsRL-0.5.0b2-cp310-cp310-win_amd64.whl/UtilsRL/env/wrapper/__init__.py
@@ -0,0 +1,11 @@
+from .atari_wrapper import (
+    NoopResetEnv, 
+    MaxAndSkipEnv, 
+    EpisodicLifeEnv, 
+    FireResetEnv, 
+    WarpFrame, 
+    ScaledFloatFrame, 
+    ClipRewardEnv, 
+    FrameStack, 
+    wrap_deepmind
+)
