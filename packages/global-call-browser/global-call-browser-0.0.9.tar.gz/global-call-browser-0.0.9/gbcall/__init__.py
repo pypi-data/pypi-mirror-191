51f9fcbc93cd7a0f5c0024f1c11d9c185566670e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:34 +0000

    Add package global-call-browser/0.0.9/global-call-browser-0.0.9.tar.gz

---
+++ packages/global-call-browser/global-call-browser-0.0.9.tar.gz/global-call-browser-0.0.9/gbcall/__init__.py
@@ -0,0 +1,16 @@
+from .typeConverter import convertParameters,typeNameConvert
+def defineExpose(input, output):
+    def decorator(func):
+        def wrapper(*args):
+            inBrowser=False
+            try:
+                __file__
+            except:
+                inBrowser=True
+            if inBrowser:
+                inputTypes=list(map(typeNameConvert,input))
+                outputTypes=list(map(typeNameConvert,output))
+                return convertParameters(outputTypes,func(*convertParameters(inputTypes,args)))
+            return func(*args)
+        return wrapper
+    return decorator
