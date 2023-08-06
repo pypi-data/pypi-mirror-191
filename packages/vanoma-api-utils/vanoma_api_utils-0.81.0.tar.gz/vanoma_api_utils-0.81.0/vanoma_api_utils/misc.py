95a25eb75388fe7cbb210cbf32e9aba98a6f0f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:22 +0000

    Add package vanoma-api-utils/0.81.0/vanoma_api_utils-0.81.0.tar.gz

---
+++ packages/vanoma-api-utils/vanoma_api_utils-0.81.0.tar.gz/vanoma_api_utils-0.81.0/vanoma_api_utils/misc.py
@@ -0,0 +1,20 @@
+import os
+import shortuuid  # type: ignore
+from typing import Dict
+from .constants import ERROR_CODE
+
+
+def resolve_environment() -> str:
+    return os.environ.get("ENVIRONMENT", "testing")
+
+
+def resolve_debug() -> bool:
+    return resolve_environment() != "production"
+
+
+def create_api_error(error_code: ERROR_CODE, error_message: str) -> Dict[str, str]:
+    return {"error_code": error_code.value, "error_message": error_message}
+
+
+def get_shortuuid() -> str:
+    return shortuuid.uuid()
