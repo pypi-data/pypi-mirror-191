30eb143386d80cd828068d968797977323004e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:25 +0000

    Add package phronesitron/0.0.6/phronesitron-0.0.6.tar.gz

---
+++ packages/phronesitron/phronesitron-0.0.6.tar.gz/phronesitron-0.0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='phronesitron',
+   version='0.0.6',
+   description='Commandline tool to request help from ML systems',
+   author='Reto Stamm',
+   author_email='phronesitron@retostamm.com',
+   packages=['phronesitron'],  
+   url="https://github.com/retospect/phronesitron",
+   install_requires=['wheel', 'openai', 'argparse', 'datetime', 'pyperclip', 'pdfminer.six'], 
+   scripts=['phronesitron/ph', 'phronesitron/paper2txt']
+)
+
