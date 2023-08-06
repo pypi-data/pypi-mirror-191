18de000b166d8c67bee358b7fea378133df75284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:28 +0000

    Add package lxr-simple-notification-receiver/0.1.0/lxr_simple_notification_receiver-0.1.0.tar.gz

---
+++ packages/lxr-simple-notification-receiver/lxr_simple_notification_receiver-0.1.0.tar.gz/lxr_simple_notification_receiver-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lxr_simple_notification_receiver',
+    version='0.1.0',
+    description='A package for simplify my Python development, especially for Android development.',
+    author='hongwen000',
+    author_email='hongwen000@outlook.com',
+    url='https://github.com/hongwen000',
+    packages=find_packages(include=['*.py']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
