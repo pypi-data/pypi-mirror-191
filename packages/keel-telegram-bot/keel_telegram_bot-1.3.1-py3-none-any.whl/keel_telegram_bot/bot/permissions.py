dcf57e1c0fab3561cda652b2ffe2e63fa9bee7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:44 +0000

    Add package keel-telegram-bot/1.3.1/keel_telegram_bot-1.3.1-py3-none-any.whl

---
+++ packages/keel-telegram-bot/keel_telegram_bot-1.3.1-py3-none-any.whl/keel_telegram_bot/bot/permissions.py
@@ -0,0 +1,31 @@
+from telegram import Update
+from telegram.ext import CallbackContext
+from telegram_click.permission.base import Permission
+
+from keel_telegram_bot.config import Config
+
+
+class _ConfigAdmins(Permission):
+
+    def __init__(self):
+        self._config = Config()
+
+    async def evaluate(self, update: Update, context: CallbackContext) -> bool:
+        from_user = update.effective_message.from_user
+        return from_user.username in self._config.TELEGRAM_ADMIN_USERNAMES.value
+
+
+CONFIG_ADMINS = _ConfigAdmins()
+
+
+class _ConfigChatId(Permission):
+
+    def __init__(self):
+        self._config = Config()
+
+    async def evaluate(self, update: Update, context: CallbackContext) -> bool:
+        chat_id = update.effective_message.chat_id
+        return str(chat_id) in self._config.TELEGRAM_CHAT_IDS.value
+
+
+CONFIGURED_CHAT_ID = _ConfigChatId()
---
+++ packages/keel-telegram-bot/keel_telegram_bot-1.3.1-py3-none-any.whl/tests/test_bot.py
@@ -0,0 +1,88 @@
+from keel_telegram_bot import util
+from tests import TestBase
+
+
+class BotTest(TestBase):
+
+    def test_is_filtered_for_real(self):
+        chat_id = "123456789"
+        identifier = "deployment/local-path-storage/local-path-provisioner:v0.0.22"
+
+        filters = [
+            {
+                "chat_id": "123456789",
+                "identifier": ".*satisfactory.*"
+            },
+            {
+                "chat_id": "-123456789123456",
+                "identifier": ".*"
+            }
+        ]
+
+        result = util._is_filtered_for(filters, chat_id, identifier)
+
+        self.assertTrue(result)
+
+    def test_is_filtered_for_real2(self):
+        chat_id = "-123456789123456"
+        identifier = "deployment/local-path-storage/local-path-provisioner:v0.0.22"
+
+        filters = [
+            {
+                "chat_id": "123456789",
+                "identifier": ".*"
+            },
+            {
+                "chat_id": "-123456789123456",
+                "identifier": ".*satisfactory.*"
+            }
+        ]
+
+        result = util._is_filtered_for(filters, chat_id, identifier)
+
+        self.assertTrue(result)
+
+    def test_is_filtered_for_false(self):
+        chat_id = "123456"
+        identifier = "deployment/gameservers/satisfactory"
+
+        filters = [
+            {
+                "chat_id": "123456",
+                "identifier": ".*satisfactory.*"
+            }
+        ]
+
+        result = util._is_filtered_for(filters, chat_id, identifier)
+
+        self.assertFalse(result)
+
+    def test_is_filtered_for_true(self):
+        chat_id = "123456"
+        identifier = "deployment/gameservers/satisfactory"
+
+        filters = [
+            {
+                "chat_id": "123456",
+                "identifier": ".*wiki.*"
+            }
+        ]
+
+        result = util._is_filtered_for(filters, chat_id, identifier)
+
+        self.assertTrue(result)
+
+    def test_is_filtered_for_no_match(self):
+        chat_id = "123456"
+        identifier = "deployment/gameservers/satisfactory"
+
+        filters = [
+            {
+                "chat_id": "654321",
+                "identifier": ".*satisfactory.*"
+            }
+        ]
+
+        result = util._is_filtered_for(filters, chat_id, identifier)
+
+        self.assertFalse(result)
