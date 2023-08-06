715c7121fffa4f0b9057df677ff0a0c1ac1b5ff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:47 +0000

    Add package hello-world-pkg-wesley-wang/0.0.1/hello_world_pkg_wesley_wang-0.0.1.tar.gz

---
+++ packages/hello-world-pkg-wesley-wang/hello_world_pkg_wesley_wang-0.0.1.tar.gz/hello_world_pkg_wesley_wang-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="hello_world_pkg_wesley_wang",
+    version="0.0.1",
+    author="wesleywang",
+    author_email="wangwes@gmail.com",
+    description="This is a temporary description for the package.",
+    long_description_content_type="text/markdown",
+    url="",
+    install_requires=["numpy"],
+    license="MIT",
+    keywords=["temporary", "hello", "world"],
+    packages=find_packages(),
+    classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License", "Operating System :: OS Independent"]
+    )
