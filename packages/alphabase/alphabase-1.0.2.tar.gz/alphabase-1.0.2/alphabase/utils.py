d83860e8509bc3ebd96826de3b7d3f040367ec73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:10 +0000

    Add package alphabase/1.0.2/alphabase-1.0.2.tar.gz

---
+++ packages/alphabase/alphabase-1.0.2.tar.gz/alphabase-1.0.2/alphabase/utils.py
@@ -0,0 +1,33 @@
+import tqdm
+import os
+import sys
+import pandas as pd
+import itertools
+
+# from alphatims
+def process_bar(iterator, len_iter):
+    with tqdm.tqdm(total=len_iter) as bar:
+        i = 0
+        for i,iter in enumerate(iterator):
+            yield iter
+            bar.update()
+        bar.update(len_iter-i-1)
+
+def _flatten(list_of_lists):
+    '''
+    Flatten a list of lists
+    '''
+    return list(
+        itertools.chain.from_iterable(list_of_lists)
+    )
+
+def explode_multiple_columns(df:pd.DataFrame, columns:list):
+    try:
+        return df.explode(columns)
+    except ValueError:
+        # pandas < 1.3.0
+        print(f'pandas=={pd.__version__} cannot explode multiple columns')
+        ret_df = df.explode(columns[0])
+        for col in columns[1:]:
+            ret_df[col] = _flatten(df[col].values)
+        return ret_df
