deb8ff377a89064b87067315c8ceb72db8d6c003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:50 +0000

    Add package datature/0.6.3/datature-0.6.3.tar.gz

---
+++ packages/datature/datature-0.6.3.tar.gz/datature-0.6.3/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+  ████
+██    ██   Datature
+  ██  ██   Powering Breakthrough AI
+    ██
+
+@File    :   setip.py
+@Author  :   Raighne.Weng
+@Version :   0.6.3
+@Contact :   raighne@datature.io
+@License :   Apache License 2.0
+@Desc    :   Setup module
+'''
+
+import setuptools
+
+# read the contents of your README file
+with open("readme.md", "r", encoding="utf8") as rd:
+    long_description = rd.read()
+
+setuptools.setup(name="datature",
+                 version="0.6.3",
+                 author="Raighne Weng",
+                 author_email="raighne@datature.io",
+                 long_description_content_type="text/markdown",
+                 long_description=long_description,
+                 description="Python bindings for the Datature API",
+                 packages=setuptools.find_namespace_packages(),
+                 python_requires=">=3.7",
+                 install_requires=["requests", "google-crc32c", "pyhumps"])
