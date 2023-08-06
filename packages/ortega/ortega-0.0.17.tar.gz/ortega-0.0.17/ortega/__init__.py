d68797573af1a2902be035db4c3192a268383cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:29 +0000

    Add package ortega/0.0.17/ortega-0.0.17.tar.gz

---
+++ packages/ortega/ortega-0.0.17.tar.gz/ortega-0.0.17/ortega/__init__.py
@@ -0,0 +1,13 @@
+from ortega.ortega import *
+from ortega.ellipses import *
+from ortega.STPoint import *
+from .visualization import *
+from .output import *
+
+__title__ = "ortega"
+__version__ = "0.0.17"
+__author__ = ""
+__url__ = ""
+__uri__ = __url__
+__license__ = "MIT"
+__description__ = ""
