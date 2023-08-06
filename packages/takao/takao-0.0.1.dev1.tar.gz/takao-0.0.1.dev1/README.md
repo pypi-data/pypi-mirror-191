45d692ac50cb2a4edde0e0d6a53ad68314564e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:21 +0000

    Add package takao/0.0.1.dev1/takao-0.0.1.dev1.tar.gz

---
+++ packages/takao/takao-0.0.1.dev1.tar.gz/takao-0.0.1.dev1/README.md
@@ -0,0 +1,24 @@
+# Takao
+
+Takao is a dark theme for Sphinx I created for use in my own projects.
+
+## Installation
+
+1. Install the Python package:
+
+   ```shell
+   pip install takao
+   ```
+
+2. Update `html_theme` in your Sphinx project configuration:
+
+   ```python
+   html_theme = "takao"
+   ```
+
+3. Create a fresh documentation build in your Sphinx project directory:
+
+   ```shell
+   make clean
+   make html
+   ```
---
+++ packages/takao/takao-0.0.1.dev1.tar.gz/takao-0.0.1.dev1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['takao']
+
+package_data = \
+{'': ['*'], 'takao': ['partials/*']}
+
+install_requires = \
+['sphinx>=4']
+
+entry_points = \
+{'sphinx.html_themes': ['takao = takao']}
+
+setup_kwargs = {
+    'name': 'takao',
+    'version': '0.0.1.dev1',
+    'description': 'A dark theme for Sphinx.',
+    'long_description': '# Takao\n\nTakao is a dark theme for Sphinx I created for use in my own projects.\n\n## Installation\n\n1. Install the Python package:\n\n   ```shell\n   pip install takao\n   ```\n\n2. Update `html_theme` in your Sphinx project configuration:\n\n   ```python\n   html_theme = "takao"\n   ```\n\n3. Create a fresh documentation build in your Sphinx project directory:\n\n   ```shell\n   make clean\n   make html\n   ```\n',
+    'author': 'Reupen Shah',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/reupen/takao',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
