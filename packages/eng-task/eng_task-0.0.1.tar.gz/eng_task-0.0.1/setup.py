61addb7fa84fb143a392c10bfea95649b19ec9e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:23 +0000

    Add package eng-task/0.0.1/eng_task-0.0.1.tar.gz

---
+++ packages/eng-task/eng_task-0.0.1.tar.gz/eng_task-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'reading a csv file from a website'
+
+
+
+# Setting up
+setup(
+    name="eng_task",
+    version=VERSION,
+    author="Jack Edmundson",
+    author_email="<jackedmundson1997@gmail.com>",
+    packages=find_packages(),
+    include_package_data=True,
+    intall_requires=['pandas','numpy'],
+    entry_points={
+        'console_scripts': [
+            # List your command line scripts here
+        ],
+    },
+    classifiers=[
+        # List the classifiers for your package here
+    ],
+    data_files=[
+        ('config', ['configure.ini']),
+    ],
+    # Add the following argument to pass the output directory when installing your package
+    extras_require={
+        'output_dir': ['output_dir']
+    }
+    )
