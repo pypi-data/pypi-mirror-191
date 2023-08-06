d9a7dd57b80dbb980d37f228b077e66158e1f5c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:24 +0000

    Add package bluescan/0.8.10/bluescan-0.8.10-py3-none-any.whl

---
+++ packages/bluescan/bluescan-0.8.10-py3-none-any.whl/bluescan/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+
+def main():
+    print("`\x1B[1;33mbluescan\x1B[0m` has evolved into `\x1B[1;34mbluing\x1B[0m`, see \x1B[1;34mhttps://github.com/fO-000/bluing\x1B[0m")
+
+
+if __name__ == '__main__':
+    main()
