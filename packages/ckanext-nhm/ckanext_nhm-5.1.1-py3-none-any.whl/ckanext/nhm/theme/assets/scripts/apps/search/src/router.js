68098a1c8530b4a72476252ace12b8289ab41e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:05 +0000

    Add package ckanext-nhm/5.1.1/ckanext_nhm-5.1.1-py3-none-any.whl

---
+++ packages/ckanext-nhm/ckanext_nhm-5.1.1-py3-none-any.whl/ckanext/nhm/theme/assets/scripts/apps/search/src/router.js
@@ -0,0 +1,13 @@
+import App from './App.vue';
+import VueRouter from 'vue-router';
+
+const routes = [
+    {path: '/search/:slug*', component: App}
+]
+const router = new VueRouter({
+                                 routes,
+                                 mode: 'history'
+                             })
+
+
+export default router;
