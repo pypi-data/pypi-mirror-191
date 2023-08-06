a29983d23d5717a389d263fe81c74494878fa6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:26 +0000

    Add package data-spec-validator/2.1.0/data-spec-validator-2.1.0.tar.gz

---
+++ packages/data-spec-validator/data-spec-validator-2.1.0.tar.gz/data-spec-validator-2.1.0/data_spec_validator/spec/__init__.py
@@ -0,0 +1,40 @@
+from .actions import validate_data_spec
+
+# Export generic validator NAME
+from .defines import (
+    AMOUNT,
+    AMOUNT_RANGE,
+    ANY_KEY_EXISTS,
+    BOOL,
+    COND_EXIST,
+    DATE,
+    DATE_RANGE,
+    DECIMAL_PLACE,
+    DICT,
+    DIGIT_STR,
+    DUMMY,
+    EMAIL,
+    FOREACH,
+    INT,
+    JSON,
+    JSON_BOOL,
+    KEY_COEXISTS,
+    LENGTH,
+    LIST,
+    LIST_OF,
+    NONE,
+    ONE_OF,
+    REGEX,
+    SELF,
+    SPEC,
+    STR,
+    UUID,
+    Checker,
+    CheckerOP,
+    DSVError,
+    ErrorMode,
+    not_,
+    reset_msg_level,
+)
+from .features import dsv_feature
+from .utils import raise_if
