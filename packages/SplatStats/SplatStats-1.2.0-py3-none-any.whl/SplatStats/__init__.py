221e75816fc8033beae2e99b1455642d8748f42e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:43 +0000

    Add package SplatStats/1.2.0/SplatStats-1.2.0-py3-none-any.whl

---
+++ packages/SplatStats/SplatStats-1.2.0-py3-none-any.whl/SplatStats/__init__.py
@@ -0,0 +1,17 @@
+
+# Constants
+from .constants import *
+from .colors import *
+# Functions
+from .auxiliary import *
+from .parsers import *
+from .stats import *
+from .files import *
+from .plots import *
+from .plotsTeam import *
+from .plotsAux import *
+# Classes
+# from .History import *
+from .Battle import *
+from .Player import *
+from .Team import *
