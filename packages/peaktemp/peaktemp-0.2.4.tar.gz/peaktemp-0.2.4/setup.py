4cb4a0a6f8f20237e3e278339b39bff4a6effde4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:45 +0000

    Add package peaktemp/0.2.4/peaktemp-0.2.4.tar.gz

---
+++ packages/peaktemp/peaktemp-0.2.4.tar.gz/peaktemp-0.2.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='peaktemp',
+    version='0.2.4',
+    license='MIT',
+    author="Jake Hofgard",
+    author_email='whofgard@stanford.edu',
+    packages=find_packages('peaktemp'),
+    package_dir={'': 'peaktemp'},
+    url='https://github.com/jakehofgard/peaktemp',
+    keywords='climate, temperature, forecasting, peak load',
+    python_requires='>=3.8',
+    install_requires=[
+        'matplotlib',
+        'pandas',
+        'numpy',
+        'seaborn',
+        'cdsapi',
+        'xarray',
+        'xgboost',
+        'scikit-learn',
+        'datetime',
+        'geopy'
+    ],
+)
