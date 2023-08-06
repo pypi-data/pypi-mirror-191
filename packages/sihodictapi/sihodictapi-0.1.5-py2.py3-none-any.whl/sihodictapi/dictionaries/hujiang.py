dad6cca47fb991fcc24fdb232d956e5f7fb30143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:20 +0000

    Add package sihodictapi/0.1.5/sihodictapi-0.1.5-py2.py3-none-any.whl

---
+++ packages/sihodictapi/sihodictapi-0.1.5-py2.py3-none-any.whl/sihodictapi/dictionaries/hujiang.py
@@ -0,0 +1,50 @@
+from enum import Enum
+
+from sihodictapi import utils
+
+
+class Hujiang:
+    """沪江小D"""
+
+    class Lang(str, Enum):
+        CN = 'cn'
+        '''中文(简体)'''
+
+        CHT = 'cht'
+        '''中文(繁体)'''
+
+        EN = 'en'
+        '''英文'''
+
+        JA = 'jp'
+        '''日语'''
+        KR = 'kr'
+        '''韩语'''
+
+        FR = 'fr'
+        '''法语'''
+
+        DE = 'de'
+        '''德语'''
+
+        ES = 'es'
+        '''西班牙语'''
+
+        TH = 'th'
+        '''泰语'''
+
+        RU = 'ru'
+        '''俄语'''
+
+        PT = 'pt'
+        '''葡萄牙语'''
+
+    cookies = {
+        'HJ_UID': '0'
+    }
+
+    @classmethod
+    def translate(cls, text: str, from_lang: Lang, to_lang: Lang) -> dict:
+        return utils.request_post(f"https://dict.hjenglish.com/v10/dict/translation/{from_lang}/{to_lang}", data={
+            'content': text
+        }, cookies=cls.cookies).json()
