389940cadcd9c1a3e19011654a973d6f61610990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:32 +0000

    Add package ctrl4ai/1.0.24/ctrl4ai-1.0.24.tar.gz

---
+++ packages/ctrl4ai/ctrl4ai-1.0.24.tar.gz/ctrl4ai-1.0.24/ctrl4ai/exceptions.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+About Ctrl4AI
+    Open Source Project Developed by VKreaT (www.vkreat.com)
+    Ctrl4AI (www.ctrl4ai.com) has automated methods to automate the entire flow of pre-processing
+
+About ctrl4ai.exceptions
+    The module has user defined exceptions that are used by other functions in the package
+
+Last Updated On: 14 Feb 2023
+"""
+
+
+class ParameterError(Exception):
+    """
+    User Defined Exception
+    Thrown because the argumrnt is incorrect
+    """
+    pass
