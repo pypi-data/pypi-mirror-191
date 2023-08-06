9cde900c30a2732cebc7bdd8bda8c565ebe0b720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:02 +0000

    Add package marcosgeo-bumbo/0.0.5/marcosgeo_bumbo-0.0.5-py3-none-any.whl

---
+++ packages/marcosgeo-bumbo/marcosgeo_bumbo-0.0.5-py3-none-any.whl/app/conftest.py
@@ -0,0 +1,16 @@
+# conftest.py
+
+import pytest
+
+from bumbo.api import API
+
+
+@pytest.fixture
+def api():
+    return API()
+
+
+@pytest.fixture
+def client(api):
+    return api.test_session()
+
