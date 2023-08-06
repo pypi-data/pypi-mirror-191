36df4825346fa21f1c523229ae587cf033e7efb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:01 +0000

    Add package playfilm-shared/2.0.1/playfilm_shared-2.0.1.tar.gz

---
+++ packages/playfilm-shared/playfilm_shared-2.0.1.tar.gz/playfilm_shared-2.0.1/src/playfilm_shared/decorators.py
@@ -0,0 +1,29 @@
+import logging
+import traceback
+from logging.handlers import SysLogHandler
+from typing import Any, Callable
+
+
+def logging_function(func: Callable[..., Any]) -> Callable[..., Any]:
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        logger = init_logger()
+        value = None
+        try:
+            value = func(*args, **kwargs)
+            if value is not None:
+                logger.info(f'[INFO] Logging in {func.__name__} return value {value}')
+        except Exception as error:
+            logger.error(
+                f'[ERROR] Error in {func.__name__} with {error} traceback: {traceback.format_exc()}')
+        return value
+
+    def init_logger():
+        host = 'logs.papertrailapp.com'
+        port = 40259
+        logger = logging.getLogger('playfilm-shared')
+        logger.setLevel(logging.DEBUG)
+        handler = SysLogHandler(address=(host, port))
+        logger.addHandler(handler)
+        return logger
+
+    return wrapper
