f483515215f6942f08ae29324f7dd35fceef710d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:48 +0000

    Add package dep-search/0.2.1/dep-search-0.2.1.tar.gz

---
+++ packages/dep-search/dep-search-0.2.1.tar.gz/dep-search-0.2.1/search/search.py
@@ -0,0 +1,50 @@
+"""Search unit."""
+
+from typing import List, Union
+
+from .internals.types import ExpressionError  # noqa
+from .internals.search import Search  # noqa
+from .internals import operators as op  # noqas
+from .internals.operators import (  # noqa
+    TypeOpEq,
+    TypeOpNe,
+    TypeOpGt,
+    TypeOpGte,
+    TypeOpLt,
+    TypeOpLte,
+    TypeOpRegex,
+    TypeOpIn,
+    TypeOpNotIn,
+    TypeOpAnd,
+    TypeOpOr,
+    TypeExpression,
+    Expression,
+    ComparisonExpression,
+    ComparisonListExpression,
+    ConditionalExpression,
+    Eq,
+    Ne,
+    Gt,
+    Gte,
+    Lt,
+    Lte,
+    Contains,
+    IContains,
+    In,
+    NotIn,
+    And,
+    Or,
+)
+
+
+def match_query(
+    expressions: List[TypeExpression],
+) -> Union[TypeExpression, None]:
+    """Match query expression."""
+
+    if not bool(expressions) or len(expressions) == 0:
+        return None
+    elif len(expressions) == 1:
+        return expressions[0]
+
+    return And(expressions)
