adfed7ccf0daf61f9567a4b46ba4deec501fe86a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:40 +0000

    Add package eng-task/0.0.9/eng_task-0.0.9.tar.gz

---
+++ packages/eng-task/eng_task-0.0.9.tar.gz/eng_task-0.0.9/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.9'
+DESCRIPTION = 'reading a csv file from a website'
+
+from setuptools import setup, find_packages
+import sys
+import os
+import configparser
+
+def save_config(directory):
+    config = configparser.ConfigParser()
+    config['DEFAULT'] = {'output_dir': directory}
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
+for arg in sys.argv:
+    if arg.startswith('--output_dir='):
+        directory = arg.split('=')[1]
+        save_config(directory)
+
+setup(
+    name='eng_task',
+    version=VERSION,
+    description='Example package',
+    author='Jack Edmundson',
+    author_email='jackedmundson1997@gmail.com',
+    packages=find_packages(),
+    data_files=[('.', ['configure.ini'])],
+    install_requires=['configparser']
+)
+    
