ca477fed0530cff437df4f21eb825dcf6f088cb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:25 +0000

    Add package xia-agent/0.0.3/xia_agent-0.0.3-cp310-none-win_amd64.whl

---
+++ packages/xia-agent/xia_agent-0.0.3-cp310-none-win_amd64.whl/xia_agent/__init__.py
@@ -0,0 +1,9 @@
+from xia_agent.pusher import Pusher
+from xia_agent.puller import Puller
+from xia_agent.synchronizer import Synchronizer
+
+__all__ = [
+    "Pusher", "Puller", "Synchronizer"
+]
+
+__version__ = "0.0.3"
