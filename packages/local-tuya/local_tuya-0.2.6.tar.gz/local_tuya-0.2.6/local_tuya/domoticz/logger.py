13413f7aa40d2e9a7f4c22746318a2a576644f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:52 +0000

    Add package local-tuya/0.2.6/local_tuya-0.2.6.tar.gz

---
+++ packages/local-tuya/local_tuya-0.2.6.tar.gz/local_tuya-0.2.6/local_tuya/domoticz/logger.py
@@ -0,0 +1,24 @@
+from logging import DEBUG, INFO, Formatter, LogRecord, StreamHandler
+
+try:
+    import DomoticzEx
+except ModuleNotFoundError:
+    from local_tuya.domoticz.types import DomoticzEx
+
+
+class DomoticzHandler(StreamHandler):
+    """Log handler to send to Domoticz logger."""
+
+    def __init__(self):
+        super().__init__()
+        self.setFormatter(Formatter("%(levelname)s: %(name)s: %(message)s"))
+        self.setLevel(DEBUG)
+
+    def emit(self, record: LogRecord) -> None:
+        msg = self.format(record)
+        if record.levelno <= DEBUG:
+            DomoticzEx.Debug(msg)
+        elif record.levelno <= INFO:
+            DomoticzEx.Log(msg)
+        else:
+            DomoticzEx.Error(msg)
