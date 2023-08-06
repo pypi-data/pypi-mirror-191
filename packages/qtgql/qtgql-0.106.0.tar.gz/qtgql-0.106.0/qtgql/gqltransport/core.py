7dc924cee8bd3fd14142948fb1e14e30106cc342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:22 +0000

    Add package qtgql/0.106.0/qtgql-0.106.0.tar.gz

---
+++ packages/qtgql/qtgql-0.106.0.tar.gz/qtgql-0.106.0/qtgql/gqltransport/core.py
@@ -0,0 +1,50 @@
+import json
+import re
+import uuid
+from typing import Generic, Optional, TypeVar, Union
+
+from attr import asdict, define
+
+from qtgql.typingref import UNSET
+
+
+@define
+class EncodeAble:
+    def asdict(self) -> dict:
+        return {k: v for k, v in asdict(self).items() if v is not UNSET}
+
+
+T = TypeVar("T")
+
+
+@define(kw_only=True)
+class QueryPayload(Generic[T], EncodeAble):
+    query: str
+    operationName: Optional[str] = UNSET
+    variables: Optional[Union[dict, T]] = UNSET
+
+    def __attrs_post_init__(self):
+        if not self.operationName:
+            match = re.search(r"(subscription|mutation|query)(.*?({|\())", self.query)
+            op_name = match.group(2).replace(" ", "").strip("{").strip("(")
+            self.operationName = op_name
+
+
+@define(kw_only=True, repr=False)
+class SafeGqlPayload(QueryPayload):
+    ...
+
+
+@define(kw_only=True)
+class GqlResult:
+    data: dict
+    errors: Optional[list[dict]] = None
+
+
+class GqlEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, uuid.UUID):  # noqa: R505
+            return str(obj)
+        elif isinstance(obj, (EncodeAble, QueryPayload)):
+            return obj.asdict()
+        return None
---
+++ packages/qtgql/qtgql-0.106.0.tar.gz/qtgql-0.106.0/qtgql/itemsystem/__init__.py
@@ -0,0 +1,3 @@
+from .core import GenericModel, RoleDoesNotExist, asdict, get_base_type, role
+
+__all__ = ["role", "get_base_type", "GenericModel", "RoleDoesNotExist", "asdict"]
