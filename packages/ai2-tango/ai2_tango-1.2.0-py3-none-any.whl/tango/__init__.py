06275c63f46e6d43c95f6fd4125b048810c895b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:18 +0000

    Add package ai2-tango/1.2.0/ai2_tango-1.2.0-py3-none-any.whl

---
+++ packages/ai2-tango/ai2_tango-1.2.0-py3-none-any.whl/tango/__init__.py
@@ -0,0 +1,33 @@
+"""
+A Python library for choreographing your machine learning research.
+"""
+
+__all__ = [
+    "Format",
+    "DillFormat",
+    "DillFormatIterator",
+    "Executor",
+    "JsonFormat",
+    "JsonFormatIterator",
+    "SqliteDictFormat",
+    "Step",
+    "step",
+    "StepResources",
+    "StepCache",
+    "StepGraph",
+    "Workspace",
+]
+
+from tango.executor import Executor
+from tango.format import (
+    DillFormat,
+    DillFormatIterator,
+    Format,
+    JsonFormat,
+    JsonFormatIterator,
+    SqliteDictFormat,
+)
+from tango.step import Step, StepResources, step
+from tango.step_cache import StepCache
+from tango.step_graph import StepGraph
+from tango.workspace import Workspace
