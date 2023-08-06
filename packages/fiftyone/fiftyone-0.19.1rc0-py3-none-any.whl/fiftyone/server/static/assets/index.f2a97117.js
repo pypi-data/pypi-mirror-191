97ed0fbe1ef70617edcb829085888b2de9d06548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:32 +0000

    Add package fiftyone/0.19.1rc0/fiftyone-0.19.1rc0-py3-none-any.whl

---
+++ packages/fiftyone/fiftyone-0.19.1rc0-py3-none-any.whl/fiftyone/server/static/assets/index.f2a97117.js
@@ -0,0 +1,1 @@
+import{aj as t,z as a,r as s,R as o,N as c}from"./index.6fc90905.js";const n=({})=>{const e=t(a);return s.exports.useLayoutEffect(()=>{e(null)},[]),o.createElement(c,null,"No dataset selected")},l=n;export{l as default};
