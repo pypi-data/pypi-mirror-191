ed854b085c2858af7e08c75be411782fb080b7f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:18 +0000

    Add package intezer-analyze-cli/1.9.2/intezer-analyze-cli-1.9.2.tar.gz

---
+++ packages/intezer-analyze-cli/intezer-analyze-cli-1.9.2.tar.gz/intezer-analyze-cli-1.9.2/intezer_analyze_cli/config.py
@@ -0,0 +1,22 @@
+class Config(object):
+    def __init__(self):
+        # Client
+        self.unusual_amount_in_dir = 1000
+
+        # Urls
+        self.api_url = 'https://analyze.intezer.com/api/'
+        self.api_version = 'v2-0'
+        self.analyses_url = 'https://analyze.intezer.com/analyses'
+        self.endpoint_analyses_url = 'https://analyze.intezer.com/endpoint-analyses'
+        self.index_results_url = 'https://analyze.intezer.com/private-indexed-files'
+
+        # Key Store
+        self.key_dir_name = '.intezer'
+        self.key_file_name = 'key'
+        self.url_file_name = 'url'
+
+        # Other
+        self.is_cloud = True
+
+
+default_config = Config()
