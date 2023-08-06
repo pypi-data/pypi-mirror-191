f711cd6c9688124f1b3210086bd44cd608b15900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:28 +0000

    Add package libtakiyasha/2.1.0/libtakiyasha-2.1.0.tar.gz

---
+++ packages/libtakiyasha/libtakiyasha-2.1.0.tar.gz/libtakiyasha-2.1.0/src/libtakiyasha/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from . import kgmvpr, kwm, ncm, qmc
+from . import pkgmetadata
+
+__all__ = ['pkgmetadata', 'kgmvpr', 'kwm', 'ncm', 'qmc']
