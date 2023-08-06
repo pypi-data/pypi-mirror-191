d5a92c0f818ccf10c784852fe866dd8ab036df92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:30:55 +0000

    Add package Langton-project/0.1/Langton_project-0.1.tar.gz

---
+++ packages/Langton-project/Langton_project-0.1.tar.gz/Langton_project-0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup (
+    name = "Langton_project",
+    version = "0.1",
+    description = "Study of cellular automata",
+    long_description = read("README.md"),
+    author = "CHALAMET Victor",
+    author_email = "victorchalamet@gmail.com",
+    packages = ["Langton_project", "Langton_project_interface"],
+    license = "MIT",
+    url = "https://github.com/victorchalamet/Langton_project",
+    download_url = "https://github.com/victorchalamet/Langton_project/archive/refs/tags/v_0.1.tar.gz",
+    keywords = ["emergence", "animation", "data"],
+    install_requires = [
+        "numpy",
+        "datetime",
+        ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10'
+    ]
+)
