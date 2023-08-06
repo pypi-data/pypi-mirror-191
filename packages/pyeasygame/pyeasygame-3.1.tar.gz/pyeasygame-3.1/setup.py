18e811c38bad109c0f9e1ac0388bb295dfc5b81d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:10 +0000

    Add package pyeasygame/3.1/pyeasygame-3.1.tar.gz

---
+++ packages/pyeasygame/pyeasygame-3.1.tar.gz/pyeasygame-3.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='pyeasygame',
+   version='3.1',
+   description='A useful game engine for beginners',
+   author='Morris El Helou',
+   author_email='morriselhelou816@gmail.com',
+   packages=['pyeasygame'], 
+   install_requires=['pygame','keyboard'], #external packages as dependencies
+)
