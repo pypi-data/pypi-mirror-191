d0abb2cee77e414aadca103bdcb63c655e3a7ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:54 +0000

    Add package authomize-rest-api-client/2.10.0/authomize-rest-api-client-2.10.0.tar.gz

---
+++ packages/authomize-rest-api-client/authomize-rest-api-client-2.10.0.tar.gz/authomize-rest-api-client-2.10.0/authomize/rest_api_client/__init__.py
@@ -0,0 +1,20 @@
+from authomize.rest_api_client.client import Client
+from authomize.rest_api_client.generated.schemas import (
+    AccessDescription,
+    AccessTypes,
+    AssetDescription,
+    AssetsInheritance,
+    AssetTypes,
+    IdentitiesInheritance,
+    IdentityDescription,
+    IdentityTypes,
+    ItemsBundleSchema,
+    ServiceDescription,
+    UserStatus,
+)
+
+__all__ = [
+    'Client', 'ItemsBundleSchema', 'IdentityDescription', 'AssetDescription',
+    'IdentitiesInheritance', 'AssetsInheritance', 'AccessDescription',
+    'AccessTypes', 'UserStatus', 'IdentityTypes', 'AssetTypes',
+]
