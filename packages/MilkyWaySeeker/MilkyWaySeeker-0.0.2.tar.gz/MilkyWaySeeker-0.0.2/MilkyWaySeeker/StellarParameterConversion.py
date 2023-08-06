39a7926bab05cbfd18f9ebf1ab575ad8cba84d72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:28 +0000

    Add package MilkyWaySeeker/0.0.2/MilkyWaySeeker-0.0.2.tar.gz

---
+++ packages/MilkyWaySeeker/MilkyWaySeeker-0.0.2.tar.gz/MilkyWaySeeker-0.0.2/MilkyWaySeeker/StellarParameterConversion.py
@@ -0,0 +1,4 @@
+"""
+This file includes the functions for stellar parameter conversion, including the atmospher calculation,
+position and velocity conversion
+"""
---
+++ packages/MilkyWaySeeker/MilkyWaySeeker-0.0.2.tar.gz/MilkyWaySeeker-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MilkyWaySeeker',
+    version = '0.0.2',
+    description = 'A tool of htian for studying the Milky Way',
+    author = 'htian_astro',
+    author_email = 'htian_astro@163.com',
+    packages = ['MilkyWaySeeker'],
+    include_package_data=True
+)
