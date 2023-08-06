5ba510d8354c125dc80a8467a76cd684b4b7a74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:52 +0000

    Add package python-code-parse/0.0.11/python_code_parse-0.0.11-py2.py3-none-any.whl

---
+++ packages/python-code-parse/python_code_parse-0.0.11-py2.py3-none-any.whl/python_code_parse/models/function_arg.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class FunctionArg:
+    """A dataclass to hold information about a function argument."""
+
+    name: str
+    annotation: str = None
+    default: str = None
