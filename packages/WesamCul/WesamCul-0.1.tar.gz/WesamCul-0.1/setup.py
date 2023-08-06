c25414a38c12efa2424ff1f23a5571bb5a1a826d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:39 +0000

    Add package WesamCul/0.1/WesamCul-0.1.tar.gz

---
+++ packages/WesamCul/WesamCul-0.1.tar.gz/WesamCul-0.1/setup.py
@@ -0,0 +1,13 @@
+import  setuptools
+setuptools.setup(
+    name='WesamCul',
+    version='0.1',
+    author='Wesam-Saeed',
+    description='simple library in python for test',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ]
+
+)
