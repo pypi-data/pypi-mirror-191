4dec6322db06d33acfc8b4f1e3b3d9f6d422c5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:47 +0000

    Add package martin-macak-calculator-test/0.0.1/martin_macak_calculator_test-0.0.1-py3-none-any.whl

---
+++ packages/martin-macak-calculator-test/martin_macak_calculator_test-0.0.1-py3-none-any.whl/my_turing_calc/calc.py
@@ -0,0 +1,20 @@
+calc_memory = 0
+
+def add(calc_memory,x):
+    print(calc_memory+x)
+
+def substract(calc_memory,x):
+    print(calc_memory-x)
+    x
+def multiply(calc_memory,x):
+    print(calc_memory*x)
+
+def divide(calc_memory,x):
+    print(calc_memory/x)
+
+def nroot(calc_memory,x):
+    print(calc_memory**(1/x))
+
+def clear(calc_memory):
+    calc_memory == 0
+
