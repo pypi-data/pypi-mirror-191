431a3b1ffdfb6fb599a6b4dc6001f2dca8b1d047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:42 +0000

    Add package jupyter-manager/0.0.2/jupyter_manager-0.0.2.tar.gz

---
+++ packages/jupyter-manager/jupyter_manager-0.0.2.tar.gz/jupyter_manager-0.0.2/README.md
@@ -0,0 +1,12 @@
+[![Datalayer](https://assets.datalayer.design/datalayer-25.svg)](https://datalayer.io)
+
+# Jupyter Manager
+
+```bash
+pip install -e .[test]
+jupyter labextension develop . --overwrite
+jupyter labextension list
+jupyter server extension list
+# open http://localhost:8686/api/jupyter/lab?token=60c1661cc408f978c309d04157af55c9588ff9557c9380e4fb50785750703da6
+yarn jupyterlab
+```
---
+++ packages/jupyter-manager/jupyter_manager-0.0.2.tar.gz/jupyter_manager-0.0.2/src/index.ts
@@ -0,0 +1,82 @@
+import { JupyterFrontEnd, JupyterFrontEndPlugin } from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+import { MainAreaWidget, ICommandPalette } from '@jupyterlab/apputils';
+
+import { ILauncher } from '@jupyterlab/launcher';
+
+import { reactIcon } from '@jupyterlab/ui-components';
+
+import { requestAPI } from './handler';
+
+import { DatalayerWidget } from './widget';
+
+import '../style/index.css';
+
+/**
+ * The command IDs used by the react-widget plugin.
+ */
+namespace CommandIDs {
+  export const create = 'create-jupyter-manager-widget';
+}
+
+/**
+ * Initialization data for the @datalayer/jupyter-manager extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: '@datalayer/jupyter-manager:plugin',
+  autoStart: true,
+  requires: [ICommandPalette],
+  optional: [ISettingRegistry, ILauncher],
+  activate: (
+    app: JupyterFrontEnd,
+    palette: ICommandPalette,
+    settingRegistry: ISettingRegistry | null,
+    launcher: ILauncher
+  ) => {
+    const { commands } = app;
+    const command = CommandIDs.create;
+    commands.addCommand(command, {
+      caption: 'Show Jupyter Manager',
+      label: 'Jupyter Manager',
+      icon: (args: any) => reactIcon,
+      execute: () => {
+        const content = new DatalayerWidget();
+        const widget = new MainAreaWidget<DatalayerWidget>({ content });
+        widget.title.label = 'Jupyter Manager';
+        widget.title.icon = reactIcon;
+        app.shell.add(widget, 'main');
+      }
+    });
+    const category = 'Jupyter Manager';
+    palette.addItem({ command, category, args: { origin: 'from palette' } });
+    if (launcher) {
+      launcher.add({
+        command
+      });
+    }
+    console.log('JupyterLab extension @datalayer/jupyter-manager is activated!');
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log('@datalayer/jupyter-manager settings loaded:', settings.composite);
+        })
+        .catch(reason => {
+          console.error('Failed to load settings for @datalayer/jupyter-manager.', reason);
+        });
+    }
+    requestAPI<any>('get_example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The datalayer server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default plugin;
