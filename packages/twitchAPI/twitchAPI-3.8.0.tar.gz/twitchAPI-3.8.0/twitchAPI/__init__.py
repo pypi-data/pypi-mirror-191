40b4ce7d3bada2b52fba0d755fd461236dc9fc13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:42 +0000

    Add package twitchAPI/3.8.0/twitchAPI-3.8.0.tar.gz

---
+++ packages/twitchAPI/twitchAPI-3.8.0.tar.gz/twitchAPI-3.8.0/twitchAPI/__init__.py
@@ -0,0 +1,9 @@
+from .twitch import Twitch
+from .eventsub import EventSub
+from .pubsub import PubSub
+from .oauth import UserAuthenticator
+from .chat import Chat
+
+VERSION = (3, 8, 0)
+
+__version__ = '3.8.0'
