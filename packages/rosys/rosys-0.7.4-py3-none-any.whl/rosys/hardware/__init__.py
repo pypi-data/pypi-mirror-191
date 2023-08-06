96eb3acb6b0b78afa3a7d7c7dca022c8d6a8e8d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:12 +0000

    Add package rosys/0.7.4/rosys-0.7.4-py3-none-any.whl

---
+++ packages/rosys/rosys-0.7.4-py3-none-any.whl/rosys/hardware/__init__.py
@@ -0,0 +1,5 @@
+from .communication import Communication, SerialCommunication, WebCommunication
+from .robot_brain import RobotBrain
+from .wheels import Wheels
+from .wheels_hardware import WheelsHardware
+from .wheels_simulation import WheelsSimulation
---
+++ packages/rosys/rosys-0.7.4-py3-none-any.whl/rosys/hardware/wheels.py
@@ -0,0 +1,28 @@
+import abc
+import logging
+
+from .. import rosys
+from ..event import Event
+
+
+class Wheels(abc.ABC):
+    '''The wheels module is a simple example for a representation of real or simulated robot hardware.
+
+    Wheels can be moved using the `drive` methods and provide measured velocities as an event.
+    '''
+
+    def __init__(self) -> None:
+        self.VELOCITY_MEASURED = Event()
+        '''new velocity measurements are available for processing (argument: list of velocities)'''
+
+        self.log = logging.getLogger(__name__)
+
+        rosys.on_shutdown(self.stop)
+
+    @abc.abstractmethod
+    async def drive(self, linear: float, angular: float) -> None:
+        pass
+
+    @abc.abstractmethod
+    async def stop(self) -> None:
+        pass
