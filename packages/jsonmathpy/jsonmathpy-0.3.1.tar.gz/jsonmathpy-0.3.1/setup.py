95a658844428f91bea3881c5005892af1b7e1ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:34 +0000

    Add package jsonmathpy/0.3.1/jsonmathpy-0.3.1.tar.gz

---
+++ packages/jsonmathpy/jsonmathpy-0.3.1.tar.gz/jsonmathpy-0.3.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jsonmathpy',
+    version='0.3.1',
+    packages=find_packages(include=["jsonmathpy"]),
+    description='A package used to parse strings into math json objects.',
+    author='Ashley Cottrell',
+    author_email='cottrellashley@gmail.com',
+    install_requires=['regex', 'more_itertools', 'numpy', 'sympy']
+)
