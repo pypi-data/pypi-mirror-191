e430898ba758c9e24001782d740ff71c4f3e6b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:56 +0000

    Add package mosyle/0.0.1/mosyle-0.0.1.tar.gz

---
+++ packages/mosyle/mosyle-0.0.1.tar.gz/mosyle-0.0.1/README.md
@@ -0,0 +1,2 @@
+# Mosyle Manager
+
---
+++ packages/mosyle/mosyle-0.0.1.tar.gz/mosyle-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name="mosyle",
+    version="0.0.1",
+    description="Mosyle Manager API",
+    # long_description=""
+    author="Nathan McGuire",
+    author_email="mcguiren@osageschools.org",
+    # maintainer="",
+    # maintainer_email="mcguiren@osageschools.org",
+    url="https://github.com/SchoolOfTheOsage/mosyle",
+    # download_url="",
+    packages=["mosyle"],
+    # py_modules="",
+    # scripts="",
+    # ext_modules="",
+    # classifiers=[],
+    # distclass="",
+    # script_name="",
+    # script_args="",
+    # options="",
+    # license="",
+    # platforms="",
+    # cmdclass="",
+    # data_files="",
+    # package_dir="",
+    install_requires=["requests"]
+    # keywords=
+)
