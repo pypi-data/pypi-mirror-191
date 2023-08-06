1c532e23c7582253a39022540080b1891c8e260b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:37 +0000

    Add package mds-cashbook-bookcategory/6.0.3/mds_cashbook_bookcategory-6.0.3.tar.gz

---
+++ packages/mds-cashbook-bookcategory/mds_cashbook_bookcategory-6.0.3.tar.gz/mds_cashbook_bookcategory-6.0.3/README.rst
@@ -0,0 +1,31 @@
+mds-cashbook-bookcategory
+=========================
+Tryton module to add categories for cashbooks.
+
+Install
+=======
+
+pip install mds-cashbook-bookcategory
+
+Requires
+========
+- Tryton 6.0
+
+Changes
+=======
+
+*6.0.3 - 05.02.2023*
+
+- updt: remove left/right fields
+
+*6.0.2 - 08.11.2022*
+
+- add: show linked cashbook on category-form
+
+*6.0.1 - 08.11.2022*
+
+- works
+
+*6.0.0 - 07.11.2022*
+
+- init
---
+++ packages/mds-cashbook-bookcategory/mds_cashbook_bookcategory-6.0.3.tar.gz/mds_cashbook_bookcategory-6.0.3/book.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
+from trytond.model import ModelSQL, fields
+from trytond.pool import PoolMeta
+
+
+class Cashbook(metaclass=PoolMeta):
+    __name__ = 'cashbook.book'
+
+    categories = fields.Many2Many(string='Categories',
+        relation_name='cashbook.bookcategory-rel',
+        origin='cashbook', target='category')
+
+# end Cashbook
+
+
+class CategoryCashbookRel(ModelSQL):
+    'Category Cashbook Relation'
+    __name__ = 'cashbook.bookcategory-rel'
+
+    category = fields.Many2One(string='Category', required=True, select=True,
+        model_name='cashbook.bookcategory', ondelete='CASCADE')
+    cashbook = fields.Many2One(string='Cashbook', required=True, select=True,
+        model_name='cashbook.book', ondelete='CASCADE')
+
+# end CategoryCashbookRel
