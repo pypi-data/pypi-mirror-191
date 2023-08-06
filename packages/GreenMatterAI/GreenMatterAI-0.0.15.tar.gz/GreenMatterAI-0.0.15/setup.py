5e69c17254cd32785a5f17c95b9dc7b41ef482c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:47 +0000

    Add package GreenMatterAI/0.0.15/GreenMatterAI-0.0.15.tar.gz

---
+++ packages/GreenMatterAI/GreenMatterAI-0.0.15.tar.gz/GreenMatterAI-0.0.15/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="GreenMatterAI",
+    version="0.0.15",
+    author="GreenMatterAI",
+    description="GreenMatterAI's package",
+    #py_modules=["GreenMatterAI"],
+    py_modules=setuptools.find_packages(),
+    install_requires=[
+        "requests",
+        "shortuuid",
+        "aws_requests_auth",
+        "boto3",
+        "pathlib",
+        "datetime"
+    ]
+)
