f1f68b1d8eedfce8726373b3b7c3ffeee01d2ed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:22 +0000

    Add package dwh-oppfolging/0.0.26/dwh_oppfolging-0.0.26.tar.gz

---
+++ packages/dwh-oppfolging/dwh_oppfolging-0.0.26.tar.gz/dwh_oppfolging-0.0.26/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dwh_oppfolging',
+ 'dwh_oppfolging.apis',
+ 'dwh_oppfolging.db',
+ 'dwh_oppfolging.transforms']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dbt-oracle>=1.3.1,<2.0.0',
+ 'google-cloud-secret-manager>=2.12.6,<3.0.0',
+ 'ijson>=3.1.4,<4.0.0',
+ 'oracledb>=1.2.1,<2.0.0',
+ 'pendulum>=2.1.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'dwh-oppfolging',
+    'version': '0.0.26',
+    'description': 'Oppfolging python package for DWH ETL',
+    'long_description': 'None',
+    'author': 'Team Oppfolging',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
