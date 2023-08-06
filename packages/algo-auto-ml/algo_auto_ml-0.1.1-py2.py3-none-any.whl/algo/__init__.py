4a2280b5cc7c63aa72fc0e4716eab96bed7d2641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:12 +0000

    Add package algo-auto-ml/0.1.1/algo_auto_ml-0.1.1-py2.py3-none-any.whl

---
+++ packages/algo-auto-ml/algo_auto_ml-0.1.1-py2.py3-none-any.whl/algo/__init__.py
@@ -0,0 +1,17 @@
+"""
+Algo library
+
+AutoML library for binary classification and regression tasks
+Licence: MIT
+"""
+
+__author__ = "Pavel Lyulchak"
+__version__ = "0.1.1"
+
+
+__all__ = [
+    "automl",
+    "features",
+    "ml",
+    "task"
+]
