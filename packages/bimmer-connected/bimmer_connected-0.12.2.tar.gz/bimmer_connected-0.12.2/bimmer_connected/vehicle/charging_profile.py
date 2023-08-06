bc9fe894a37d13bd5169bd16a2a7a552d29b2c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:49 +0000

    Add package bimmer-connected/0.12.2/bimmer_connected-0.12.2.tar.gz

---
+++ packages/bimmer-connected/bimmer_connected-0.12.2.tar.gz/bimmer_connected-0.12.2/bimmer_connected/vehicle/charging_profile.py
@@ -0,0 +1,128 @@
+"""Models the charging profiles of a vehicle."""
+
+import datetime
+import logging
+from dataclasses import dataclass
+from typing import Any, Dict, List, Optional
+
+from bimmer_connected.const import ATTR_STATE
+from bimmer_connected.models import StrEnum, VehicleDataBase
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class ChargingMode(StrEnum):
+    """Charging mode of electric vehicle."""
+
+    IMMEDIATE_CHARGING = "IMMEDIATE_CHARGING"
+    DELAYED_CHARGING = "DELAYED_CHARGING"
+    UNKNOWN = "UNKNOWN"
+
+
+class ChargingPreferences(StrEnum):
+    """Charging preferences of electric vehicle."""
+
+    NO_PRESELECTION = "NO_PRESELECTION"
+    CHARGING_WINDOW = "CHARGING_WINDOW"
+    UNKNOWN = "UNKNOWN"
+
+
+class TimerTypes(StrEnum):
+    """Different timer types."""
+
+    TWO_WEEKS = "TWO_WEEKS_TIMER"
+    ONE_WEEK = "WEEKLY_PLANNER"
+    OVERRIDE_TIMER = "OVERRIDE_TIMER"
+    TWO_TIMES_TIMER = "TWO_TIMES_TIMER"
+    UNKNOWN = "UNKNOWN"
+
+
+class ChargingWindow:
+    """A charging window."""
+
+    def __init__(self, window_dict: dict):
+        self._window_dict = window_dict
+
+    @property
+    def start_time(self) -> datetime.time:
+        """Start of the charging window."""
+        return datetime.time(int(self._window_dict["start"]["hour"]), int(self._window_dict["start"]["minute"]))
+
+    @property
+    def end_time(self) -> datetime.time:
+        """End of the charging window."""
+        return datetime.time(int(self._window_dict["end"]["hour"]), int(self._window_dict["end"]["minute"]))
+
+
+class DepartureTimer:
+    """A departure timer."""
+
+    def __init__(self, timer_dict: dict):
+        self._timer_dict: Dict = timer_dict
+
+    @property
+    def timer_id(self) -> Optional[int]:
+        """ID of this timer."""
+        return self._timer_dict.get("id")
+
+    @property
+    def start_time(self) -> Optional[datetime.time]:
+        """Deperture time for this timer."""
+        if "timeStamp" not in self._timer_dict:
+            return None
+        return datetime.time(int(self._timer_dict["timeStamp"]["hour"]), int(self._timer_dict["timeStamp"]["minute"]))
+
+    @property
+    def action(self) -> Optional[str]:
+        """What does the timer do."""
+        return self._timer_dict.get("action")
+
+    @property
+    def weekdays(self) -> List[str]:
+        """Active weekdays for this timer."""
+        return self._timer_dict.get("timerWeekDays")  # type: ignore[return-value]
+
+
+@dataclass
+class ChargingProfile(VehicleDataBase):
+    """Models the charging profile of a vehicle."""
+
+    is_pre_entry_climatization_enabled: bool
+    """Get status of pre-entry climatization."""
+
+    timer_type: TimerTypes
+    """Returns the current timer plan type."""
+
+    departure_times: List[DepartureTimer]
+    """List of timers."""
+
+    preferred_charging_window: ChargingWindow
+    """Returns the preferred charging window."""
+
+    charging_preferences: ChargingPreferences
+    """Returns the preferred charging preferences."""
+
+    charging_mode: ChargingMode
+    """Returns the preferred charging mode."""
+
+    ac_current_limit: Optional[int] = None
+    """Returns the ac current limit."""
+
+    @classmethod
+    def _parse_vehicle_data(cls, vehicle_data: Dict) -> Dict:
+        """Parse charging data."""
+        retval: Dict[str, Any] = {}
+
+        if ATTR_STATE in vehicle_data and "chargingProfile" in vehicle_data[ATTR_STATE]:
+            charging_profile = vehicle_data[ATTR_STATE]["chargingProfile"]
+
+            retval["is_pre_entry_climatization_enabled"] = bool(charging_profile.get("climatisationOn", False))
+            retval["departure_times"] = [DepartureTimer(t) for t in charging_profile.get("departureTimes", [])]
+            retval["preferred_charging_window"] = ChargingWindow(charging_profile.get("reductionOfChargeCurrent", {}))
+            retval["timer_type"] = TimerTypes(charging_profile.get("chargingControlType", "UNKNOWN"))
+            retval["charging_preferences"] = ChargingPreferences(charging_profile.get("chargingPreference", "UNKNOWN"))
+            retval["charging_mode"] = ChargingMode(charging_profile.get("chargingMode", "UNKNOWN"))
+            if "acCurrentLimit" in charging_profile["chargingSettings"]:
+                retval["ac_current_limit"] = charging_profile["chargingSettings"]["acCurrentLimit"]
+
+        return retval
---
+++ packages/bimmer-connected/bimmer_connected-0.12.2.tar.gz/bimmer_connected-0.12.2/test/test_remote_services.py
@@ -0,0 +1,264 @@
+"""Test for remote_services."""
+from collections import defaultdict
+from copy import deepcopy
+from pathlib import Path
+from typing import Dict, List
+
+try:
+    from unittest import mock
+
+    if not hasattr(mock, "AsyncMock"):
+        # AsyncMock was only introduced with Python3.8, so we have to use the backported module
+        raise ImportError()
+except ImportError:
+    import mock  # type: ignore[import,no-redef]  # noqa: UP026
+
+from uuid import uuid4
+
+import httpx
+import pytest
+import time_machine
+
+from bimmer_connected.models import PointOfInterest
+from bimmer_connected.vehicle import remote_services
+from bimmer_connected.vehicle.remote_services import ExecutionState, RemoteServiceStatus
+
+from . import RESPONSE_DIR, VIN_G23, load_response
+from .test_account import account_mock, get_mocked_account
+
+_RESPONSE_INITIATED = RESPONSE_DIR / "remote_services" / "eadrax_service_initiated.json"
+_RESPONSE_PENDING = RESPONSE_DIR / "remote_services" / "eadrax_service_pending.json"
+_RESPONSE_DELIVERED = RESPONSE_DIR / "remote_services" / "eadrax_service_delivered.json"
+_RESPONSE_EXECUTED = RESPONSE_DIR / "remote_services" / "eadrax_service_executed.json"
+_RESPONSE_ERROR = RESPONSE_DIR / "remote_services" / "eadrax_service_error.json"
+_RESPONSE_EVENTPOSITION = RESPONSE_DIR / "remote_services" / "eadrax_service_eventposition.json"
+
+
+POI_DATA = {
+    "lat": 37.4028943,
+    "lon": -121.9700289,
+    "name": "49ers",
+    "street": "4949 Marie P DeBartolo Way",
+    "city": "Santa Clara",
+    "postal_code": "CA 95054",
+    "country": "United States",
+}
+
+STATUS_RESPONSE_ORDER = [_RESPONSE_PENDING, _RESPONSE_DELIVERED, _RESPONSE_EXECUTED]
+STATUS_RESPONSE_DICT: Dict[str, List[Path]] = defaultdict(lambda: deepcopy(STATUS_RESPONSE_ORDER))
+
+
+def service_trigger_sideeffect(request: httpx.Request) -> httpx.Response:
+    """Return specific eventId for each remote function."""
+    json_data = load_response(_RESPONSE_INITIATED)
+    json_data["eventId"] = str(uuid4())
+    return httpx.Response(200, json=json_data)
+
+
+def service_status_sideeffect(request: httpx.Request) -> httpx.Response:
+    """Return all 3 eventStatus responses per function."""
+    response_data = STATUS_RESPONSE_DICT[request.url.params["eventId"]].pop(0)
+    return httpx.Response(200, json=load_response(response_data))
+
+
+def remote_services_mock():
+    """Return mocked adapter for auth."""
+    router = account_mock()
+
+    router.post(path__regex=r"/eadrax-vrccs/v3/presentation/remote-commands/.+/.+$").mock(
+        side_effect=service_trigger_sideeffect
+    )
+    router.post("/eadrax-vrccs/v3/presentation/remote-commands/eventStatus", params={"eventId": mock.ANY}).mock(
+        side_effect=service_status_sideeffect
+    )
+
+    router.post("/eadrax-dcs/v1/send-to-car/send-to-car").respond(201)
+    router.post("/eadrax-vrccs/v3/presentation/remote-commands/eventPosition", params={"eventId": mock.ANY}).respond(
+        200,
+        json=load_response(_RESPONSE_EVENTPOSITION),
+    )
+    return router
+
+
+def test_states():
+    """Test parsing the different response types."""
+    rss = RemoteServiceStatus(load_response(_RESPONSE_PENDING))
+    assert ExecutionState.PENDING == rss.state
+
+    rss = RemoteServiceStatus(load_response(_RESPONSE_DELIVERED))
+    assert ExecutionState.DELIVERED == rss.state
+
+    rss = RemoteServiceStatus(load_response(_RESPONSE_EXECUTED))
+    assert ExecutionState.EXECUTED == rss.state
+
+
+@remote_services_mock()
+@pytest.mark.asyncio
+@pytest.mark.filterwarnings("ignore:coroutine 'AsyncMockMixin._execute_mock_call' was never awaited:RuntimeWarning")
+async def test_trigger_remote_services():
+    """Test executing a remote light flash."""
+    remote_services._POLLING_CYCLE = 0
+
+    services = [
+        ("LIGHT_FLASH", "trigger_remote_light_flash", False),
+        ("DOOR_LOCK", "trigger_remote_door_lock", True),
+        ("DOOR_UNLOCK", "trigger_remote_door_unlock", True),
+        ("CLIMATE_NOW", "trigger_remote_air_conditioning", True),
+        ("CLIMATE_STOP", "trigger_remote_air_conditioning_stop", True),
+        ("VEHICLE_FINDER", "trigger_remote_vehicle_finder", False),
+        ("HORN_BLOW", "trigger_remote_horn", False),
+        ("SEND_POI", "trigger_send_poi", False),
+        ("CHARGE_NOW", "trigger_charge_now", True),
+    ]
+
+    account = await get_mocked_account()
+    vehicle = account.get_vehicle(VIN_G23)
+
+    for service, call, triggers_update in services:
+        with mock.patch(
+            "bimmer_connected.account.MyBMWAccount.get_vehicles", new_callable=mock.AsyncMock
+        ) as mock_listener:
+            mock_listener.reset_mock()
+
+            if service == "SEND_POI":
+                response = await getattr(vehicle.remote_services, call)(POI_DATA)
+            else:
+                response = await getattr(vehicle.remote_services, call)()
+                assert ExecutionState.EXECUTED == response.state
+
+                if triggers_update:
+                    mock_listener.assert_called_once_with()
+                else:
+                    mock_listener.assert_not_called()
+
+
+@pytest.mark.asyncio
+async def test_get_remote_service_status():
+    """Test get_remove_service_status method."""
+
+    remote_services._POLLING_CYCLE = 0
+
+    account = await get_mocked_account()
+    vehicle = account.get_vehicle(VIN_G23)
+
+    with remote_services_mock() as mock_api:
+        mock_api.post("/eadrax-vrccs/v3/presentation/remote-commands/eventStatus", params={"eventId": mock.ANY}).mock(
+            side_effect=[
+                httpx.Response(500),
+                httpx.Response(200, text="You can't parse this..."),
+                httpx.Response(200, json=load_response(_RESPONSE_ERROR)),
+            ],
+        )
+
+        with pytest.raises(httpx.HTTPStatusError):
+            await vehicle.remote_services._block_until_done(uuid4())
+        with pytest.raises(ValueError):
+            await vehicle.remote_services._block_until_done(uuid4())
+        with pytest.raises(Exception):
+            await vehicle.remote_services._block_until_done(uuid4())
+
+
+@remote_services_mock()
+@pytest.mark.asyncio
+async def test_get_remote_position():
+    """Test getting position from remote service."""
+    remote_services._POLLING_CYCLE = 0
+
+    account = await get_mocked_account()
+    account.set_observer_position(1.0, 0.0)
+    vehicle = account.get_vehicle(VIN_G23)
+    status = vehicle.status
+
+    # Check original position
+    assert (48.177334, 11.556274) == status.gps_position
+    assert 180 == status.gps_heading
+
+    # Check updated position
+    await vehicle.remote_services.trigger_remote_vehicle_finder()
+    assert (123.456, 34.5678) == status.gps_position
+    assert 121 == status.gps_heading
+
+    # Position should still be from vehicle finder after status update
+    await account.get_vehicles()
+    assert (123.456, 34.5678) == status.gps_position
+    assert 121 == status.gps_heading
+
+
+@remote_services_mock()
+@pytest.mark.asyncio
+async def test_get_remote_position_fail_without_observer(caplog):
+    """Test getting position from remote service."""
+    remote_services._POLLING_CYCLE = 0
+
+    account = await get_mocked_account()
+    vehicle = account.get_vehicle(VIN_G23)
+
+    await vehicle.remote_services.trigger_remote_vehicle_finder()
+    errors = [
+        r
+        for r in caplog.records
+        if r.levelname == "ERROR"
+        and "Unknown position: Set observer position to retrieve vehicle coordinates" in r.message
+    ]
+    assert len(errors) == 1
+
+
+@remote_services_mock()
+@pytest.mark.asyncio
+async def test_fail_with_timeout():
+    """Test failing after timeout was reached."""
+    remote_services._POLLING_CYCLE = 1
+    remote_services._POLLING_TIMEOUT = 2
+
+    account = await get_mocked_account()
+    vehicle = account.get_vehicle(VIN_G23)
+
+    with pytest.raises(TimeoutError):
+        await vehicle.remote_services.trigger_remote_light_flash()
+
+
+@time_machine.travel("2020-01-01", tick=False)
+@remote_services_mock()
+@pytest.mark.asyncio
+async def test_get_remote_position_too_old():
+    """Test remote service position being ignored as vehicle status is newer."""
+    remote_services._POLLING_CYCLE = 0
+
+    account = await get_mocked_account()
+    vehicle = account.get_vehicle(VIN_G23)
+    status = vehicle.status
+
+    await vehicle.remote_services.trigger_remote_vehicle_finder()
+
+    assert (48.177334, 11.556274) == status.gps_position
+    assert 180 == status.gps_heading
+
+
+@remote_services_mock()
+@pytest.mark.asyncio
+async def test_poi():
+    """Test get_remove_service_status method."""
+    remote_services._POLLING_CYCLE = 0
+
+    account = await get_mocked_account()
+    vehicle = account.get_vehicle(VIN_G23)
+
+    with pytest.raises(TypeError):
+        await vehicle.remote_services.trigger_send_poi({"lat": 12.34})
+
+
+def test_poi_parsing():
+    """Test correct parsing of PointOfInterest."""
+
+    poi_data = PointOfInterest(**POI_DATA)
+
+    # Check parsing of attributes required by API
+    assert poi_data.coordinates.latitude == POI_DATA["lat"]
+    assert poi_data.coordinates.longitude == POI_DATA["lon"]
+    assert poi_data.name == POI_DATA["name"]
+
+    # Check that default attributes
+    poi_data = PointOfInterest(lat=POI_DATA["lat"], lon=POI_DATA["lon"])
+    assert poi_data.coordinates.latitude == POI_DATA["lat"]
+    assert poi_data.coordinates.longitude == POI_DATA["lon"]
+    assert poi_data.name == "Sent with ♥ by bimmer_connected"
