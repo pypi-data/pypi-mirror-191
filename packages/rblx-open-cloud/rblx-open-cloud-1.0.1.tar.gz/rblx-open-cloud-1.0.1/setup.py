1b256979d8783c349cef610c80d5e8cec07dc253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:41 +0000

    Add package rblx-open-cloud/1.0.1/rblx-open-cloud-1.0.1.tar.gz

---
+++ packages/rblx-open-cloud/rblx-open-cloud-1.0.1.tar.gz/rblx-open-cloud-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name='rblx-open-cloud',
+    description='API wrapper for Roblox Open Cloud',
+    version="1.0.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    python_requires='>=3.9',
+    author="TreeBen77",
+    packages=[
+        'rblxopencloud'
+    ],
+    url='https://github.com/TreeBen77/rblx-open-cloud',
+    keywords='roblox, open-cloud, data-store, place-publishing, mesageing-service',
+    install_requires=[
+        'requests'
+    ]
+)
