33b49746e420cf0661e83ba0d35242a8cf1527c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:36 +0000

    Add package redbrick-sdk/2.9.1/redbrick-sdk-2.9.1.tar.gz

---
+++ packages/redbrick-sdk/redbrick-sdk-2.9.1.tar.gz/redbrick-sdk-2.9.1/redbrick/repo/shards.py
@@ -0,0 +1,227 @@
+"""Partial queries to prevent duplication."""
+
+TAXONOMY_SHARD = """
+orgId
+name
+version
+createdAt
+categories {
+    name
+    children {
+        name
+        classId
+        disabled
+        children {
+            name
+            classId
+            disabled
+            children {
+                name
+                classId
+                disabled
+                children {
+                    name
+                    classId
+                    disabled
+                    children {
+                        name
+                        classId
+                        disabled
+                        children {
+                            name
+                            classId
+                            disabled
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+attributes {
+    name
+    attrType
+    whitelist
+    disabled
+}
+taskCategories {
+    name
+    children {
+        name
+        classId
+        disabled
+        children {
+            name
+            classId
+            disabled
+            children {
+                name
+                classId
+                disabled
+                children {
+                    name
+                    classId
+                    disabled
+                    children {
+                        name
+                        classId
+                        disabled
+                        children {
+                            name
+                            classId
+                            disabled
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+taskAttributes {
+    name
+    attrType
+    whitelist
+    disabled
+}
+colorMap {
+    name
+    color
+    classid
+    trail
+    taskcategory
+}
+archived
+isNew
+taxId
+studyClassify {
+    name
+    attrType
+    attrId
+    options {
+        name
+        optionId
+        color
+        archived
+    }
+    archived
+}
+seriesClassify {
+    name
+    attrType
+    attrId
+    options {
+        name
+        optionId
+        color
+        archived
+    }
+    archived
+}
+instanceClassify {
+    name
+    attrType
+    attrId
+    options {
+        name
+        optionId
+        color
+        archived
+    }
+    archived
+}
+objectTypes {
+    category
+    classId
+    labelType
+    attributes {
+        name
+        attrType
+        attrId
+        options {
+            name
+            optionId
+            color
+            archived
+        }
+        archived
+    }
+    color
+    archived
+}
+"""
+
+TASK_DATA_SHARD = """
+    createdAt
+    createdByEmail
+    labelsData(interpolate: true)
+    labelsStorage {
+        storageId
+    }
+    labelsMap(presigned: false) {
+        imageIndex
+        labelName
+    }
+"""
+
+TASK_SHARD = f"""
+    ... on LabelingTask {{
+        state
+        assignedTo {{
+            userId
+            email
+        }}
+        taskData {{
+            {TASK_DATA_SHARD}
+        }}
+        subTasks {{
+            state
+            assignedTo {{
+                userId
+                email
+            }}
+            taskData {{
+                {TASK_DATA_SHARD}
+            }}
+        }}
+        overallConsensusScore
+        consensusInfo {{
+            user {{
+                userId
+                email
+            }}
+            taskData {{
+                {TASK_DATA_SHARD}
+            }}
+            scores {{
+                user {{
+                    userId
+                    email
+                }}
+                score
+            }}
+        }}
+    }}
+"""
+
+DATAPOINT_SHARD: str = """
+    dataPoint {{
+        name
+        items(presigned: false)
+        {}
+        createdAt
+        createdByEntity {{
+            email
+        }}
+        metaData
+        seriesInfo {{
+            name
+            itemsIndices
+            dataType
+            numFrames
+            dimensions
+        }}
+        storageMethod {{
+            storageId
+        }}
+    }}
+"""
