7b0c2f5bc1e089948e7ada3b02fb1d428dedaa6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:13 +0000

    Add package clip-util/0.1.13/clip-util-0.1.13.tar.gz

---
+++ packages/clip-util/clip-util-0.1.13.tar.gz/clip-util-0.1.13/setup.py
@@ -0,0 +1,36 @@
+import setuptools  # type: ignore
+
+
+with open("README.md", "r") as fin:
+    long_description = fin.read()
+
+
+setuptools.setup(
+    name="clip-util",
+    version="0.1.13",
+    author="Kyle L. Davis",
+    author_email="AceofSpades5757.github@gmail.com",
+    url="https://github.com/AceofSpades5757/clip-util",
+    project_urls={
+        "Documentation": "https://clip-util.readthedocs.io/en/latest/",
+        "Author": "https://github.com/AceofSpades5757",
+    },
+    description="Clipboard utilities for use with Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    python_requires=">=3.9",
+    packages=setuptools.find_packages("src"),
+    package_dir={
+        "": "src",
+        "clipboard": "src/clipboard",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
