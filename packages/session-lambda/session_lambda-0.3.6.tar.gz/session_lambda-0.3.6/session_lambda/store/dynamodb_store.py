13ca1e707f6a348b3441dc2769d83976c0301c3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:23 +0000

    Add package session-lambda/0.3.6/session_lambda-0.3.6.tar.gz

---
+++ packages/session-lambda/session_lambda-0.3.6.tar.gz/session_lambda-0.3.6/session_lambda/store/dynamodb_store.py
@@ -0,0 +1,75 @@
+import json
+import time
+from typing import Any, Tuple
+import boto3, botocore
+from .base import StoreBase
+
+class DynamoDBStore(StoreBase):
+    """
+    JSON File-based store for testing purposes.
+    """
+    _id: str = 'key'
+    _value: str = 'value'
+    _ttl: str = 'ttl'
+    
+    def __init__(self, store) -> None:
+        super().__init__(store)
+    
+    def _validate_store(self):
+        if not isinstance(self._store, str):
+            raise TypeError("store must be a string")
+        try:
+            table = self._table()
+        except Exception as e:
+            raise e
+        
+    def _table(self):
+        try:
+            table = boto3.resource('dynamodb').Table(self._store)
+            table.table_status
+        except botocore.exceptions.ClientError as error:
+            if error.response['Error']['Code'] == 'ResourceNotFoundException':
+                raise ValueError('Requested table not found')
+            else:
+                raise error
+        return table
+    
+    def get(self, key, return_item=False) -> Tuple[Any, bool]:
+        table = self._table()
+        response = table.get_item(
+            Key={DynamoDBStore._id: key},
+        )
+        item = response.get('Item')
+        exist = not (item is None)
+        if return_item:
+            return item, exist
+        return response.get('Item',{}).get(DynamoDBStore._value), exist
+    
+    def put(self, key, value, ttl=0):
+        table = self._table()
+        update_expression = 'ADD #counter :increment SET #value = :value, #created_at = if_not_exists(#created_at, :now)'
+        expression_attribute_names={
+                '#value': DynamoDBStore._value,
+                '#counter': 'access_counter',
+                '#created_at': 'created_at',
+            }
+        expression_attribute_values={
+                ':increment': 1,
+                ':value': value,
+                ':now': int(time.time()),
+            }
+        ttl_value = int(time.time())+ttl
+        
+        if ttl>0:
+            update_expression += ', #ttl = :ttl'
+            expression_attribute_names.update({'#ttl': DynamoDBStore._ttl})
+            expression_attribute_values.update({':ttl': ttl_value})
+            
+        response = table.update_item(
+            Key={DynamoDBStore._id: key},
+            ReturnValues='ALL_OLD',
+            UpdateExpression=update_expression,
+            ExpressionAttributeNames=expression_attribute_names,
+            ExpressionAttributeValues=expression_attribute_values
+            )
+        return response.get('Attributes')
