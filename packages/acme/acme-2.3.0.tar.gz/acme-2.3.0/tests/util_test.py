711a23b7845081f7a2bdabc930409854de4a1860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:54 +0000

    Add package acme/2.3.0/acme-2.3.0.tar.gz

---
+++ packages/acme/acme-2.3.0.tar.gz/acme-2.3.0/tests/util_test.py
@@ -0,0 +1,19 @@
+"""Tests for acme.util."""
+import sys
+import unittest
+
+import pytest
+
+
+class MapKeysTest(unittest.TestCase):
+    """Tests for acme.util.map_keys."""
+
+    def test_it(self):
+        from acme.util import map_keys
+        self.assertEqual({'a': 'b', 'c': 'd'},
+                         map_keys({'a': 'b', 'c': 'd'}, lambda key: key))
+        self.assertEqual({2: 2, 4: 4}, map_keys({1: 2, 3: 4}, lambda x: x + 1))
+
+
+if __name__ == '__main__':
+    sys.exit(pytest.main(sys.argv[1:] + [__file__]))  # pragma: no cover
