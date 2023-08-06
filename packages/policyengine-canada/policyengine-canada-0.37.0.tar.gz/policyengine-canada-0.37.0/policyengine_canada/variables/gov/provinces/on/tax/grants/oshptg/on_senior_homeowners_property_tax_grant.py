d082ed6269fcce185066239c24761ca4f82767d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:39 +0000

    Add package policyengine-canada/0.37.0/policyengine-canada-0.37.0.tar.gz

---
+++ packages/policyengine-canada/policyengine-canada-0.37.0.tar.gz/policyengine-canada-0.37.0/policyengine_canada/variables/gov/provinces/on/tax/grants/oshptg/on_senior_homeowners_property_tax_grant.py
@@ -0,0 +1,16 @@
+from policyengine_canada.model_api import *
+
+
+class on_senior_homeowners_property_tax_grant(Variable):
+    value_type = float
+    entity = Person
+    label = "Ontario senior homeowners property tax grant"
+    unit = CAD
+    definition_period = YEAR
+
+    def formula(person, period, parameters):
+        base = person("on_senior_homeowners_property_tax_grant_base", period)
+        reduction = person(
+            "on_senior_homeowners_property_tax_grant_reduction", period
+        )
+        return max_(0, base - reduction)
