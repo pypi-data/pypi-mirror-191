6dcba0713092427588c3708469565e2ddd836eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:55 +0000

    Add package slackoff/1.0/slackoff-1.0-py3-none-any.whl

---
+++ packages/slackoff/slackoff-1.0-py3-none-any.whl/slackoff/config.py
@@ -0,0 +1,36 @@
+import log
+from datafiles import datafile, field
+
+
+@datafile
+class Workspace:
+    name: str
+    active: bool = True
+    uses: int = 0
+
+
+@datafile("~/Library/Preferences/slackoff.yml")
+class Settings:
+    workspaces: list[Workspace] = field(default_factory=list)
+
+    def activate(self, name: str):
+        log.debug(f"Marking workspace active: {name}")
+        workspace = self._update(name, True)
+        workspace.uses += 1
+
+    def deactivate(self, name: str):
+        log.debug(f"Marking workspace inactive: {name}")
+        self._update(name, False)
+
+    def _update(self, name: str, active: bool):
+        for workspace in self.workspaces:
+            if workspace.name == name:
+                workspace.active = active
+                return workspace
+
+        workspace = Workspace(name, active)
+        self.workspaces.append(workspace)
+        return workspace
+
+
+settings = Settings()
