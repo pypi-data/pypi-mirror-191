35ab0ae2b2c50478de3ef115fa0be2de330736e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:51 +0000

    Add package np-probe-targets/0.0.8/np_probe_targets-0.0.8.tar.gz

---
+++ packages/np-probe-targets/np_probe_targets-0.0.8.tar.gz/np_probe_targets-0.0.8/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['np_probe_targets']
+
+package_data = \
+{'': ['*'], 'np_probe_targets': ['.ipynb_checkpoints/*']}
+
+setup_kwargs = {
+    'name': 'np-probe-targets',
+    'version': '0.0.8',
+    'description': '',
+    'long_description': None,
+    'author': 'bjhardcastle',
+    'author_email': 'ben.hardcastle@alleninstitute.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
