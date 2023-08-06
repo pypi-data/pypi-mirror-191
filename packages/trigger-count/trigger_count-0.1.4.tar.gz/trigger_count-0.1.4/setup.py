dd169c237853c4b365f070f7ae9ae7fecd8e6cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:36 +0000

    Add package trigger-count/0.1.4/trigger_count-0.1.4.tar.gz

---
+++ packages/trigger-count/trigger_count-0.1.4.tar.gz/trigger_count-0.1.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['trigger_count', 'trigger_count.daq', 'trigger_count.widefield_workaround']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['labjack-ljm>=1.21.0,<2.0.0', 'pandas>=1.5.3,<2.0.0', 'pyserial>=3.5,<4.0']
+
+setup_kwargs = {
+    'name': 'trigger-count',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': '',
+    'author': 'Mathis',
+    'author_email': 'mathis.bassler@protonmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
