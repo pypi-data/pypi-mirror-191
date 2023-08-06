60d3cce6ce4dbc1a4a827f7bb22aa274e3614202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:50 +0000

    Add package computation-graph/43/computation-graph-43.tar.gz

---
+++ packages/computation-graph/computation-graph-43.tar.gz/computation-graph-43/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    _LONG_DESCRIPTION = fh.read()
+
+
+setuptools.setup(
+    name="computation-graph",
+    python_requires=">=3",
+    version="43",
+    long_description=_LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_namespace_packages(),
+    install_requires=["gamla", "typeguard", "toposort", "immutables", "termcolor"],
+    extras_require={"test": ["pygraphviz", "pytest>=5.4.0"]},
+)
