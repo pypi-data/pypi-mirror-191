29ba73fd24f6fc0bd33655e7a0b38238c750a938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:56 +0000

    Add package clipboard-files/1.0.13/clipboard-files-1.0.13.tar.gz

---
+++ packages/clipboard-files/clipboard-files-1.0.13.tar.gz/clipboard-files-1.0.13/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='clipboard-files',
+    version='1.0.13',
+    description='A script for copying file contents to the clipboard. cf is the command',
+    author='Nadav Goldstein',
+    packages=["app"],
+    install_requires=[
+        'pyperclip'
+    ],
+    entry_points={
+        'console_scripts': [
+            'cf=app.main:main',
+        ],
+    },
+)
