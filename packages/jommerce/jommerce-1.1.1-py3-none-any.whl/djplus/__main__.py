c60f85ed71cc9cbe8e2448572c2b83dba3878d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:23 +0000

    Add package jommerce/1.1.1/jommerce-1.1.1-py3-none-any.whl

---
+++ packages/jommerce/jommerce-1.1.1-py3-none-any.whl/djplus/__main__.py
@@ -0,0 +1,24 @@
+from django.core.management.templates import TemplateCommand
+from django.core.management.utils import get_random_secret_key
+from pathlib import Path
+
+PROJECT_TEMPLATE_DIR = Path(__file__).parent / "project_template"
+
+
+def main():
+    TemplateCommand().handle(
+        "project",
+        name=input("project_name[config]: ").strip() or "config",
+        target=str(Path.cwd()),
+        template=str(PROJECT_TEMPLATE_DIR),
+        files=[".gitignore"],
+        extensions=[".py", ".py-tpl"],
+        exclude=[],
+        verbosity=0,
+
+        secret_key=get_random_secret_key(),
+    )
+
+
+if __name__ == "__main__":
+    main()
