48f6db104d01d7051729a9a1d17625f06add6436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:38 +0000

    Add package rheofit/0.1.1/rheofit-0.1.1-py2.py3-none-any.whl

---
+++ packages/rheofit/rheofit-0.1.1-py2.py3-none-any.whl/rheofit/__init__.py
@@ -0,0 +1,9 @@
+'''
+Import and analyze rheological data
+-----------------------------------
+'''
+__version__ = "0.1.1"
+
+from . import models
+from . import rheodata
+from . import visualization
