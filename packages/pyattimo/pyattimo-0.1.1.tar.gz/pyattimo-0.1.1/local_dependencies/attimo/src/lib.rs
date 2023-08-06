30f1c97376cdc3d8305160cb1da7b9c9a5779040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:05 +0000

    Add package pyattimo/0.1.1/pyattimo-0.1.1.tar.gz

---
+++ packages/pyattimo/pyattimo-0.1.1.tar.gz/pyattimo-0.1.1/local_dependencies/attimo/src/lib.rs
@@ -0,0 +1,13 @@
+#![feature(allocator_api)]
+#![feature(is_sorted)]
+#![feature(map_first_last)]
+#![feature(string_remove_matches)]
+#![feature(int_roundings)]
+
+pub mod allocator;
+pub mod distance;
+pub mod load;
+pub mod lsh;
+pub mod motifs;
+pub mod sort;
+pub mod timeseries;
