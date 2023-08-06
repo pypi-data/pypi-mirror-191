686e4153d91ed950f668e9377f2c78d47eb8957a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:21 +0000

    Add package penikmatdoujin/1.0/penikmatdoujin-1.0.tar.gz

---
+++ packages/penikmatdoujin/penikmatdoujin-1.0.tar.gz/penikmatdoujin-1.0/PenikmatDoujin/Module/SiteParser/dojing.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+
+nameExc = ['<title>', '</title>', ':', '<', '>', '/', '*', '\\', '|', '\"', " - Bahasa Indonesia"]
+
+def extractTitle(content, multi, chapterNumber):
+    parsedHTML = BeautifulSoup(content, 'html.parser')
+
+    if multi == True:
+        try:
+            for title in parsedHTML.find_all(class_='entry-title'):
+                seriesTitle = title.text
+            seriesTitle = seriesTitle.replace("\n", "")
+            rawCH = parsedHTML.find('div', id='chapterlist')
+            chapterTitle = []
+            for name in rawCH.find_all(class_='chapternum'):
+                chapterTitle.append(name.text)
+            for name in nameExc:
+                seriesTitle = seriesTitle.replace(name, "")
+            chapterTitle.reverse()
+            chName = list()
+            if chapterNumber != None:
+                for x in chapterNumber:
+                    chName.append(chapterTitle[x - 1])
+            else:
+                chName = chapterTitle
+
+        except Exception as e:
+            print(f"Error Occured: {e}")
+            return
+        
+    else:
+        try:
+            tt = parsedHTML.find(class_='allc')
+            for title in tt.find_all('a'):
+                seriesTitle = title.text
+            for name in nameExc:
+                seriesTitle = seriesTitle.replace(name, '')
+            ch = parsedHTML.find(class_='entry-title')
+            chName = ch.text
+        except Exception as e:
+            print(f"Error Occured: {e}")
+            return
+
+    return seriesTitle, chName
---
+++ packages/penikmatdoujin/penikmatdoujin-1.0.tar.gz/penikmatdoujin-1.0/PenikmatDoujin/Module/SiteParser/sektedoujin.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+
+nameExc = ['<title>', '</title>', ':', '<', '>', '/', '*', '\\', '|', '\"', " - Bahasa Indonesia"]
+
+def extractTitle(content, multi, chapterNumber):
+    parsedHTML = BeautifulSoup(content, 'html.parser')
+
+    if multi == True:
+        try:
+            for title in parsedHTML.find_all(class_='entry-title'):
+                seriesTitle = title.text
+            for name in nameExc:
+                seriesTitle = seriesTitle.replace(name, '')
+            rawCH = parsedHTML.find('div', id='chapterlist')
+            chapterTitle = []
+
+            for name in rawCH.find_all(class_='chapternum'):
+                chapterTitle.append(name.text)
+            chapterTitle.reverse()
+            chName = list()
+            if chapterNumber != None:
+                for x in chapterNumber:
+                    chName.append(chapterTitle[x - 1])
+            else:
+                chName = chapterTitle
+
+        except Exception as e:
+            print(f"Error Occured: {e}")
+            return
+        
+    else:
+        try:
+            tt = parsedHTML.find(class_='allc')
+            for title in tt.find_all('a'):
+                seriesTitle = title.text
+            title = str(parsedHTML.title)
+            for name in nameExc:
+                seriesTitle = seriesTitle.replace(name, '')
+            remNum = title.find('Chapter')
+
+            chapterTitle = title[remNum:]
+            for name in nameExc:
+                chapterTitle = chapterTitle.replace(name, '')
+            chName = chapterTitle
+
+        except Exception as e:
+            print(f"Error Occured: {e}")
+            return
+    
+    return seriesTitle, chName
